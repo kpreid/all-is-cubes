9c92229f080e9757bbc0b84d03612ecf7fdbbdae
Kevin Reid <kpreid@switchb.org>
2020-07-30 07:47:27 -0700

    Implement basic block get/set.

--- src/space.rs
+++ src/space.rs
@@ -1,9 +1,11 @@
 //! That which contains many blocks.
 
+pub type GridPoint = cgmath::Point3<isize>;
+
 /// Specifies the coordinate extent of a `Space`.
 #[derive(Clone, Copy, Debug)]
 pub struct Grid {
-  lower_bounds: [isize; 3],
+  lower_bounds: GridPoint,
   sizes: [isize; 3],  // always positive
 }
 
@@ -13,7 +15,7 @@ impl Grid {
     /// For example, if on one axis the lower bound is 5 and the size is 10,
     /// then the positions where blocks can exist are numbered 5 through 14
     /// (inclusive) and the occupied volume spans 5 to 15.
-    pub fn new(lower_bounds: [isize; 3], sizes: [isize; 3]) -> Grid {
+    pub fn new(lower_bounds: GridPoint, sizes: [isize; 3]) -> Grid {
         // TODO: Replace assert! with nice error reporting and then test it
         for i in 0..3 {
             assert!(sizes[i] > 0);
@@ -31,13 +33,30 @@ impl Grid {
     /// Computes the volume of this space in blocks, i.e. the sum of all sizes.
     ///
     /// ```
-    /// let grid = all_is_cubes::space::Grid::new([-10, 3, 7], [100, 200, 300]);
+    /// let grid = all_is_cubes::space::Grid::new(all_is_cubes::space::GridPoint::new(-10, 3, 7), [100, 200, 300]);
     /// assert_eq!(grid.volume(), 6_000_000);
     /// ```
     pub fn volume(&self) -> usize {
         return (self.sizes[0] * self.sizes[1] * self.sizes[2]) as usize;
     }
+    
+    /// Determines whether a point lies within the grid and, if it does, returns the flattened
+    /// array index for it.
+    pub fn index(self, point: GridPoint) -> Option<usize> {
+        let deoffsetted = point - self.lower_bounds;
+        for i in 0..3 {
+            if deoffsetted[i] < 0 || deoffsetted[i] >= self.sizes[i] {
+                return None;
+            }
+        }
+        return Some((
+            (deoffsetted[0] * self.sizes[1] + deoffsetted[1])
+                * self.sizes[2] + deoffsetted[2]
+        ) as usize);
+    }
 }
+
+type Block = u8;  // TODO: indirection
 
 /// Container for blocks arranged in three-dimensional space.
 #[derive(Clone)]
@@ -57,6 +76,44 @@ impl Space {
             contents: vec![0; volume].into_boxed_slice(),
         }
     }
+    
+    /// Constructs a `Space` that is entirely empty and whose coordinate system
+    /// is in the +X+Y+Z quadrant.
+    pub fn empty_positive(wx: isize, wy: isize, wz: isize) -> Space {
+        return Space::empty(Grid::new(GridPoint::new(0, 0, 0), [wx, wy, wz]));
+    }
+    
+    /// Replace the block in this space at the given position.
+    ///
+    /// If the position is out of bounds, there is no effect.
+    ///
+    /// ```
+    /// use all_is_cubes::space::*;
+    /// let mut space = Space::empty_positive(1, 1, 1);
+    /// let p = GridPoint::new(0, 0, 0);
+    /// space.set(p, 123);
+    /// assert_eq!(space[p], 123);
+    /// ```
+    pub fn set(&mut self, position: GridPoint, block: Block) {
+        if let Some(index) = self.grid.index(position) {
+            self.contents[index] = block;
+        }
+    }
+}
+
+impl std::ops::Index<GridPoint> for Space {
+    type Output = Block;
+
+    /// Get the block in this space at the given position.
+    ///
+    /// If the position is out of bounds, there is no effect.
+    fn index(&self, position: GridPoint) -> &Self::Output {
+        if let Some(index) = self.grid.index(position) {
+            &self.contents[index]
+        } else {
+            &0
+        }
+    }
 }
 
 #[cfg(test)]
@@ -65,7 +122,7 @@ mod tests {
   
     #[test]
     fn it_works() {
-        let grid = Grid::new([0, 0, 0], [100, 100, 100]);
+        let grid = Grid::new(GridPoint::new(0, 0, 0), [100, 100, 100]);
         let _space = Space::empty(grid);
         
         // TODO: Replace this with something meaningful
