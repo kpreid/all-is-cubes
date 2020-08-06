b4e16d0b07f4758ffff538eec81d22c0481e24c0
Kevin Reid <kpreid@switchb.org>
2020-08-06 09:55:28 -0700

    A start at DOM manipulation.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 // Copyright 2020 Kevin Reid under the terms of the MIT License as detailed
 // in the accompanying file README.md or <http://opensource.org/licenses/MIT>.
 
+document.getElementById('scene-info-text').innerText = 'Loading code...';
 import("../pkg/index.js").then(module => {
   // Global variable for debug use only
   console.log("Module: ", module);
   window.aic_mod = module;
-}).catch(console.error);
+}).catch(error => {
+  document.getElementById('scene-info-text').innerText +=
+    'Error during initial loading! Check console for details.';
+  console.error(error);
+});
--- src/wasmglue.rs
+++ src/wasmglue.rs
@@ -1,12 +1,13 @@
 // Copyright 2020 Kevin Reid under the terms of the MIT License as detailed
 // in the accompanying file README.md or <http://opensource.org/licenses/MIT>.
 
+use wasm_bindgen::JsCast;
 use wasm_bindgen::prelude::*;
-use web_sys::console;
+use web_sys::{console, HtmlElement};
 
-#[wasm_bindgen]
-pub fn smoke_test_1() -> String {
-    "foo".to_owned()
+fn append_inner_text(element: HtmlElement, text: &str) {
+    //let text = text.into();
+    element.set_inner_text(&(element.inner_text() + text));
 }
 
 #[wasm_bindgen(start)]
@@ -15,6 +16,12 @@ pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
     console::log_1(&JsValue::from_str("Hello world from Rust!"));
+
+    let document = web_sys::window().expect("missing `window`")
+        .document().expect("missing `document`");
+    let info_text = document.get_element_by_id("scene-info-text").expect("missing .scene-info-text")
+        .dyn_into::<HtmlElement>().expect("not HTML???");
+    append_inner_text(info_text, "\nRusting...");
 
     Ok(())
 }
