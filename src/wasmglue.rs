f3be19fd74603cb93b4ce40647cc7e56ac2d5d2c
Kevin Reid <kpreid@switchb.org>
2020-08-06 10:35:53 -0700

    Generic glue for static DOM elements.

--- src/wasmglue.rs
+++ src/wasmglue.rs
@@ -1,14 +1,10 @@
 // Copyright 2020 Kevin Reid under the terms of the MIT License as detailed
 // in the accompanying file README.md or <http://opensource.org/licenses/MIT>.
 
-use wasm_bindgen::JsCast;
+use wasm_bindgen::JsCast;  // dyn_into()
 use wasm_bindgen::prelude::*;
-use web_sys::{console, HtmlElement};
-
-fn append_inner_text(element: HtmlElement, text: &str) {
-    //let text = text.into();
-    element.set_inner_text(&(element.inner_text() + text));
-}
+use js_sys::{Error};
+use web_sys::{console, Document, HtmlElement};
 
 /// Runs on module load. Do only key Rust environment initialization things here.
 #[wasm_bindgen(start)]
@@ -26,11 +22,38 @@ pub fn main_js() -> Result<(), JsValue> {
 pub fn start_game() -> Result<(), JsValue> {
     let document = web_sys::window().expect("missing `window`")
         .document().expect("missing `document`");
-    let info_text = document.get_element_by_id("scene-info-text").expect("missing .scene-info-text")
-        .dyn_into::<HtmlElement>().expect("not HTML???");
-    append_inner_text(info_text, "\nRusting...");
+
+    let static_dom = StaticDom::new(document)?;
+
+    append_inner_text(static_dom.scene_info_text, "\nRusting...");
 
     // TODO: everything else goes here...
 
     Ok(())
+}
+
+struct StaticDom {
+    scene_info_text: HtmlElement,
+}
+
+impl StaticDom {
+    fn new(document: Document) -> Result<Self, Error> {
+        Ok(Self {
+            scene_info_text: Self::get_mandatory_element(document, "scene-info-text")?,
+        })
+    }
+
+    fn get_mandatory_element<E: JsCast>(document: Document, id: &'static str) -> Result<E, Error> {
+        document.get_element_by_id(id)
+            .ok_or_else(|| Error::new(&format!("missing element {:?}", id)))?
+            .dyn_into::<E>()
+            .map_err(|_| Error::new(&format!("element {:?} was not a {:?}", id, std::any::type_name::<E>())))
+    }
+}
+
+/// Equivalent of JS `element.innerText += text`.
+/// Note that this is a read-modify-write and as such is not efficient for long text.
+fn append_inner_text<'a>(element: HtmlElement, text: impl Into<&'a str>) {
+    let text = text.into();
+    element.set_inner_text(&(element.inner_text() + text));
 }
