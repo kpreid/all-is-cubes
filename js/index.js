e30bcdadf464c00a638ceb54fca40ed9fa0760d9
Kevin Reid <kpreid@switchb.org>
2020-08-06 10:04:25 -0700

    Separate app startup from wasm_bindgen(start) hook.
    This way we can load the module in a different context, e.g. for tests.
    
    Also improved error logging by not logging a generic error in the event
    of a Rust panic.

--- js/index.js
+++ js/index.js
@@ -4,10 +4,15 @@
 document.getElementById('scene-info-text').innerText = 'Loading code...';
 import("../pkg/index.js").then(module => {
   // Global variable for debug use only
-  console.log("Module: ", module);
   window.aic_mod = module;
+  console.log("Module: ", module);
+  
+  module.start_game();
 }).catch(error => {
   document.getElementById('scene-info-text').innerText +=
-    'Error during initial loading! Check console for details.';
-  console.error(error);
+    '\nError during initial loading! Check console for details.';
+  if (String(error) !== 'RuntimeError: unreachable') {
+    // Only log errors that aren't Rust's panics because those are logged separately.
+    console.error(error);
+  }
 });
--- src/wasmglue.rs
+++ src/wasmglue.rs
@@ -10,18 +10,27 @@ fn append_inner_text(element: HtmlElement, text: &str) {
     element.set_inner_text(&(element.inner_text() + text));
 }
 
+/// Runs on module load. Do only key Rust environment initialization things here.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
-    console::log_1(&JsValue::from_str("Hello world from Rust!"));
+    console::log_1(&JsValue::from_str("Rust startup hook ran."));
 
+    Ok(())
+}
+
+/// Entry point for normal game-in-a-web-page operation.
+#[wasm_bindgen]
+pub fn start_game() -> Result<(), JsValue> {
     let document = web_sys::window().expect("missing `window`")
         .document().expect("missing `document`");
     let info_text = document.get_element_by_id("scene-info-text").expect("missing .scene-info-text")
         .dyn_into::<HtmlElement>().expect("not HTML???");
     append_inner_text(info_text, "\nRusting...");
+
+    // TODO: everything else goes here...
 
     Ok(())
 }
