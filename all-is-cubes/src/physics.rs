690e6e9431debfac04020a708ec9048b96750657
Kevin Reid <kpreid@switchb.org>
2020-10-07 20:52:31 -0700

    Implement continuous collision detection and sliding.
    There is a bug in this version of the algorithm, such that pushing
    against an inside corner will eventually, but not immediately, allow
    passing through one of the blocks. I am letting it be for the moment
    expecting that future experimentation will reveal the problem.
    
    Also, this version does not reset velocities to zero on collision.
    (Fixing that might end up hiding the above bug.)

--- all-is-cubes/src/physics.rs
+++ all-is-cubes/src/physics.rs
@@ -1,11 +1,11 @@
 // Copyright 2020 Kevin Reid under the terms of the MIT License as detailed
 // in the accompanying file README.md or <http://opensource.org/licenses/MIT>.
 
-use cgmath::{Basis2, Deg, InnerSpace as _, Point3, Rotation, Rotation2, Vector2, Vector3};
+use cgmath::{Basis2, Deg, InnerSpace as _, Point3, Rotation, Rotation2, Vector2, Vector3, Zero};
 use std::collections::HashSet;
 use std::time::Duration;
 
-use crate::math::{Face, FreeCoordinate, GridCoordinate, GridPoint};
+use crate::math::{Face, FreeCoordinate, GridPoint};
 use crate::raycast::Raycaster;
 use crate::space::Space;
 use crate::util::ConciseDebug as _;
@@ -53,35 +53,62 @@ impl Body {
 
         // Do collision detection and resolution.
         if let Some(space) = colliding_space {
-            // Test just the block (collide like we're a point).
             // TODO: Axis-aligned box collision volume.
-
-            // TODO: already_colliding will need to be a list of blocks
-            let mut already_colliding: HashSet<GridPoint> = HashSet::new();
-            for hit in Raycaster::new(self.position, delta_position).within_grid(*space.grid()) {
-                if hit.t_distance >= 1.0 {
-                    // This is as far as we go in this timestep.
-                    break;
-                }
-                if hit.face == Face::WITHIN {
-                    already_colliding.insert(hit.cube);
-                    continue;
-                }
-                if space.get_evaluated(hit.cube).attributes.solid
-                    && !already_colliding.contains(&hit.cube)
-                {
-                    // hit something, stop at that distance.
-                    // TODO: implement sliding movement by retrying after deleting the velocity towards which face we hit.
-                    delta_position = self.velocity * dt * hit.t_distance
-                        + hit.face.normal_vector() * POSITION_EPSILON;
-                    break;
-                }
+            let mut n = 0;
+            while delta_position != Vector3::zero() {
+                assert!(n < 3, "sliding collision loop did not finish");
+                n += 1;
+                // Each call to collide_and_advance will zero at least one axis of delta_position.
+                // The nonzero axes are for sliding movement.
+                delta_position = self.collide_and_advance(space, delta_position);
             }
+        } else {
+            self.position += delta_position;
         }
 
         // TODO: after gravity, falling-below-the-world protection
+    }
 
+    /// Perform a single straight-line position change, stopping at the first obstacle.
+    /// Returns the remainder of `delta_position` that should be retried for sliding movement.
+    fn collide_and_advance(
+        &mut self,
+        space: &Space,
+        mut delta_position: Vector3<FreeCoordinate>,
+    ) -> Vector3<FreeCoordinate> {
+        let mut already_colliding: HashSet<GridPoint> = HashSet::new();
+        for hit in Raycaster::new(self.position, delta_position).within_grid(*space.grid()) {
+            if hit.t_distance >= 1.0 {
+                // Movement is unobstructed in this timestep.
+                break;
+            }
+            if hit.face == Face::WITHIN {
+                // If we are intersecting a block, we are allowed to leave it; pretend
+                // it doesn't exist.
+                // TODO: already_colliding is currently useless but will become useful
+                // when we generalize to colliding as a box instead of a point.
+                already_colliding.insert(hit.cube);
+                continue;
+            }
+            if space.get_evaluated(hit.cube).attributes.solid
+                && !already_colliding.contains(&hit.cube)
+            {
+                // We hit something.
+                // Advance however much straight-line distance is available.
+                let unobstructed_delta_position =
+                    delta_position * hit.t_distance + hit.face.normal_vector() * POSITION_EPSILON;
+                self.position += unobstructed_delta_position;
+                // Figure the distance we have have left.
+                delta_position -= unobstructed_delta_position;
+                // Convert it to sliding movement for the axes we didn't collide in.
+                delta_position[hit.face.axis_number()] = 0.0;
+                return delta_position;
+            }
+        }
+
+        // We did not hit anything for the length of the raycast. Proceed unobstructed.
         self.position += delta_position;
+        Vector3::zero()
     }
 
     pub fn walk(&mut self, x: FreeCoordinate, z: FreeCoordinate) {
