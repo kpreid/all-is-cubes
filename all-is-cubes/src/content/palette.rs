d4e62137b62a5968e2b39570835c7b2ccab08c17
Kevin Reid <kpreid@switchb.org>
2021-01-07 19:08:03 -0800

    Formatting

--- all-is-cubes/src/content/palette.rs
+++ all-is-cubes/src/content/palette.rs
@@ -22,8 +22,8 @@ pub const MISSING_VOXEL_FALLBACK: RGBA = rgba_const!(0.8, 0.0, 1.0, 1.0);
 pub const GRASS: RGB = rgb_const!(0.380, 0.667, 0.192);
 pub const DIRT: RGB = rgb_const!(0.424, 0.314, 0.267);
 pub const STONE: RGB = rgb_const!(0.851, 0.843, 0.835);
-pub const TREE_BARK: RGB = rgb_const!(0.6, 0.3, 0.6);  // TODO: never used, wrong
-pub const TREE_LEAVES: RGB = rgb_const!(0.0, 0.7, 0.2);  // TODO: never used, wrong
+pub const TREE_BARK: RGB = rgb_const!(0.6, 0.3, 0.6); // TODO: never used, wrong
+pub const TREE_LEAVES: RGB = rgb_const!(0.0, 0.7, 0.2); // TODO: never used, wrong
 
 // TODO: Decide what we want *actual* logo(type) colors to be.
 pub const LOGO_FILL: RGB = rgb_const!(0.78, 0.20, 0.47);
