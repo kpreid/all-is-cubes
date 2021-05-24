b5e35828757e5de44b63cc8574db192d57370916
Kevin Reid <kpreid@switchb.org>
2021-05-23 21:26:00 -0700

    Move `all_is_cubes::block` modules into separate files.

---
+++ all-is-cubes/src/block/builder.rs
@@ -0,0 +1,243 @@
+// Copyright 2020-2021 Kevin Reid under the terms of the MIT License as detailed
+// in the accompanying file README.md or <https://opensource.org/licenses/MIT>.
+
+//! Lesser-used helpers for [`BlockBuilder`].
+
+use cgmath::EuclideanSpace as _;
+use std::borrow::Cow;
+
+use crate::block::{Block, BlockAttributes, BlockCollision, BlockDef, Resolution};
+use crate::math::{GridPoint, Rgb, Rgba};
+use crate::space::{Grid, SetCubeError, Space};
+use crate::universe::{Name, URef, Universe, UniverseIndex};
+
+/// Tool for constructing [`Block`] values conveniently.
+///
+/// To create one, call [`Block::builder()`].
+/// ([`BlockBuilder::default()`] is also available.)
+///
+/// ```
+/// use all_is_cubes::block::Block;
+/// use all_is_cubes::math::Rgba;
+/// use std::borrow::Cow;
+///
+/// let block = Block::builder()
+///    .display_name("BROWN")
+///    .color(Rgba::new(0.5, 0.5, 0., 1.))
+///    .build();
+///
+/// assert_eq!(block.evaluate().unwrap().color, Rgba::new(0.5, 0.5, 0., 1.));
+/// assert_eq!(
+///     block.evaluate().unwrap().attributes.display_name,
+///     Cow::Borrowed("BROWN"),
+/// );
+/// ```
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct BlockBuilder<C> {
+    attributes: BlockAttributes,
+    content: C,
+}
+
+impl Default for BlockBuilder<NeedsColorOrVoxels> {
+    fn default() -> Self {
+        // Delegate to inherent impl const fn
+        Self::default()
+    }
+}
+
+impl BlockBuilder<NeedsColorOrVoxels> {
+    pub const fn default() -> BlockBuilder<NeedsColorOrVoxels> {
+        BlockBuilder {
+            attributes: BlockAttributes::default(),
+            content: NeedsColorOrVoxels,
+        }
+    }
+}
+
+impl<C> BlockBuilder<C> {
+    // TODO: When #![feature(const_precise_live_drops)] becomes stable, we can make
+    // this builder mostly usable in const contexts.
+    // https://github.com/rust-lang/rust/issues/73255
+    // Doing that will also require creating non-trait-using alternate methods,
+    // until const traits https://github.com/rust-lang/rust/issues/67792 is also available.
+
+    /// Sets the value for [`BlockAttributes::display_name`].
+    pub fn attributes(mut self, value: BlockAttributes) -> Self {
+        self.attributes = value;
+        self
+    }
+
+    /// Sets the value for [`BlockAttributes::display_name`].
+    pub fn display_name(mut self, value: impl Into<Cow<'static, str>>) -> Self {
+        self.attributes.display_name = value.into();
+        self
+    }
+
+    /// Sets the value for [`BlockAttributes::selectable`].
+    pub const fn selectable(mut self, value: bool) -> Self {
+        self.attributes.selectable = value;
+        self
+    }
+
+    /// Sets the value for [`BlockAttributes::collision`].
+    pub const fn collision(mut self, value: BlockCollision) -> Self {
+        self.attributes.collision = value;
+        self
+    }
+
+    /// Sets the value for [`BlockAttributes::light_emission`].
+    pub fn light_emission(mut self, value: impl Into<Rgb>) -> Self {
+        self.attributes.light_emission = value.into();
+        self
+    }
+
+    /// Sets the color value for building a [`Block::Atom`].
+    ///
+    /// This will replace any previous color **or voxels.**
+    pub fn color(self, color: impl Into<Rgba>) -> BlockBuilder<Rgba> {
+        BlockBuilder {
+            attributes: self.attributes,
+            content: color.into(),
+        }
+    }
+
+    /// Sets the space for building a [`Block::Recur`].
+    ///
+    /// This will replace any previous voxels **or color.**
+    pub fn voxels_ref(
+        self,
+        resolution: Resolution,
+        space: URef<Space>,
+    ) -> BlockBuilder<BlockBuilderVoxels> {
+        BlockBuilder {
+            attributes: self.attributes,
+            content: BlockBuilderVoxels {
+                space,
+                resolution,
+                offset: GridPoint::origin(),
+            },
+        }
+    }
+
+    /// Constructs a `Space` for building a [`Block::Recur`], and calls
+    /// the given function to fill it with blocks, in the manner of [`Space::fill`].
+    ///
+    /// Note that the resulting builder is cloned, all clones will share the same
+    /// space.
+    // TODO: (doc) test for this
+    pub fn voxels_fn<F, B>(
+        self,
+        // TODO: awkward requirement for universe; defer computation instead, or
+        // add a `.universe()` method to provide it once.
+        universe: &mut Universe,
+        // TODO: Maybe resolution should be a separate method? Check usage patterns later.
+        resolution: Resolution,
+        mut function: F,
+    ) -> Result<BlockBuilder<BlockBuilderVoxels>, SetCubeError>
+    where
+        F: FnMut(GridPoint) -> B,
+        B: std::borrow::Borrow<Block>,
+    {
+        let grid = Grid::for_block(resolution);
+        let mut space = Space::empty(grid);
+        space.fill(grid, |point| Some(function(point)))?;
+        Ok(self.voxels_ref(resolution, universe.insert_anonymous(space)))
+    }
+
+    /// Converts this builder into a block value.
+    pub fn build(self) -> Block
+    where
+        C: BuilderContentIndependent,
+    {
+        self.content.build_i(self.attributes)
+    }
+
+    /// Converts this builder into a block value and stores it as a [`BlockDef`] in
+    /// the given [`Universe`] with the given name, then returns a [`Block::Indirect`]
+    /// referring to it.
+    pub fn into_named_definition(
+        self,
+        universe: &mut Universe,
+        name: impl Into<Name>,
+    ) -> Result<Block, crate::universe::InsertError>
+    where
+        C: BuilderContentInUniverse,
+    {
+        let block = self.content.build_u(self.attributes, universe);
+        let def_ref = universe.insert(name.into(), BlockDef::new(block))?;
+        Ok(Block::Indirect(def_ref))
+    }
+}
+
+/// Voxel-specific builder methods.
+impl BlockBuilder<BlockBuilderVoxels> {
+    pub fn offset(mut self, offset: GridPoint) -> Self {
+        self.content.offset = offset;
+        self
+    }
+
+    // TODO: It might be useful to have "offset equal to resolution"
+    // and "add offset", but don't add those until use cases are seen.
+}
+
+/// Allows implicitly converting `BlockBuilder` to the block it would build.
+impl<C: BuilderContentIndependent> From<BlockBuilder<C>> for Block {
+    fn from(builder: BlockBuilder<C>) -> Self {
+        builder.build()
+    }
+}
+/// Equivalent to `Block::builder().color(color)`.
+impl From<Rgba> for BlockBuilder<Rgba> {
+    fn from(color: Rgba) -> Self {
+        Block::builder().color(color)
+    }
+}
+/// Equivalent to `Block::builder().color(color.with_alpha_one())`.
+impl From<Rgb> for BlockBuilder<Rgba> {
+    fn from(color: Rgb) -> Self {
+        Block::builder().color(color.with_alpha_one())
+    }
+}
+
+/// Placeholder type for an incomplete [`BlockBuilder`]'s content. The builder
+/// cannot create an actual block until this is replaced.
+#[allow(clippy::exhaustive_structs)]
+#[derive(Copy, Clone, Debug, Default, Eq, Hash, PartialEq)]
+pub struct NeedsColorOrVoxels;
+/// Content of a [`BlockBuilder`] that can build a block without a [`Universe`].
+pub trait BuilderContentIndependent {
+    fn build_i(self, attributes: BlockAttributes) -> Block;
+}
+/// Content of a [`BlockBuilder`] that can only build a block with a [`Universe`].
+pub trait BuilderContentInUniverse {
+    fn build_u(self, attributes: BlockAttributes, universe: &mut Universe) -> Block;
+}
+/// Every `BuilderContentIndependent` can act as `BuilderContentInUniverse`.
+impl<T: BuilderContentIndependent> BuilderContentInUniverse for T {
+    fn build_u(self, attributes: BlockAttributes, _: &mut Universe) -> Block {
+        self.build_i(attributes)
+    }
+}
+/// Used by [`BlockBuilder::color`].
+impl BuilderContentIndependent for Rgba {
+    fn build_i(self, attributes: BlockAttributes) -> Block {
+        Block::Atom(attributes, self)
+    }
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct BlockBuilderVoxels {
+    space: URef<Space>,
+    resolution: Resolution,
+    offset: GridPoint,
+}
+impl BuilderContentIndependent for BlockBuilderVoxels {
+    fn build_i(self, attributes: BlockAttributes) -> Block {
+        Block::Recur {
+            attributes,
+            offset: self.offset,
+            resolution: self.resolution,
+            space: self.space,
+        }
+    }
+}
---
+++ all-is-cubes/src/block/tests.rs
@@ -0,0 +1,396 @@
+// Copyright 2020-2021 Kevin Reid under the terms of the MIT License as detailed
+// in the accompanying file README.md or <https://opensource.org/licenses/MIT>.
+
+use cgmath::EuclideanSpace as _;
+use std::borrow::Cow;
+
+use crate::block::{
+    builder, Block, BlockAttributes, BlockBuilder, BlockCollision, BlockDef, Evoxel, Resolution,
+};
+use crate::content::make_some_blocks;
+use crate::listen::Sink;
+use crate::math::{GridPoint, GridVector, Rgb, Rgba};
+use crate::space::{Grid, GridArray, Space};
+use crate::universe::Universe;
+
+#[test]
+fn evaluate_opaque_atom_and_attributes() {
+    let color = Rgba::new(1.0, 2.0, 3.0, 1.0);
+    let attributes = BlockAttributes {
+        display_name: Cow::Borrowed(&"hello world"),
+        selectable: false,
+        collision: BlockCollision::None,
+        light_emission: Rgb::ONE,
+        ..BlockAttributes::default()
+    };
+    let block = Block::Atom(attributes.clone(), color);
+    let e = block.evaluate().unwrap();
+    assert_eq!(e.attributes, attributes);
+    assert_eq!(e.color, block.color());
+    assert!(e.voxels.is_none());
+    assert_eq!(e.resolution, 1);
+    assert_eq!(e.opaque, true);
+    assert_eq!(e.visible, true);
+}
+
+#[test]
+fn evaluate_transparent_atom() {
+    let color = Rgba::new(1.0, 2.0, 3.0, 0.5);
+    let block = Block::Atom(BlockAttributes::default(), color);
+    let e = block.evaluate().unwrap();
+    assert_eq!(e.color, block.color());
+    assert!(e.voxels.is_none());
+    assert_eq!(e.opaque, false);
+    assert_eq!(e.visible, true);
+}
+
+#[test]
+fn evaluate_invisible_atom() {
+    let block = Block::Atom(BlockAttributes::default(), Rgba::TRANSPARENT);
+    let e = block.evaluate().unwrap();
+    assert_eq!(e.color, Rgba::TRANSPARENT);
+    assert!(e.voxels.is_none());
+    assert_eq!(e.opaque, false);
+    assert_eq!(e.visible, false);
+}
+
+#[test]
+fn evaluate_voxels_checked_individually() {
+    let resolution = 4;
+    let mut universe = Universe::new();
+
+    let attributes = BlockAttributes {
+        display_name: Cow::Borrowed(&"hello world"),
+        ..BlockAttributes::default()
+    };
+    let block = Block::builder()
+        .attributes(attributes.clone())
+        .voxels_fn(&mut universe, resolution, |point| {
+            let point = point.cast::<f32>().unwrap();
+            Block::from(Rgba::new(point.x, point.y, point.z, 1.0))
+        })
+        .unwrap()
+        .build();
+
+    let e = block.evaluate().unwrap();
+    assert_eq!(e.attributes, attributes);
+    assert_eq!(
+        e.voxels,
+        Some(GridArray::generate(Grid::for_block(resolution), |point| {
+            let point = point.cast::<f32>().unwrap();
+            Evoxel {
+                color: Rgba::new(point.x, point.y, point.z, 1.0),
+                selectable: true,
+                collision: BlockCollision::Hard,
+            }
+        }))
+    );
+    assert_eq!(e.resolution, resolution);
+    assert_eq!(e.opaque, true);
+    assert_eq!(e.visible, true);
+}
+
+#[test]
+fn evaluate_transparent_voxels() {
+    let mut universe = Universe::new();
+    let resolution = 4;
+    let block = Block::builder()
+        .voxels_fn(&mut universe, resolution, |point| {
+            Block::from(Rgba::new(
+                0.0,
+                0.0,
+                0.0,
+                if point == GridPoint::new(0, 0, 0) {
+                    0.5
+                } else {
+                    1.0
+                },
+            ))
+        })
+        .unwrap()
+        .build();
+
+    let e = block.evaluate().unwrap();
+    assert_eq!(e.opaque, false);
+    assert_eq!(e.visible, true);
+}
+
+#[test]
+fn evaluate_voxels_not_filling_block() {
+    let mut universe = Universe::new();
+    let block = Block::builder()
+        .voxels_fn(&mut universe, 4, |point| {
+            Block::from(Rgba::new(
+                0.0,
+                0.0,
+                0.0,
+                if point == GridPoint::new(1, 1, 1) {
+                    1.0
+                } else {
+                    0.0
+                },
+            ))
+        })
+        .unwrap()
+        .build();
+
+    let e = block.evaluate().unwrap();
+    assert_eq!(e.resolution, 4);
+    assert_eq!(e.opaque, false);
+    assert_eq!(e.visible, true);
+}
+
+/// Tests that the `offset` field of `Block::Recur` is respected.
+#[test]
+fn recur_with_offset() {
+    let resolution = 4;
+    let offset = GridVector::new(resolution, 0, 0);
+    let mut universe = Universe::new();
+    let mut space = Space::empty_positive(resolution * 2, resolution, resolution);
+    space
+        .fill(space.grid(), |point| {
+            let point = point.cast::<f32>().unwrap();
+            Some(Block::Atom(
+                BlockAttributes::default(),
+                Rgba::new(point.x, point.y, point.z, 1.0),
+            ))
+        })
+        .unwrap();
+    let space_ref = universe.insert_anonymous(space);
+    let block_at_offset = Block::Recur {
+        attributes: BlockAttributes::default(),
+        offset: GridPoint::from_vec(offset),
+        resolution: resolution as Resolution,
+        space: space_ref.clone(),
+    };
+
+    let e = block_at_offset.evaluate().unwrap();
+    assert_eq!(
+        e.voxels,
+        Some(GridArray::generate(
+            Grid::for_block(resolution as Resolution),
+            |point| {
+                let point = (point + offset).cast::<f32>().unwrap();
+                Evoxel {
+                    color: Rgba::new(point.x, point.y, point.z, 1.0),
+                    selectable: true,
+                    collision: BlockCollision::Hard,
+                }
+            }
+        ))
+    );
+}
+
+#[test]
+fn indirect_equivalence() {
+    let resolution = 4;
+    let mut universe = Universe::new();
+    let mut space = Space::empty_positive(resolution, resolution, resolution);
+    // TODO: BlockGen should support constructing indirects (by default, even)
+    // and we can use the more concise version
+    space
+        .fill(space.grid(), |point| {
+            let point = point.cast::<f32>().unwrap();
+            Some(Block::from(Rgba::new(point.x, point.y, point.z, 1.0)))
+        })
+        .unwrap();
+    let space_ref = universe.insert_anonymous(space);
+    let block = Block::builder()
+        .voxels_ref(resolution as Resolution, space_ref.clone())
+        .build();
+    let eval_bare = block.evaluate();
+    let block_def_ref = universe.insert_anonymous(BlockDef::new(block));
+    let eval_def = block_def_ref.borrow().block.evaluate();
+    assert_eq!(eval_bare, eval_def);
+}
+
+#[test]
+fn listen_atom() {
+    let block = Block::from(Rgba::WHITE);
+    let mut sink = Sink::new();
+    block.listen(sink.listener()).unwrap();
+    assert_eq!(None, sink.next());
+    // No notifications are possible, so nothing more to test.
+}
+
+#[test]
+fn listen_indirect_atom() {
+    let mut universe = Universe::new();
+    let block_def_ref = universe.insert_anonymous(BlockDef::new(Block::from(Rgba::WHITE)));
+    let indirect = Block::Indirect(block_def_ref.clone());
+    let mut sink = Sink::new();
+    indirect.listen(sink.listener()).unwrap();
+    assert_eq!(None, sink.next());
+
+    // Now mutate it and we should see a notification.
+    *(block_def_ref.borrow_mut().modify()) = Block::from(Rgba::BLACK);
+    assert!(sink.next().is_some());
+}
+
+/// Testing double indirection not because it's a case we expect to use routinely,
+/// but because it exercises the generality of the notification mechanism.
+#[test]
+fn listen_indirect_double() {
+    let mut universe = Universe::new();
+    let block_def_ref1 = universe.insert_anonymous(BlockDef::new(Block::from(Rgba::WHITE)));
+    let block_def_ref2 =
+        universe.insert_anonymous(BlockDef::new(Block::Indirect(block_def_ref1.clone())));
+    let indirect2 = Block::Indirect(block_def_ref2.clone());
+    let mut sink = Sink::new();
+    indirect2.listen(sink.listener()).unwrap();
+    assert_eq!(None, sink.next());
+
+    // Now mutate the original block and we should see a notification.
+    *(block_def_ref1.borrow_mut().modify()) = Block::from(Rgba::BLACK);
+    assert!(sink.next().is_some());
+
+    // Remove block_def_ref1 from the contents of block_def_ref2...
+    *(block_def_ref2.borrow_mut().modify()) = Block::from(Rgba::BLACK);
+    assert!(sink.next().is_some());
+    assert!(sink.next().is_none());
+    // ...and then block_def_ref1's changes should NOT be forwarded.
+    *(block_def_ref1.borrow_mut().modify()) = Block::from(Rgba::WHITE);
+    assert!(sink.next().is_none());
+}
+
+/// Test that changes to a `Space` propagate to block listeners.
+#[test]
+fn listen_recur() {
+    let mut universe = Universe::new();
+    let [block_0, block_1] = make_some_blocks();
+    let space_ref = universe.insert_anonymous(Space::empty_positive(2, 1, 1));
+    let block = Block::builder().voxels_ref(1, space_ref.clone()).build();
+    let mut sink = Sink::new();
+    block.listen(sink.listener()).unwrap();
+    assert_eq!(None, sink.next());
+
+    // Now mutate the space and we should see a notification.
+    space_ref.borrow_mut().set((0, 0, 0), &block_0).unwrap();
+    assert!(sink.next().is_some());
+
+    // TODO: Also test that we don't propagate lighting changes
+
+    // A mutation out of bounds should not trigger a notification
+    space_ref.borrow_mut().set((1, 0, 0), &block_1).unwrap();
+    assert_eq!(sink.next(), None);
+}
+
+// TODO: test of evaluate where the block's space is the wrong size
+
+#[test]
+fn builder_defaults() {
+    let color = Rgba::new(0.1, 0.2, 0.3, 0.4);
+    assert_eq!(
+        Block::builder().color(color).build(),
+        Block::Atom(BlockAttributes::default(), color),
+    );
+}
+
+#[test]
+fn builder_every_field() {
+    let color = Rgba::new(0.1, 0.2, 0.3, 0.4);
+    let light_emission = Rgb::new(0.1, 3.0, 0.1);
+    assert_eq!(
+        Block::builder()
+            .display_name("hello world")
+            .collision(BlockCollision::None) // TODO: when we have more interesting values, use one
+            .color(color)
+            .selectable(false)
+            .light_emission(light_emission)
+            .build(),
+        Block::Atom(
+            BlockAttributes {
+                display_name: "hello world".into(),
+                collision: BlockCollision::None,
+                selectable: false,
+                light_emission
+            },
+            color
+        ),
+    );
+}
+
+#[test]
+fn builder_voxels_from_space() {
+    let mut universe = Universe::new();
+    let space_ref = universe.insert_anonymous(Space::empty_positive(1, 1, 1));
+
+    assert_eq!(
+        Block::builder()
+            .display_name("hello world")
+            .voxels_ref(2, space_ref.clone())
+            .build(),
+        Block::Recur {
+            attributes: BlockAttributes {
+                display_name: "hello world".into(),
+                ..BlockAttributes::default()
+            },
+            offset: GridPoint::origin(),
+            resolution: 2, // not same as space size
+            space: space_ref
+        },
+    );
+}
+
+#[test]
+fn builder_default_equivalent() {
+    assert_eq!(
+        BlockBuilder::<builder::NeedsColorOrVoxels>::default(),
+        <BlockBuilder<builder::NeedsColorOrVoxels> as Default>::default()
+    );
+}
+
+#[test]
+fn attributes_default_equivalent() {
+    assert_eq!(
+        BlockAttributes::default(),
+        <BlockAttributes as Default>::default()
+    );
+}
+
+#[test]
+fn attributes_debug() {
+    let default = BlockAttributes::default;
+    fn debug(a: BlockAttributes) -> String {
+        format!("{:?}", a)
+    }
+    assert_eq!(&*debug(BlockAttributes::default()), "BlockAttributes {}",);
+    assert_eq!(
+        &*debug(BlockAttributes {
+            display_name: "x".into(),
+            ..default()
+        }),
+        "BlockAttributes { display_name: \"x\" }",
+    );
+    assert_eq!(
+        &*debug(BlockAttributes {
+            selectable: false,
+            ..default()
+        }),
+        "BlockAttributes { selectable: false }",
+    );
+    assert_eq!(
+        &*debug(BlockAttributes {
+            collision: BlockCollision::None,
+            ..default()
+        }),
+        "BlockAttributes { collision: None }",
+    );
+    assert_eq!(
+        &*debug(BlockAttributes {
+            light_emission: Rgb::new(1.0, 2.0, 3.0),
+            ..default()
+        }),
+        "BlockAttributes { light_emission: Rgb(1.0, 2.0, 3.0) }",
+    );
+
+    // Test a case of multiple attributes
+    assert_eq!(
+        &*debug(BlockAttributes {
+            display_name: "y".into(),
+            selectable: false,
+            ..default()
+        }),
+        "BlockAttributes { display_name: \"y\", selectable: false }",
+    );
+}
