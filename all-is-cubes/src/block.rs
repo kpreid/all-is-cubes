kpreid/all-is-cubes

commit 25d133ddf167ba10392e00b4bb25238b67093d11

Make EvaluatedBlock's grid partial so we can do high-res text blocks cheaper.

all-is-cubes/src/block.rs
@@ -173,9 +173,9 @@ impl Block {
                 let resolution_g: GridCoordinate = resolution.max(1).into();
 
                 let block_space = space_ref.try_borrow()?;
-                // TODO: Compute and store a potentially smaller grid, to save time and memory
-                // with thin high-resolution blocks (i.e. text).
-                let grid = Grid::new(offset, [resolution_g, resolution_g, resolution_g]);
+                let grid = Grid::new(offset, [resolution_g, resolution_g, resolution_g])
+                    .intersection(block_space.grid())
+                    .unwrap_or_else(|| Grid::new(offset, [1, 1, 1])); // TODO: would be nice not to need this arbitrary value
                 let voxels = block_space
                     .extract(
                         grid,
@@ -211,14 +211,17 @@ impl Block {
             // TODO: this has no unit tests
             Block::Rotated(rotation, block) => {
                 let base = block.evaluate()?;
+                let resolution = base.resolution;
                 Ok(EvaluatedBlock {
                     voxels: base.voxels.map(|voxels| {
-                        let resolution = voxels.grid().size().x;
-                        let matrix = rotation.to_positive_octant_matrix(resolution);
-                        // TODO: Use a rotated grid on the output, which will prevent the function from panicking if the grid size is incorrect
-                        GridArray::generate(voxels.grid(), |cube| {
-                            voxels[matrix.transform_cube(cube)]
-                        })
+                        let matrix = rotation.to_positive_octant_matrix(resolution.into());
+                        let inverse_matrix = rotation
+                            .inverse()
+                            .to_positive_octant_matrix(resolution.into());
+                        GridArray::generate(
+                            voxels.grid().transform(inverse_matrix).unwrap(),
+                            |cube| voxels[matrix.transform_cube(cube)],
+                        )
                     }),
                     ..base
                 })
@@ -545,6 +548,15 @@ pub struct Evoxel {
 }
 
 impl Evoxel {
+    /// The `Evoxel` value that would have resulted from using [`AIR`] in a recursive block.
+    ///
+    /// TODO: Write a test for that.
+    pub const AIR: Self = Self {
+        color: Rgba::TRANSPARENT,
+        selectable: false,
+        collision: BlockCollision::None,
+    };
+
     /// Construct the [`Evoxel`] that would have resulted from evaluating a voxel block
     /// with the given color and default attributes.
     pub const fn new(color: Rgba) -> Self {

all-is-cubes/src/triangulator/block_tri.rs
@@ -167,6 +167,9 @@ pub fn triangulate_block<V: From<BlockVertex>, A: TextureAllocator>(
             // How should we scale texels versus the standard size to get correct display?
             let voxel_scale_modifier =
                 block_resolution as TextureCoordinate / tile_resolution as TextureCoordinate;
+            // Map quad vertices in voxel grid coordinates to containing block coordinates.
+            let vertex_scale = FreeCoordinate::from(block.resolution).recip();
+            let scale_vertex = |s| FreeCoordinate::from(s) * vertex_scale;
 
             let mut texture_if_needed: Option<A::Tile> = None;
 
@@ -175,9 +178,17 @@ pub fn triangulate_block<V: From<BlockVertex>, A: TextureAllocator>(
             for &face in Face::ALL_SIX {
                 let transform = face.matrix(block_resolution - 1);
 
+                // Rotate the voxel array's extent into our local coordinate system, so we can find
+                // out what range to iterate over.
+                // TODO: Avoid using a matrix inversion
+                let rotated_voxel_range = voxels
+                    .grid()
+                    .transform(face.matrix(block_resolution).inverse_transform().unwrap())
+                    .unwrap();
+
                 // Layer 0 is the outside surface of the cube and successive layers are
                 // deeper below that surface.
-                for layer in 0..block_resolution {
+                for layer in rotated_voxel_range.z_range() {
                     // TODO: Have EvaluatedBlock tell us when a block is fully cubical and opaque,
                     // and then only scan the first and last layers. EvaluatedBlock.fully_opaque
                     // is not quite that because it is defined to allow concavities.
@@ -190,20 +201,16 @@ pub fn triangulate_block<V: From<BlockVertex>, A: TextureAllocator>(
                     // That is, it excludes all obscured interior volume.
                     // First, we traverse the block and fill this with non-obscured voxels,
                     // then we erase it as we convert contiguous rectangles of it to quads.
-                    let mut visible_image: Vec<Rgba> =
-                        Vec::with_capacity(block_resolution.pow(2) as usize);
+                    let mut visible_image: Vec<Rgba> = Vec::with_capacity(
+                        rotated_voxel_range.x_range().len() * rotated_voxel_range.y_range().len(),
+                    );
 
-                    for t in 0..block_resolution {
-                        for s in 0..block_resolution {
+                    for t in rotated_voxel_range.y_range() {
+                        for s in rotated_voxel_range.x_range() {
                             let cube: Point3<GridCoordinate> =
                                 transform.transform_point(Point3::new(s, t, layer));
 
-                            // Diagnose out-of-space accesses. TODO: Tidy this up and document it, or remove it:
-                            // it will happen whenever the space is the wrong size for the textures.
-                            let color = voxels
-                                .get(cube)
-                                .unwrap_or(&Evoxel::new(palette::MISSING_VOXEL_FALLBACK))
-                                .color;
+                            let color = voxels.get(cube).unwrap_or(&Evoxel::AIR).color;
 
                             if layer == 0 && !color.fully_opaque() {
                                 // If the first layer is transparent in any cube at all, then the face is
@@ -249,48 +256,53 @@ pub fn triangulate_block<V: From<BlockVertex>, A: TextureAllocator>(
 
                     // Traverse `visible_image` using the "greedy meshing" algorithm for
                     // breaking an irregular shape into quads.
-                    GreedyMesher::new(visible_image, block_resolution).run(
-                        |mesher, low_corner, high_corner| {
-                            // Generate quad.
-                            let coloring = if let Some(single_color) = mesher.single_color {
-                                // The quad we're going to draw has identical texels, so we might as
-                                // well use a solid color and skip needing a texture.
-                                QuadColoring::<A::Tile>::Solid(single_color)
+                    GreedyMesher::new(
+                        visible_image,
+                        rotated_voxel_range.x_range(),
+                        rotated_voxel_range.y_range(),
+                    )
+                    .run(|mesher, low_corner, high_corner| {
+                        let low_corner = low_corner.map(scale_vertex);
+                        let high_corner = high_corner.map(scale_vertex);
+                        // Generate quad.
+                        let coloring = if let Some(single_color) = mesher.single_color {
+                            // The quad we're going to draw has identical texels, so we might as
+                            // well use a solid color and skip needing a texture.
+                            QuadColoring::<A::Tile>::Solid(single_color)
+                        } else {
+                            if texture_if_needed.is_none() {
+                                // Try to compute texture
+                                texture_if_needed =
+                                    copy_voxels_to_texture(texture_allocator, voxels);
+                            }
+                            if let Some(ref texture) = texture_if_needed {
+                                QuadColoring::Texture(texture, voxel_scale_modifier)
                             } else {
-                                if texture_if_needed.is_none() {
-                                    // Try to compute texture
-                                    texture_if_needed =
-                                        copy_voxels_to_texture(texture_allocator, voxels);
-                                }
-                                if let Some(ref texture) = texture_if_needed {
-                                    QuadColoring::Texture(texture, voxel_scale_modifier)
-                                } else {
-                                    // Texture allocation failure.
-                                    // TODO: Mark this triangulation as defective in the return value, so
-                                    // that when more space is available, it can be retried, rather than
-                                    // having lingering failures.
-                                    // TODO: Add other fallback strategies such as using multiple quads instead
-                                    // of textures.
-                                    QuadColoring::Solid(palette::MISSING_TEXTURE_FALLBACK)
-                                }
-                            };
+                                // Texture allocation failure.
+                                // TODO: Mark this triangulation as defective in the return value, so
+                                // that when more space is available, it can be retried, rather than
+                                // having lingering failures.
+                                // TODO: Add other fallback strategies such as using multiple quads instead
+                                // of textures.
+                                QuadColoring::Solid(palette::MISSING_TEXTURE_FALLBACK)
+                            }
+                        };
 
-                            push_quad(
-                                vertices,
-                                if mesher.rect_has_alpha {
-                                    indices_transparent
-                                } else {
-                                    indices_opaque
-                                },
-                                face,
-                                depth,
-                                low_corner,
-                                high_corner,
-                                coloring,
-                                tile_resolution,
-                            );
-                        },
-                    );
+                        push_quad(
+                            vertices,
+                            if mesher.rect_has_alpha {
+                                indices_transparent
+                            } else {
+                                indices_opaque
+                            },
+                            face,
+                            depth,
+                            low_corner,
+                            high_corner,
+                            coloring,
+                            tile_resolution,
+                        );
+                    });
                 }
             }
 

all-is-cubes/src/triangulator/planar.rs
@@ -8,6 +8,7 @@ use cgmath::{
     Zero as _,
 };
 use std::convert::{TryFrom, TryInto};
+use std::ops::Range;
 
 use crate::math::{Face, FreeCoordinate, GridCoordinate, Rgba};
 use crate::triangulator::{BlockVertex, Coloring, TextureCoordinate, TextureTile};
@@ -16,19 +17,24 @@ use crate::triangulator::{BlockVertex, Coloring, TextureCoordinate, TextureTile}
 /// <https://0fps.net/2012/06/30/meshing-in-a-minecraft-game/>
 pub(crate) struct GreedyMesher {
     visible_image: Vec<Rgba>,
-    resolution_g: GridCoordinate,
-    resolution_s: usize,
+    // Logical bounding rectangle of the data in `visible_image`.
+    image_s_range: Range<GridCoordinate>,
+    image_t_range: Range<GridCoordinate>,
     /// Contains a color if all voxels examined so far have that color.
     pub(crate) single_color: Option<Rgba>,
     pub(crate) rect_has_alpha: bool,
 }
 impl GreedyMesher {
     /// Create the initial state.
-    pub(crate) fn new(visible_image: Vec<Rgba>, resolution: GridCoordinate) -> Self {
+    pub(crate) fn new(
+        visible_image: Vec<Rgba>,
+        image_s_range: Range<GridCoordinate>,
+        image_t_range: Range<GridCoordinate>,
+    ) -> Self {
         Self {
             visible_image,
-            resolution_g: resolution,
-            resolution_s: resolution.try_into().unwrap(),
+            image_s_range,
+            image_t_range,
             single_color: None,
             rect_has_alpha: false,
         }
@@ -37,10 +43,10 @@ impl GreedyMesher {
     /// Actually run the algorithm.
     pub(crate) fn run(
         mut self,
-        mut quad_callback: impl FnMut(&Self, Point2<FreeCoordinate>, Point2<FreeCoordinate>),
+        mut quad_callback: impl FnMut(&Self, Point2<GridCoordinate>, Point2<GridCoordinate>),
     ) {
-        for tl in 0..self.resolution_g {
-            for sl in 0..self.resolution_g {
+        for tl in self.image_t_range.clone() {
+            for sl in self.image_s_range.clone() {
                 if !self.add_seed(sl, tl) {
                     continue;
                 }
@@ -48,7 +54,7 @@ impl GreedyMesher {
                 let mut sh = sl;
                 loop {
                     sh += 1;
-                    if sh >= self.resolution_g {
+                    if sh >= self.image_s_range.end {
                         break; // Found the far edge
                     }
                     if !self.add_candidate(sh, tl) {
@@ -59,7 +65,7 @@ impl GreedyMesher {
                 let mut th = tl;
                 'expand_t: loop {
                     th += 1;
-                    if th >= self.resolution_g {
+                    if th >= self.image_t_range.end {
                         break; // Found the far edge
                     }
                     // Check if all the voxels are wanted
@@ -78,13 +84,7 @@ impl GreedyMesher {
                         self.erase(s, t);
                     }
                 }
-                let map_coord =
-                    |c| FreeCoordinate::from(c) / FreeCoordinate::from(self.resolution_g);
-                quad_callback(
-                    &self,
-                    Point2::new(map_coord(sl), map_coord(tl)),
-                    Point2::new(map_coord(sh), map_coord(th)),
-                );
+                quad_callback(&self, Point2::new(sl, tl), Point2::new(sh, th));
             }
         }
     }
@@ -92,16 +92,16 @@ impl GreedyMesher {
     #[inline]
     fn index(&self, s: GridCoordinate, t: GridCoordinate) -> usize {
         // Can't fail because a usize ≈ u16 platform is too small anyway.
-        let s = usize::try_from(s).unwrap();
-        let t = usize::try_from(t).unwrap();
-        t * self.resolution_s as usize + s
+        let s = usize::try_from(s - self.image_s_range.start).unwrap();
+        let t = usize::try_from(t - self.image_t_range.start).unwrap();
+        t * self.image_s_range.len() + s
     }
 
     /// Checks if a voxel is visible and thus can be the seed of a rectangle,
     /// returns false if not, and updates `single_color`.
     #[inline]
     fn add_seed(&mut self, s: GridCoordinate, t: GridCoordinate) -> bool {
-        if s >= self.resolution_g || t >= self.resolution_g {
+        if !self.image_s_range.contains(&s) || !self.image_t_range.contains(&t) {
             panic!("seed loop ran out of bounds");
         }
         let color = self.visible_image[self.index(s, t)];
@@ -117,7 +117,7 @@ impl GreedyMesher {
     /// returns false if not, and updates `single_color`.
     #[inline]
     fn add_candidate(&mut self, s: GridCoordinate, t: GridCoordinate) -> bool {
-        if s >= self.resolution_g || t >= self.resolution_g {
+        if !self.image_s_range.contains(&s) || !self.image_t_range.contains(&t) {
             return false;
         }
         let color = self.visible_image[self.index(s, t)];

commit 5d96d3ed3ad5b69301edf360fe830cf10f78b2d6

Fix recursive blocks with small spaces counting as opaque.
This fixes a bug that was introduced in commit 25d133ddf167ba10392e00b4bb25238b67093d11
by adding special cases for small spaces without considering the effects on the opacity
logic.

all-is-cubes/src/block.rs
@@ -167,18 +167,21 @@ impl Block {
                 resolution,
                 space: ref space_ref,
             } => {
+                let block_space = space_ref.try_borrow()?;
+
                 // Ensure resolution is at least 1 to not panic on bad data.
                 // (We could eliminate this if Grid allowed a size of zero, but that
                 // might lead to division-by-zero trouble elsewhere...)
                 let resolution_g: GridCoordinate = resolution.max(1).into();
-
-                let block_space = space_ref.try_borrow()?;
-                let grid = Grid::new(offset, [resolution_g, resolution_g, resolution_g])
+                let full_resolution_grid =
+                    Grid::new(offset, [resolution_g, resolution_g, resolution_g]);
+                let occupied_grid = full_resolution_grid
                     .intersection(block_space.grid())
-                    .unwrap_or_else(|| Grid::new(offset, [1, 1, 1])); // TODO: would be nice not to need this arbitrary value
+                    .unwrap_or_else(|| Grid::new(offset, [1, 1, 1]) /* arbitrary value */);
+
                 let voxels = block_space
                     .extract(
-                        grid,
+                        occupied_grid,
                         #[inline(always)]
                         |_index, sub_block_data, _lighting| {
                             let sub_evaluated = sub_block_data.evaluated();
@@ -193,12 +196,13 @@ impl Block {
                 Ok(EvaluatedBlock {
                     attributes: attributes.clone(),
                     color: Rgba::new(0.5, 0.5, 0.5, 1.0), // TODO replace this with averaging the voxels
-                    // TODO wrong test: we want to see if the _faces_ are all opaque but allow hollows
                     resolution,
-                    opaque: voxels.grid().interior_iter().all(
-                        #[inline(always)]
-                        |p| voxels[p].color.fully_opaque(),
-                    ),
+                    // TODO wrong test: we want to see if the _faces_ are all opaque but allow hollows
+                    opaque: occupied_grid == full_resolution_grid
+                        && voxels.grid().interior_iter().all(
+                            #[inline(always)]
+                            |p| voxels[p].color.fully_opaque(),
+                        ),
                     visible: voxels.grid().interior_iter().any(
                         #[inline(always)]
                         |p| !voxels[p].color.fully_transparent(),

all-is-cubes/src/block/tests.rs
@@ -141,6 +141,27 @@ fn evaluate_voxels_not_filling_block() {
     assert_eq!(e.visible, true);
 }
 
+/// Test the situation where the space is smaller than the block: in particular,
+/// even if the space is all opaque, the block should not be counted as opaque.
+#[test]
+fn evaluate_voxels_partial_not_filling() {
+    let resolution = 4;
+    let mut universe = Universe::new();
+    let mut space = Space::empty_positive(2, 4, 4);
+    space
+        .fill_uniform(space.grid(), Block::from(Rgba::WHITE))
+        .unwrap();
+    let space_ref = universe.insert_anonymous(space);
+    let block = Block::builder()
+        .voxels_ref(resolution as Resolution, space_ref.clone())
+        .build();
+
+    let e = block.evaluate().unwrap();
+    assert_eq!(e.resolution, 4);
+    assert_eq!(e.opaque, false);
+    assert_eq!(e.visible, true);
+}
+
 /// Tests that the `offset` field of `Block::Recur` is respected.
 #[test]
 fn recur_with_offset() {

all-is-cubes/src/triangulator/block_tri.rs
@@ -186,11 +186,16 @@ pub fn triangulate_block<V: From<BlockVertex>, A: TextureAllocator>(
                     .transform(face.matrix(block_resolution).inverse_transform().unwrap())
                     .unwrap();
 
+                // Check the case where the block's voxels don't meet its front face.
+                if !rotated_voxel_range.z_range().contains(&0) {
+                    output_by_face[face].fully_opaque = false;
+                }
+
                 // Layer 0 is the outside surface of the cube and successive layers are
                 // deeper below that surface.
                 for layer in rotated_voxel_range.z_range() {
                     // TODO: Have EvaluatedBlock tell us when a block is fully cubical and opaque,
-                    // and then only scan the first and last layers. EvaluatedBlock.fully_opaque
+                    // and then only scan the first and last layers. EvaluatedBlock.opaque
                     // is not quite that because it is defined to allow concavities.
 
                     // Becomes true if there is any voxel that is both non-fully-transparent and