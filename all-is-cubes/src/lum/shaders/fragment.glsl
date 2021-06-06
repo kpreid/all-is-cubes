3246914b47cfb709085d333609e0cceb05bf7a30
Kevin Reid <kpreid@switchb.org>
2021-06-06 08:50:48 -0700

    Convert to sRGB in fragment shader.
    Unfortunately we don't have any API guarantees about target color space,
    but most things use sRGB and it produces the right result in my testing,
    including matching the raytracer.
    
    Of course, this does throw off all my color palette choices;
    besides general lightness, the road, grass, and dirt colors were chosen
    by sampling photographs and copying the sRGB numbers without thinking
    about it. Oops.

--- all-is-cubes/src/lum/shaders/fragment.glsl
+++ all-is-cubes/src/lum/shaders/fragment.glsl
@@ -22,7 +22,7 @@ in lowp float fog_mix;
 // from the camera position to this fragment.
 in highp vec3 camera_ray_direction;
 
-out mediump vec4 fragment_color;
+out mediump vec4 fragment_color_srgb;
 
 
 // Find the smallest positive `t` such that `s + t * ds` is an integer,
@@ -205,6 +205,16 @@ void main(void) {
   // Fog
   color.rgb = mix(color.rgb, fog_color, fog_mix);
 
+  // Convert from linear to sRGB color.
+  // Source: <https://en.wikipedia.org/w/index.php?title=SRGB&oldid=1002296118#The_forward_transformation_(CIE_XYZ_to_sRGB)> (version as of Feb 3, 2020)
+  if (true) {
+    color.rgb = mix(
+      (211. * pow(color.rgb, vec3(5.0 / 12.0)) - vec3(11.0)) / 200.0,
+      color.rgb * (323.0 / 25.0),
+      vec3(lessThan(color.rgb, vec3(0.0031308)))
+    );
+  }
+
   // Multiply alpha because our blend function choice is premultiplied alpha.
-  fragment_color = vec4(color.rgb * color.a, color.a);
+  fragment_color_srgb = vec4(color.rgb * color.a, color.a);
 }
