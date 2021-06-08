2769caf2d054e618fe6ca2313bb7902a56c76fb2
Kevin Reid <kpreid@switchb.org>
2021-06-08 08:34:56 -0700

    Split Body-related code out of physics.rs.

---
+++ all-is-cubes/src/physics/collision.rs
@@ -0,0 +1,6 @@
+// Copyright 2020-2021 Kevin Reid under the terms of the MIT License as detailed
+// in the accompanying file README.md or <https://opensource.org/licenses/MIT>.
+
+//! Algorithms for collision detection with [`Space`](crate::space::Space)s.
+
+// TODO: Move collision algorithms here (refactoring in progress).
