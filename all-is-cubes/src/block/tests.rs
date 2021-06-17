4a7b2b9fe6c69be00d4e14819cd786182d04eb61
Kevin Reid <kpreid@switchb.org>
2021-06-17 08:08:15 -0700

    Rust 1.53: suppress new warning bool_assert_comparison

--- all-is-cubes/src/block/tests.rs
+++ all-is-cubes/src/block/tests.rs
@@ -1,6 +1,8 @@
 // Copyright 2020-2021 Kevin Reid under the terms of the MIT License as detailed
 // in the accompanying file README.md or <https://opensource.org/licenses/MIT>.
 
+#![allow(clippy::bool_assert_comparison)]
+
 use cgmath::EuclideanSpace as _;
 use std::borrow::Cow;
 
