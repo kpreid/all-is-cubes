6a1cfcdb0e477740c6b4dc488be201082da98e3b
Kevin Reid <kpreid@switchb.org>
2021-07-06 08:07:56 -0700

    Add fuzz_physics target.
    This “test” doesn't “pass” yet, but the goal is that a `Body`
    moving around in an unchanging `Space` should never trigger the
    push-out recovery routine.
    
    This is loosely based on the existing no_passing_through_blocks test.
    
    Future work: we should make the Space generation based on fuzzer input
    data, so that small test cases can be produced.

---
+++ all-is-cubes/fuzz/fuzz_targets/fuzz_physics.rs
@@ -0,0 +1,42 @@
+#![no_main]
+extern crate all_is_cubes;
+
+use all_is_cubes::apps::Tick;
+use all_is_cubes::cgmath::{EuclideanSpace, InnerSpace, Point3, Vector3};
+use all_is_cubes::content::UniverseTemplate;
+use all_is_cubes::math::FreeCoordinate;
+
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|input: ([FreeCoordinate; 3], [FreeCoordinate; 3])| {
+    let position: Point3<FreeCoordinate> = input.0.into();
+    let velocity: Vector3<FreeCoordinate> = input.1.into();
+    // TODO: write a proper Arbitrary impl on a wrapper
+    if !position.to_vec().magnitude2().is_finite() {
+        return;
+    }
+    if !velocity.magnitude2().is_finite() {
+        return;
+    }
+
+    // Use DemoCity as a complex world to exercise physics in.
+    // TODO: Instead, take fuzzer input data to build a just-complex-enough one.
+    let universe = UniverseTemplate::DemoCity.build().unwrap();
+    let mut character_mut = universe.get_default_character().unwrap().borrow_mut();
+    character_mut.body.position = position;
+    character_mut.body.velocity = velocity;
+
+    for i in 0..5000 {
+        let (info, _tx) = character_mut.step(None, Tick::arbitrary());
+
+        // Check for no push out, but not on the first step, which might have been due to initial
+        // placement in a bad location.
+        if i != 0 {
+            assert_eq!(
+                info.expect("should be making body steps").push_out,
+                None,
+                "triggered push_out"
+            );
+        }
+    }
+});
