5b8f04b9490cbbdc602c70d9e64c0473d3db9b39
Kevin Reid <kpreid@switchb.org>
2021-09-04 20:05:20 -0700

    fuzz_physics uses Arbitrary space instead of demo city.
    This lets us run much faster test cases.
    It also now hangs, which we will need to investigate --
    it appears to be stuck inside `collide_along_ray`, which shouldn't
    be happening since we've checked the ray length...

--- all-is-cubes/fuzz/fuzz_targets/fuzz_physics.rs
+++ all-is-cubes/fuzz/fuzz_targets/fuzz_physics.rs
@@ -2,45 +2,56 @@
 extern crate all_is_cubes;
 
 use all_is_cubes::apps::Tick;
-use all_is_cubes::cgmath::{EuclideanSpace, InnerSpace, Point3, Vector3};
-use all_is_cubes::content::UniverseTemplate;
-use all_is_cubes::math::FreeCoordinate;
+use all_is_cubes::cgmath::{InnerSpace, Point3, Vector3};
+use all_is_cubes::character::Character;
+use all_is_cubes::math::{Aab, FreeCoordinate, NotNan};
+use all_is_cubes::space::Space;
+use all_is_cubes::universe::Universe;
+use all_is_cubes::util::{ConciseDebug, CustomFormat};
 
 use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|input: ([FreeCoordinate; 3], [FreeCoordinate; 3])| {
-    let position: Point3<FreeCoordinate> = input.0.into();
-    let velocity: Vector3<FreeCoordinate> = input.1.into();
+fuzz_target!(|input: ([FreeCoordinate; 3], [FreeCoordinate; 3], Space)| {
+    let (position, velocity, space) = input;
+
+    let interesting_bounds_aab = Aab::from(space.grid()).enlarge(10.0);
+
     // TODO: write a proper Arbitrary impl on a wrapper
-    if !position.to_vec().magnitude2().is_finite() {
+    let position: Point3<FreeCoordinate> = position.into();
+    let velocity: Vector3<FreeCoordinate> = velocity.into();
+    if space.physics().gravity.map(NotNan::into_inner).magnitude() > 100. {
         return;
     }
-    if !velocity.magnitude2().is_finite() {
-        return;
+
+    println!(
+        "{} {}",
+        position.custom_format(ConciseDebug),
+        velocity.custom_format(ConciseDebug)
+    );
+
+    let mut universe = Universe::new();
+    let space_ref = universe.insert_anonymous(space);
+    let mut character = Character::spawn_default(space_ref);
+    character.body.position = position;
+    character.body.velocity = velocity;
+    for i in 0..5000 {
+        if !interesting_bounds_aab.contains(character.body.position) {
+            // Flying out of bounds is not interesting.
+            return;
+        }
+
+        // dbg!((i, character.body.position));
+        let (info, _tx) = character.step(None, Tick::arbitrary());
+        // dbg!(info);
+
+        // Check for no push out, but not on the first step, which might have been due to initial
+        // placement in a bad location.
+        if i != 0 {
+            assert_eq!(
+                info.expect("should be making body steps").push_out,
+                None,
+                "triggered push_out"
+            );
+        }
     }
-
-    // Use DemoCity as a complex world to exercise physics in.
-    // TODO: Instead, take fuzzer input data to build a just-complex-enough one.
-    let universe = UniverseTemplate::DemoCity.build().unwrap();
-    let character_ref = universe.get_default_character().unwrap();
-    character_ref
-        .try_modify(|character_mut| {
-            character_mut.body.position = position;
-            character_mut.body.velocity = velocity;
-
-            for i in 0..5000 {
-                let (info, _tx) = character_mut.step(None, Tick::arbitrary());
-
-                // Check for no push out, but not on the first step, which might have been due to initial
-                // placement in a bad location.
-                if i != 0 {
-                    assert_eq!(
-                        info.expect("should be making body steps").push_out,
-                        None,
-                        "triggered push_out"
-                    );
-                }
-            }
-        })
-        .unwrap();
 });
