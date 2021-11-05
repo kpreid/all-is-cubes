kpreid/all-is-cubes

commit 4b9e752eb60af825f0947772387d88a3cdb0d003

Add `EvaluatedBlock::voxel_opacity_mask`.

all-is-cubes/src/block.rs
@@ -12,7 +12,9 @@ use std::sync::Arc;
 use cgmath::{EuclideanSpace as _, Point3, Vector4, Zero as _};
 
 use crate::listen::{Gate, Listener, Notifier};
-use crate::math::{FreeCoordinate, GridCoordinate, GridPoint, GridRotation, Rgb, Rgba};
+use crate::math::{
+    FreeCoordinate, GridCoordinate, GridPoint, GridRotation, OpacityCategory, Rgb, Rgba,
+};
 use crate::raycast::{Ray, Raycaster};
 use crate::space::{Grid, GridArray, SetCubeError, Space, SpaceChange};
 use crate::transaction::{
@@ -174,6 +176,10 @@ impl Block {
                 resolution: 1,
                 opaque: color.fully_opaque(),
                 visible: !color.fully_transparent(),
+                voxel_opacity_mask: GridArray::from_elements(
+                    Grid::for_block(1),
+                    [color.opacity_category()],
+                ),
             }),
 
             &Block::Recur {
@@ -194,6 +200,7 @@ impl Block {
                         resolution: 1,
                         opaque: false,
                         visible: false,
+                        voxel_opacity_mask: None,
                     });
                 }
 
@@ -243,6 +250,9 @@ impl Block {
                         #[inline(always)]
                         |p| !voxels[p].color.fully_transparent(),
                     ),
+                    voxel_opacity_mask: Some(GridArray::from_fn(voxels.grid(), |p| {
+                        voxels[p].color.opacity_category()
+                    })),
 
                     voxels: Some(voxels),
                 })
@@ -609,6 +619,7 @@ pub const AIR_EVALUATED: EvaluatedBlock = EvaluatedBlock {
     resolution: 1,
     opaque: false,
     visible: false,
+    voxel_opacity_mask: None,
 };
 
 const AIR_ATTRIBUTES: BlockAttributes = BlockAttributes {
@@ -652,6 +663,10 @@ pub struct EvaluatedBlock {
     /// Whether the block has any voxels/color at all that make it visible; that is, this
     /// is false if the block is completely transparent.
     pub visible: bool,
+    /// The opacity of all voxels. This is redundant with the data  [`Self::voxels`],
+    /// and is provided as a pre-computed convenience that can be cheaply compared with
+    /// other values of the same type.
+    pub(crate) voxel_opacity_mask: Option<GridArray<OpacityCategory>>,
 }
 
 // TODO: Wait, this isn't really what ConciseDebug is for... shouldn't this be a regular impl Debug?
@@ -664,6 +679,7 @@ impl CustomFormat<ConciseDebug> for EvaluatedBlock {
             .field("visible", &self.visible)
             .field("resolution", &self.resolution)
             .field("voxels", &"...")
+            .field("voxel_opacity_mask", &"...")
             .finish()
     }
 }

all-is-cubes/src/block/tests.rs
@@ -12,7 +12,7 @@ use crate::block::{
 };
 use crate::content::{make_some_blocks, make_some_voxel_blocks};
 use crate::listen::{NullListener, Sink};
-use crate::math::{GridPoint, GridRotation, GridVector, Rgb, Rgba};
+use crate::math::{GridPoint, GridRotation, GridVector, OpacityCategory, Rgb, Rgba};
 use crate::space::{Grid, GridArray, Space, SpacePhysics, SpaceTransaction};
 use crate::universe::Universe;
 
@@ -34,6 +34,10 @@ fn evaluate_opaque_atom_and_attributes() {
     assert_eq!(e.resolution, 1);
     assert_eq!(e.opaque, true);
     assert_eq!(e.visible, true);
+    assert_eq!(
+        e.voxel_opacity_mask,
+        GridArray::from_elements(Grid::for_block(1), [OpacityCategory::Opaque])
+    )
 }
 
 #[test]
@@ -45,6 +49,10 @@ fn evaluate_transparent_atom() {
     assert!(e.voxels.is_none());
     assert_eq!(e.opaque, false);
     assert_eq!(e.visible, true);
+    assert_eq!(
+        e.voxel_opacity_mask,
+        GridArray::from_elements(Grid::for_block(1), [OpacityCategory::Partial])
+    )
 }
 
 #[test]
@@ -55,6 +63,10 @@ fn evaluate_invisible_atom() {
     assert!(e.voxels.is_none());
     assert_eq!(e.opaque, false);
     assert_eq!(e.visible, false);
+    assert_eq!(
+        e.voxel_opacity_mask,
+        GridArray::from_elements(Grid::for_block(1), [OpacityCategory::Invisible])
+    )
 }
 
 #[test]
@@ -92,6 +104,13 @@ fn evaluate_voxels_checked_individually() {
     assert_eq!(e.resolution, resolution);
     assert_eq!(e.opaque, true);
     assert_eq!(e.visible, true);
+    assert_eq!(
+        e.voxel_opacity_mask,
+        GridArray::from_elements(
+            Grid::for_block(resolution),
+            vec![OpacityCategory::Opaque; (resolution as usize).pow(3)]
+        )
+    )
 }
 
 #[test]

commit c0c938fe68cc05ec5a0214a73bc1a4e6e2a943ef

Fix `EvaluatedBlock::voxel_opacity_mask` not being rotated.
This bug was introduced in commit 4b9e752eb60af825f0947772387d88a3cdb0d003
and caused meshes to be reused incorrectly when a block was removed from
the space and then reinserted with a different rotation.

all-is-cubes/src/block.rs
@@ -265,20 +265,37 @@ impl Block {
             // TODO: this has no unit tests
             Block::Rotated(rotation, block) => {
                 let base = block.evaluate()?;
+                if base.voxels.is_none() && base.voxel_opacity_mask.is_none() {
+                    // Skip computation of transforms
+                    return Ok(base);
+                }
+
+                // TODO: Add a shuffle-in-place rotation operation to GridArray and try implementing this using that, which should have less arithmetic involved than these matrix ops
                 let resolution = base.resolution;
+                let inner_to_outer = rotation.to_positive_octant_matrix(resolution.into());
+                let outer_to_inner = rotation
+                    .inverse()
+                    .to_positive_octant_matrix(resolution.into());
+
                 Ok(EvaluatedBlock {
                     voxels: base.voxels.map(|voxels| {
-                        let inner_to_outer = rotation.to_positive_octant_matrix(resolution.into());
-                        let outer_to_inner = rotation
-                            .inverse()
-                            .to_positive_octant_matrix(resolution.into());
-                        // TODO: Add a shuffle-in-place rotation operation to GridArray and try implementing this using that, which should have less unnecessary arithmetic
                         GridArray::from_fn(
                             voxels.grid().transform(inner_to_outer).unwrap(),
                             |cube| voxels[outer_to_inner.transform_cube(cube)],
                         )
                     }),
-                    ..base
+                    voxel_opacity_mask: base.voxel_opacity_mask.map(|mask| {
+                        GridArray::from_fn(mask.grid().transform(inner_to_outer).unwrap(), |cube| {
+                            mask[outer_to_inner.transform_cube(cube)]
+                        })
+                    }),
+
+                    // Unaffected
+                    attributes: base.attributes,
+                    color: base.color,
+                    resolution,
+                    opaque: base.opaque,
+                    visible: base.visible,
                 })
             }
         }

all-is-cubes/src/block/tests.rs
@@ -3,12 +3,14 @@
 
 #![allow(clippy::bool_assert_comparison)]
 
-use cgmath::EuclideanSpace as _;
 use std::borrow::Cow;
+
+use cgmath::EuclideanSpace as _;
+use pretty_assertions::assert_eq;
 
 use crate::block::{
     builder, AnimationHint, Block, BlockAttributes, BlockBuilder, BlockCollision, BlockDef,
-    BlockDefTransaction, EvalBlockError, Evoxel, Resolution, AIR, AIR_EVALUATED,
+    BlockDefTransaction, EvalBlockError, EvaluatedBlock, Evoxel, Resolution, AIR, AIR_EVALUATED,
 };
 use crate::content::{make_some_blocks, make_some_voxel_blocks};
 use crate::listen::{NullListener, Sink};
@@ -218,6 +220,59 @@ fn evaluate_voxels_zero_resolution() {
     assert_eq!(e.resolution, 1);
     assert_eq!(e.opaque, false);
     assert_eq!(e.visible, false);
+}
+
+// Unlike other tests, this one asserts the entire `EvaluatedBlock` value because
+// a new field is a potential bug.
+#[test]
+fn evaluate_rotated() {
+    let resolution = 2;
+    let block_grid = Grid::for_block(resolution);
+    let rotation = GridRotation::RYXZ;
+    let mut universe = Universe::new();
+    let color_fn = |cube: GridPoint| {
+        Rgba::new(
+            cube.x as f32,
+            cube.y as f32,
+            cube.z as f32,
+            if cube.y == 0 { 1.0 } else { 0.0 },
+        )
+    };
+    let rotated_color_fn = |cube: GridPoint| {
+        color_fn(rotation.to_positive_octant_matrix(resolution.into()).transform_cube(cube))
+    };
+    let block = Block::builder()
+        .voxels_fn(&mut universe, resolution, |cube| {
+            // Construct a lower half block with all voxels distinct
+            Block::from(color_fn(cube))
+        })
+        .unwrap()
+        .build();
+    let rotated = block.clone().rotate(rotation);
+    assert_eq!(
+        rotated.evaluate().unwrap(),
+        EvaluatedBlock {
+            attributes: BlockAttributes::default(),
+            color: rgba_const!(0.5, 0.5, 0.5, 0.5),
+            voxels: Some(GridArray::from_fn(block_grid, |cube| {
+                Evoxel {
+                    color: rotated_color_fn(cube),
+                    selectable: true,
+                    collision: BlockCollision::Hard,
+                }
+            })),
+            resolution: 2,
+            opaque: false,
+            visible: true,
+            voxel_opacity_mask: Some(GridArray::from_fn(block_grid, |cube| {
+                if cube.x == 0 {
+                    OpacityCategory::Opaque
+                } else {
+                    OpacityCategory::Invisible
+                }
+            })),
+        }
+    );
 }
 
 /// Tests that the `offset` field of `Block::Recur` is respected.
@@ -594,6 +649,7 @@ mod txn {
     use super::*;
     use crate::block::BlockDefTransaction;
     use crate::transaction::{Merge, TransactionTester};
+    use pretty_assertions::assert_eq;
 
     #[test]
     fn causes_notification() {