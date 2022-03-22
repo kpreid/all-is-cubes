b51e2f6e3d2b57ee6d294aa79ccde53cc15967df
Kevin Reid <kpreid@switchb.org>
2022-03-21 20:04:57 -0700

    Add `impl Default for Layers`.

--- all-is-cubes/src/apps.rs
+++ all-is-cubes/src/apps.rs
@@ -354,7 +354,7 @@ impl AllIsCubesAppState {
 // Exhaustive: Changing this will probably be breaking anyway, until we make it a
 // more thorough abstraction.
 #[allow(clippy::exhaustive_structs)]
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+#[derive(Clone, Copy, Debug, Default, Eq, Hash, PartialEq)]
 pub struct Layers<T> {
     pub world: T,
     pub ui: T,
