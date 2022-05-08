6ad05c712f04eede4d1f5f06ab9e94e46aa126c6
Kevin Reid <kpreid@switchb.org>
2022-05-08 15:49:57 -0700

    fuzz_raycast: Add detailed printing option.

--- fuzz/fuzz_targets/fuzz_raycast.rs
+++ fuzz/fuzz_targets/fuzz_raycast.rs
@@ -5,17 +5,31 @@ extern crate all_is_cubes;
 use all_is_cubes::raycast::Raycaster;
 use all_is_cubes::space::Grid;
 
+const PRINT: bool = false;
+
 fuzz_target!(|input: (bool, [f64; 3], [f64; 3])| {
+    if PRINT {
+        println!("Inputs: {input:?}");
+    }
     let (use_grid, origin, direction) = input;
 
-    if use_grid {
-        let grid = Grid::from_lower_upper([-10, -20, -30], [10, 20, 30]);
-        for step in Raycaster::new(origin, direction).within_grid(grid).take(10) {
-            assert!(grid.contains_cube(step.cube_ahead()));
-        }
+    let grid = Grid::from_lower_upper([-10, -20, -30], [10, 20, 30]);
+    let raycaster = if use_grid {
+        Raycaster::new(origin, direction).within_grid(grid)
     } else {
-        for _step in Raycaster::new(origin, direction).take(10) {
-            // println!("{:?}", step);
+        Raycaster::new(origin, direction)
+    };
+
+    if PRINT {
+        dbg!(&raycaster);
+    }
+
+    for step in raycaster.take(10) {
+        if PRINT {
+            println!("Step: {step:?}");
+        }
+        if use_grid {
+            assert!(grid.contains_cube(step.cube_ahead()));
         }
     }
 });
