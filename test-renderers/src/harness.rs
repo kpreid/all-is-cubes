ac683da3e92c1184ab211aa2e84351da586d3344
Kevin Reid <kpreid@switchb.org>
2022-05-10 20:08:41 -0700

    Run renderer tests in parallel.

--- test-renderers/src/harness.rs
+++ test-renderers/src/harness.rs
@@ -11,6 +11,7 @@ use std::time::{Duration, Instant};
 use all_is_cubes::util::{CustomFormat as _, StatusText};
 use async_fn_traits::{AsyncFn0, AsyncFn1, AsyncFn2};
 use futures_core::future::BoxFuture;
+use tokio::task::JoinHandle;
 
 use crate::{
     results_json_path, write_report_file, ComparisonRecord, HeadlessRenderer, Overlays,
@@ -60,17 +61,54 @@ impl RenderTestContext {
 /// `factory_factory` is a function to be called once per each test which returns a
 /// [`RendererFactory`] for the type of renderer under test, which should be as isolated
 /// as is reasonable for testing.
-pub async fn harness_main<Factory>(
+pub async fn harness_main<Factory, Ff>(
     renderer_id: RendererId,
     test_suite: fn(&mut TestCaseCollector<'_>),
-    factory_factory: impl AsyncFn0<Output = Factory>, // TODO: better name
+    factory_factory: Ff, // TODO: better name
 ) -> Result<(), ()>
 where
     Factory: RendererFactory + 'static,
+    Ff: AsyncFn0<Output = Factory> + Send + Sync + 'static,
+    Ff::OutputFuture: Send,
 {
     // Gather tests (don't run them yet).
     let mut test_table: BTreeMap<String, BoxedTest> = BTreeMap::new();
     test_suite(&mut TestCaseCollector(&mut test_table));
+
+    // Start the tests, in parallel.
+    // TODO: When we have more tests we might benefit from concurrency limits.
+    let suite_start_time = Instant::now();
+    let test_handles: BTreeMap<String, RunningTest> = test_table
+        .into_iter()
+        .map(|(name, test_function)| {
+            let comparison_log: Arc<Mutex<Vec<ComparisonRecord>>> = Default::default();
+
+            let handle = {
+                let test_id = name.clone();
+                let comparison_log = comparison_log.clone();
+                let factory_future = factory_factory();
+                tokio::spawn(async move {
+                    let context = RenderTestContext {
+                        test_id,
+                        renderer_factory: Box::new(factory_future.await),
+                        comparison_log: comparison_log.clone(),
+                    };
+
+                    let case_start_time = Instant::now();
+                    test_function(context).await;
+                    case_start_time.elapsed()
+                })
+            };
+
+            (
+                name,
+                RunningTest {
+                    comparison_log,
+                    handle,
+                },
+            )
+        })
+        .collect();
 
     let mut logging = io::stderr();
     let mut per_test_output = BTreeMap::new();
@@ -78,26 +116,13 @@ where
     let mut count_failed = 0;
     let mut cumulative_time = Duration::ZERO;
 
-    let suite_start_time = Instant::now();
+    // Collect results.
     writeln!(logging).unwrap();
-    for (name, test_function) in test_table {
+    for (name, running_test) in test_handles {
         write!(logging, "test {name:20} ...").unwrap();
         logging.flush().unwrap();
 
-        let comparison_log: Arc<Mutex<Vec<ComparisonRecord>>> = Default::default();
-        let context = RenderTestContext {
-            test_id: name.clone(),
-            renderer_factory: Box::new(factory_factory().await),
-            comparison_log: comparison_log.clone(),
-        };
-
-        let join_handle = tokio::spawn(async move {
-            let case_start_time = Instant::now();
-            test_function(context).await;
-            case_start_time.elapsed()
-        });
-        // TODO: add concurrency of tests by not immediately awaiting
-        let passed = match join_handle.await {
+        let passed = match running_test.handle.await {
             Ok(case_time) => {
                 writeln!(logging, " ok in {}", case_time.custom_format(StatusText)).unwrap();
                 count_passed += 1;
@@ -120,7 +145,7 @@ where
             TestCaseOutput {
                 passed,
                 test_id: name,
-                comparisons: Arc::try_unwrap(comparison_log)
+                comparisons: Arc::try_unwrap(running_test.comparison_log)
                     .expect("somebody hung onto the log")
                     .into_inner()
                     .unwrap(),
@@ -203,4 +228,10 @@ impl<'a> TestCaseCollector<'a> {
             });
         }
     }
+}
+
+/// Handle to a test that might be still running.
+struct RunningTest {
+    handle: JoinHandle<Duration>,
+    comparison_log: Arc<Mutex<Vec<ComparisonRecord>>>,
 }
