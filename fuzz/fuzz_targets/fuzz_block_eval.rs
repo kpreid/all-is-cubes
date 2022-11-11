13ea9531dde71c96474b1b637db0a4ee55d3fc8c
Kevin Reid <kpreid@switchb.org>
2022-11-10 19:24:11 -0800

    Use `impl Arbitrary for Block` in `fuzz_block_eval`.
    This requires some additional care now that it is possible that the
    generated test data *should* fail to evaluate.

--- fuzz/fuzz_targets/fuzz_block_eval.rs
+++ fuzz/fuzz_targets/fuzz_block_eval.rs
@@ -2,40 +2,32 @@
 use libfuzzer_sys::fuzz_target;
 extern crate all_is_cubes;
 
-use all_is_cubes::block::{Block, BlockAttributes, Modifier, Resolution};
-use all_is_cubes::math::Rgba;
-use all_is_cubes::space::Space;
-use all_is_cubes::universe::Universe;
+use all_is_cubes::block::Block;
 
-fuzz_target!(|input: (
-    BlockAttributes,
-    Rgba,
-    Vec<Modifier>,
-    Option<(Resolution, Space)>
-)| {
-    let (attributes, color, modifiers, voxels) = input;
+fuzz_target!(|block: Block| {
+    // TODO: The `Block` will have pending URefs (not inserted in a Universe).
+    // Put them in a universe once that is possible.
 
-    let mut universe = Universe::new();
-
-    let builder = Block::builder().attributes(attributes);
-    let mut block = if let Some((resolution, space)) = voxels {
-        builder
-            .voxels_ref(resolution, universe.insert_anonymous(space))
-            .build()
-    } else {
-        builder.color(color).build()
-    };
-    block.modifiers_mut().extend(modifiers);
-
-    // Currently, no evaluation possible from this fuzz input should ever fail.
-    // (That might change in the future.)
-    let evaluated = block.evaluate().unwrap();
-
-    evaluated.consistency_check();
-
-    // Exercise listen(), because it is a recursive operation on the block + modifiers.
     let sink = all_is_cubes::listen::Sink::new();
-    block.listen(sink.listener()).unwrap();
+
+    match (block.evaluate(), block.listen(sink.listener())) {
+        (Ok(evaluated), Ok(())) => {
+            evaluated.consistency_check();
+        }
+        (Err(_), Err(_) | Ok(())) => {
+            // Errors are an expected possibility; this fuzz test is looking for no panic.
+            //
+            // It should never be the case that listen() fails when evaluate() succeeds.
+            // (The case when evaluate() only fails is when a `BlockDef` has a broken ref
+            // inside it.)
+
+            // TODO: something like assert_eq!(eval_err, listen_err)
+            // (but for now there are differences in _which_ URef is checked first)
+        }
+        (eval_result, listen_result) => {
+            panic!("Bad outcome:\neval {eval_result:?}\nlisten {listen_result:?}");
+        }
+    }
 
     // Exercise visit_refs(), because it is a recursive operation on the block + modifiers.
     // TODO: To do this we need a handy visitor, but I think it should be simplified...
