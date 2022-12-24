f8f1f44c4ae62c2dcec4e242b59aa86b5ee53e9b
Kevin Reid <kpreid@switchb.org>
2022-12-23 21:23:35 -0800

    Log overly-long gaps in `YieldProgress` calls.
    Note that this is not really an accurate detection of undesired pauses,
    In particular, if the code calls other code that does not interact with
    `YieldProgress` but *does* yield (e.g. because it is an IO operation),
    then the `YieldProgress` will observe a gap in yielding. In order to be
    accurate, we would need to use a background task that detects when it is
    prevented from running.
    
    But, for now, this will be a useful tool since the current uses of
    `YieldProgress` are 100% compute, and it also detects the situation
    where the task is proceeding but not _reporting_ that is proceeding
    (though that should in principle have a considerably longer timeout than
    for yielding).

--- all-is-cubes/src/util/yield_progress.rs
+++ all-is-cubes/src/util/yield_progress.rs
@@ -1,20 +1,43 @@
 use std::fmt;
-use std::sync::Arc;
+use std::panic::Location;
+use std::sync::{Arc, Mutex};
 
 use futures_core::future::{BoxFuture, Future};
+use instant::{Duration, Instant};
 
-/// Allows a long-running async task to report its progress, and to yield to the
-/// scheduler if appropriate in the current environment (i.e. web).
+/// Allows a long-running async task to report its progress, while also yielding to the
+/// scheduler (e.g. for single-threaded web environment) and introducing cancellation
+/// points.
 ///
-/// * TODO: This might end up being converted to a trait to allow `Sync` depending
-///   on the executor's choice.
-/// * TODO: We should probably have a non-async interface for progress reporting
-///   once that's built.
+/// These go together because the rate at which it makes sense to yield (to avoid event
+/// loop hangs) is similar to the rate at which it makes sense to report progress.
+///
+/// Note that while a [`YieldProgress`] is `Send` and `Sync`, it does not currently
+/// support meaningfully being used from multiple threads or futures at once — only
+/// reporting the progress of, and yielding periodically within, a fully sequential
+/// operation. This might change in the future, but for now, it will just output
+/// inconsistent results if you try to use it otherwise.
 pub struct YieldProgress {
     start: f32,
     end: f32,
-    yielder: Arc<dyn Fn() -> BoxFuture<'static, ()> + Send + Sync>,
+    // common: Arc<Mutex<YpCommon>>,
+    yielding: Arc<Yielding<dyn Fn() -> BoxFuture<'static, ()> + Send + Sync>>,
     progressor: Arc<dyn Fn(f32) + Send + Sync>,
+}
+
+/// Piggyback on the `Arc` we need to store the `dyn Fn` anyway to also store some state.
+struct Yielding<F: ?Sized> {
+    state: Mutex<YieldState>,
+
+    yielder: F,
+}
+
+#[derive(Clone, Copy)]
+struct YieldState {
+    /// The most recent instant at which `yielder`'s future completed.
+    /// Used to detect overlong time periods between yields.
+    last_finished_yielding: Instant,
+    last_yield_location: &'static Location<'static>,
 }
 
 impl fmt::Debug for YieldProgress {
@@ -27,16 +50,30 @@ impl fmt::Debug for YieldProgress {
 }
 
 impl YieldProgress {
+    /// Construct a new [`YieldProgress`], which will call `yielder` to yield and
+    /// `progressor` to report progress.
+    ///
+    /// Note that it measures time intervals between yields starting from when this
+    /// function is called, as if this is the first yield.
+    #[track_caller]
     pub fn new<Y, YFut, P>(yielder: Y, progressor: P) -> Self
     where
         Y: Fn() -> YFut + Send + Sync + 'static,
         YFut: Future<Output = ()> + Send + 'static,
         P: Fn(f32) + Send + Sync + 'static,
     {
+        let yielding: Arc<Yielding<_>> = Arc::new(Yielding {
+            state: Mutex::new(YieldState {
+                last_finished_yielding: Instant::now(),
+                last_yield_location: Location::caller(),
+            }),
+            yielder: move || -> BoxFuture<'static, ()> { Box::pin(yielder()) },
+        });
+
         Self {
             start: 0.0,
             end: 1.0,
-            yielder: Arc::new(move || Box::pin(yielder())),
+            yielding,
             progressor: Arc::new(progressor),
         }
     }
@@ -61,24 +98,34 @@ impl YieldProgress {
     }
 
     /// Report the current amount of progress (a number from 0 to 1) and yield.
-    pub async fn progress(&self, progress_fraction: f32) {
+    #[track_caller] // This is not an `async fn` because `track_caller` is not compatible
+    pub fn progress(&self, progress_fraction: f32) -> impl Future<Output = ()> + Send + 'static {
+        let location = Location::caller();
         (self.progressor)(self.point_in_range(progress_fraction));
-        (self.yielder)().await;
+
+        self.yielding.clone().yield_only(location)
     }
 
     /// Report that the given amount of progress has been made, then return
     /// a [`YieldProgress`] covering the remaining range.
-    pub async fn finish_and_cut(self, progress_fraction: f32) -> Self {
+    #[track_caller] // This is not an `async fn` because `track_caller` is not compatible
+    pub fn finish_and_cut(
+        self,
+        progress_fraction: f32,
+    ) -> impl Future<Output = Self> + Send + 'static {
         let [a, b] = self.split(progress_fraction);
-        a.progress(1.0).await;
-        b
+        let progress_future = a.progress(1.0);
+        async move {
+            progress_future.await;
+            b
+        }
     }
 
     fn with_new_range(&self, start: f32, end: f32) -> Self {
         Self {
             start,
             end,
-            yielder: Arc::clone(&self.yielder),
+            yielding: Arc::clone(&self.yielding),
             progressor: Arc::clone(&self.progressor),
         }
     }
@@ -104,6 +151,37 @@ impl YieldProgress {
                 self.point_in_range((index + 1) as f32 / count as f32),
             )
         })
+    }
+}
+
+impl<F: ?Sized + Fn() -> BoxFuture<'static, ()> + Send + Sync> Yielding<F> {
+    async fn yield_only(self: Arc<Self>, location: &'static Location<'static>) {
+        // Note that we avoid holding the lock while calling yielder().
+        // The worst outcome of an inconsistency is that we will output a meaningless
+        // "between {location} and {location} message", but none should occur because
+        // [`YieldProgress`] is intended to be used in a sequential manner.
+        let previous_state: YieldState = { *self.state.lock().unwrap() };
+
+        let delta = Instant::now().duration_since(previous_state.last_finished_yielding);
+        if delta > Duration::from_millis(100) {
+            log::trace!(
+                "Yielding after {delta} ms between {old_location} and {new_location}",
+                delta = delta.as_millis(),
+                old_location = previous_state.last_yield_location,
+                new_location = location,
+            );
+        }
+
+        // TODO: Since we're tracking time, we might as well decide whether to not bother
+        // yielding if it has been a short time ... except that different yielders might
+        // want different granularities/policies.
+        (self.yielder)().await;
+
+        {
+            let mut state = self.state.lock().unwrap();
+            state.last_finished_yielding = Instant::now();
+            state.last_yield_location = location;
+        }
     }
 }
 
