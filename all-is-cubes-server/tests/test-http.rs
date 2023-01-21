8f08c58f28a154cff98c07a73459b3e52467fd67
Kevin Reid <kpreid@switchb.org>
2023-01-20 19:55:09 -0800

    Join URLs properly in `test-http`.
    Previously the test was emitting `http://<addr>//style.css`,
    which axum 0.6 didn't route as pseudo-intended any more.
    
    (I forget what the URL/HTTP RFCs have to say about double slashes
    being equivalent or not.)

--- all-is-cubes-server/tests/test-http.rs
+++ all-is-cubes-server/tests/test-http.rs
@@ -4,9 +4,10 @@ use std::process::Stdio;
 
 use async_fn_traits::AsyncFnOnce1;
 use reqwest::header::HeaderValue;
+use reqwest::Url;
 use tokio::io::AsyncBufReadExt;
 
-async fn with_server<F: AsyncFnOnce1<String, Output = ()>>(f: F) {
+async fn with_server<F: AsyncFnOnce1<Url, Output = ()>>(f: F) {
     let mut server = tokio::process::Command::new(env!("CARGO_BIN_EXE_aic-server"))
         .kill_on_drop(true)
         .stdout(Stdio::piped())
@@ -22,8 +23,9 @@ async fn with_server<F: AsyncFnOnce1<String, Output = ()>>(f: F) {
         server_url_line.starts_with("http"),
         "Expected URL: {server_url_line:?}"
     );
+    let url = Url::parse(server_url_line.trim()).expect("parsing server URL failed");
 
-    f(server_url_line.trim().to_owned()).await;
+    f(url).await;
 
     server.kill().await.unwrap()
 }
@@ -32,8 +34,8 @@ async fn with_server<F: AsyncFnOnce1<String, Output = ()>>(f: F) {
 /// (Or if we change the URL layout.)
 #[tokio::test]
 async fn server_static_files_smoke_test() {
-    with_server(|root_url| async move {
-        let resp = reqwest::get(format!("{root_url}/style.css")) // TODO: use a url resolver library
+    with_server(|root_url: Url| async move {
+        let resp = reqwest::get(root_url.join("style.css").unwrap())
             .await
             .unwrap()
             .error_for_status()
