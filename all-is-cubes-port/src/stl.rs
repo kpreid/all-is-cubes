0966de17e51f01d25fe561ec4c66d34979f99e8e
Kevin Reid <kpreid@switchb.org>
2023-07-03 10:24:17 -0700

    [unstable-rust] mesh: Use `!` type in place of `NoTexture`.

--- all-is-cubes-port/src/stl.rs
+++ all-is-cubes-port/src/stl.rs
@@ -12,11 +12,7 @@ use all_is_cubes::math::{notnan, Cube, FreeCoordinate};
 use all_is_cubes::space::Space;
 use all_is_cubes::universe::PartialUniverse;
 use all_is_cubes::util::YieldProgress;
-use all_is_cubes_mesh::{
-    self as mesh,
-    texture::{NoTexture, NoTextures},
-    BlockVertex,
-};
+use all_is_cubes_mesh::{self as mesh, texture::NoTextures, BlockVertex};
 
 pub(crate) async fn export_stl(
     progress: YieldProgress,
@@ -115,9 +111,9 @@ enum StlMt {}
 
 impl mesh::MeshTypes for StlMt {
     // TODO: It'd be more efficient to use a custom vertex type but we're not bothering for now.
-    type Vertex = BlockVertex<NoTexture>;
+    type Vertex = BlockVertex<!>;
     type Alloc = NoTextures;
-    type Tile = NoTexture;
+    type Tile = !;
 }
 
 #[cfg(test)]
