e5de7ad8ef8c45f44fcefdd097e62563bc6d383b
Kevin Reid <kpreid@switchb.org>
2023-10-26 13:02:22 -0700

    desktop: Fix end-to-end tests being environment sensitive.

--- all-is-cubes-desktop/tests/end-to-end.rs
+++ all-is-cubes-desktop/tests/end-to-end.rs
@@ -2,6 +2,11 @@
 fn trycmd_tests() {
     let tc = trycmd::TestCases::new();
 
+    // Prevent `anyhow` from printing backtraces if it otherwise would due to env having
+    // "RUST_BACKTRACE=1", because they'd cause our error output tests to fail.
+    // Docs: https://docs.rs/anyhow/1.0.75/anyhow/index.html#details
+    tc.env("RUST_LIB_BACKTRACE", "0");
+
     tc.insert_var("[DESKTOPVERSION]", env!("CARGO_PKG_VERSION"))
         .unwrap();
     tc.case("tests/end-to-end/*.toml");
