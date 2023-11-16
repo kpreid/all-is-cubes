692547a210a07723d984e0290b182349a42c02e3
Kevin Reid <kpreid@switchb.org>
2023-11-16 10:36:48 -0800

    Rust 1.74: Move common lint configuration from attributes to Cargo.toml.
    This makes use of the new Cargo-based lint configuration in Rust 1.74.0.
    This does not cause a package MSRV bump because old Cargo will ignore
    the configuration.
    
    Note the “lenience for tests” section is also gone; it does not seem to
    be necessary any more.
    
    Advantages:
    * Single configuration location; fewer things to carefully edit.
    * Automatically applies to all targets in all packages, not only those
      we paste the config block into the root file of.
    
    Disadvantages:
    * View-library-source doesn't instantly reveal what lints we obey.
    
    Future work:
    * We may want to enable some lints globally that we did not before,
      and then only disable them per crate.

--- all-is-cubes-server/src/lib.rs
+++ all-is-cubes-server/src/lib.rs
@@ -1,38 +1,6 @@
 //! Web server and game server (multiplayer/server-side storage) for All is Cubes.
 
-// Basic lint settings, which should be identical across all all-is-cubes project crates.
-// This list is sorted.
-#![allow(clippy::collapsible_else_if)]
-#![allow(clippy::collapsible_if)]
-#![allow(clippy::needless_update)]
-#![allow(clippy::single_match)]
-#![deny(rust_2018_idioms)]
-#![deny(unsafe_op_in_unsafe_fn)]
-#![warn(clippy::cast_lossless)]
-#![warn(clippy::doc_markdown)]
-#![warn(clippy::exhaustive_enums)]
-#![warn(clippy::exhaustive_structs)]
-#![warn(clippy::modulo_arithmetic)]
-#![warn(clippy::return_self_not_must_use)]
-#![warn(clippy::undocumented_unsafe_blocks)]
-#![warn(clippy::uninlined_format_args)]
-#![warn(clippy::unnecessary_self_imports)]
-#![warn(clippy::wrong_self_convention)]
-#![warn(explicit_outlives_requirements)]
-#![warn(missing_debug_implementations)]
-#![warn(noop_method_call)]
-#![warn(trivial_numeric_casts)]
-#![warn(unused_extern_crates)]
-#![warn(unused_lifetimes)]
-#![warn(unused_qualifications)]
-// Lenience for tests.
-#![cfg_attr(test,
-    allow(clippy::float_cmp), // deterministic tests
-    allow(clippy::redundant_clone), // prefer regularity over efficiency
-)]
-// #![warn(unused_crate_dependencies)]  // noisy for dev-dependencies; enable sometimes for review
-
-// Crate-specific lint settings.
+// Crate-specific lint settings. (General settings can be found in the workspace manifest.)
 #![forbid(unsafe_code)]
 #![warn(missing_docs)]
 
--- tools/xtask/src/xtask.rs
+++ tools/xtask/src/xtask.rs
@@ -13,38 +13,7 @@
 // TODO: Make this independent of the current directory so that it will never do surprising
 // things to the current directory.
 
-// Basic lint settings, which should be identical across all all-is-cubes project crates.
-// This list is sorted.
-#![allow(clippy::collapsible_else_if)]
-#![allow(clippy::collapsible_if)]
-#![allow(clippy::needless_update)]
-#![allow(clippy::single_match)]
-#![deny(rust_2018_idioms)]
-#![deny(unsafe_op_in_unsafe_fn)]
-#![warn(clippy::cast_lossless)]
-#![warn(clippy::doc_markdown)]
-#![warn(clippy::exhaustive_enums)]
-#![warn(clippy::exhaustive_structs)]
-#![warn(clippy::modulo_arithmetic)]
-#![warn(clippy::return_self_not_must_use)]
-#![warn(clippy::undocumented_unsafe_blocks)]
-#![warn(clippy::unnecessary_self_imports)]
-#![warn(clippy::wrong_self_convention)]
-#![warn(explicit_outlives_requirements)]
-#![warn(missing_debug_implementations)]
-#![warn(noop_method_call)]
-#![warn(trivial_numeric_casts)]
-#![warn(unused_extern_crates)]
-#![warn(unused_lifetimes)]
-#![warn(unused_qualifications)]
-// Lenience for tests.
-#![cfg_attr(test,
-    allow(clippy::float_cmp), // deterministic tests
-    allow(clippy::redundant_clone), // prefer regularity over efficiency
-)]
-// #![warn(unused_crate_dependencies)]  // noisy for dev-dependencies; enable sometimes for review
-
-// Crate-specific lint settings.
+// Crate-specific lint settings. (General settings can be found in the workspace manifest.)
 #![forbid(unsafe_code)]
 
 use std::collections::BTreeSet;
