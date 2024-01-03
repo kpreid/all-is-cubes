1477d1dd238e6673edb10b3d1a9e1211dea0bdc9
Kevin Reid <kpreid@switchb.org>
2024-01-03 14:58:53 -0800

    ui: Define button sets for non-toggle graphics options (disabled).
    Needs better layout, both in the button labels and of the overall
    dialog.

--- all-is-cubes-ui/src/ui_content/options.rs
+++ all-is-cubes-ui/src/ui_content/options.rs
@@ -1,11 +1,14 @@
-use all_is_cubes::arcstr::literal;
 use alloc::sync::Arc;
+use core::fmt;
 
-use all_is_cubes::camera::{AntialiasingOption, GraphicsOptions};
+use all_is_cubes::arcstr::{self, literal};
+use all_is_cubes::camera::{self, AntialiasingOption, GraphicsOptions};
+use all_is_cubes::math::Face6;
+use all_is_cubes::notnan;
 
 use crate::apps::ControlMessage;
 use crate::ui_content::hud::HudInputs;
-use crate::vui::{widgets, LayoutTree, UiBlocks, Widget, WidgetTree};
+use crate::vui::{self, widgets, LayoutTree, UiBlocks, Widget, WidgetTree};
 
 #[derive(Clone, Copy, Debug)]
 pub(crate) enum OptionsStyle {
@@ -32,10 +35,50 @@ pub(crate) fn graphics_options_widgets(
             },
         )));
     }
+    // TODO: Improve labeled button layout and enable these
+    if false {
+        w.extend([
+            graphics_enum_button(
+                hud_inputs,
+                style,
+                |g| &g.fog,
+                |g, v| g.fog = v,
+                // TODO: put these lists somewhere that is not individual UI code
+                [
+                    camera::FogOption::None,
+                    camera::FogOption::Abrupt,
+                    camera::FogOption::Compromise,
+                    camera::FogOption::Physical,
+                ],
+            ),
+            graphics_enum_button(
+                hud_inputs,
+                style,
+                |g| &g.lighting_display,
+                |g, v| g.lighting_display = v,
+                [
+                    camera::LightingOption::None,
+                    camera::LightingOption::Flat,
+                    camera::LightingOption::Smooth,
+                ],
+            ),
+            graphics_enum_button(
+                hud_inputs,
+                style,
+                |g| &g.transparency,
+                |g, v| g.transparency = v,
+                [
+                    camera::TransparencyOption::Surface,
+                    camera::TransparencyOption::Volumetric,
+                    camera::TransparencyOption::Threshold(notnan!(0.5)),
+                ],
+            ),
+        ]);
+    }
     w.extend([
-        // TODO: this needs to be a different kind of button for the multiple states. But
-        // for now, while we have only small interactive controls and the IfCheap option
-        // is just conditional on the renderer type, there's no reason to select IfCheap.
+        // TODO: this needs to be an enum button set for the multiple states, in principle. But
+        // for now, while we aren't actually saving the options, there's no reason to select
+        // IfCheap.
         graphics_toggle_button(
             hud_inputs,
             style,
@@ -94,8 +137,8 @@ fn graphics_toggle_button(
     hud_inputs: &HudInputs,
     style: OptionsStyle,
     icon_key: UiBlocks,
-    getter: fn(&GraphicsOptions) -> bool,
-    setter: fn(&mut GraphicsOptions, bool),
+    getter: impl Fn(&GraphicsOptions) -> bool + Clone + Send + Sync + 'static,
+    setter: impl Fn(&mut GraphicsOptions, bool) + Clone + Send + Sync + 'static,
 ) -> WidgetTree {
     let icon = hud_inputs.hud_blocks.ui_blocks[icon_key].clone();
     let text: Option<widgets::Label> = match style {
@@ -115,12 +158,14 @@ fn graphics_toggle_button(
     };
     let button: Arc<dyn Widget> = widgets::ToggleButton::new(
         hud_inputs.graphics_options.clone(),
-        getter,
+        getter.clone(),
         label,
         &hud_inputs.hud_blocks.widget_theme,
         {
             let cc = hud_inputs.app_control_channel.clone();
             move || {
+                let getter = getter.clone();
+                let setter = setter.clone();
                 let _ignore_errors = cc.send(ControlMessage::ModifyGraphicsOptions(Box::new(
                     move |mut g| {
                         let mg = Arc::make_mut(&mut g);
@@ -132,6 +177,51 @@ fn graphics_toggle_button(
         },
     );
     LayoutTree::leaf(button)
+}
+
+/// Generate a group of buttons that selects one of an enum of options.
+///
+/// These buttons do not show up in the compact style.
+fn graphics_enum_button<T: Clone + fmt::Debug + PartialEq + Send + Sync + 'static>(
+    hud_inputs: &HudInputs,
+    style: OptionsStyle,
+    getter: fn(&GraphicsOptions) -> &T,
+    setter: fn(&mut GraphicsOptions, T),
+    list: impl IntoIterator<Item = T>,
+) -> WidgetTree {
+    match style {
+        OptionsStyle::CompactRow => LayoutTree::spacer(vui::LayoutRequest::EMPTY),
+        OptionsStyle::LabeledColumn => Arc::new(LayoutTree::Stack {
+            direction: Face6::PX,
+            children: list
+                .into_iter()
+                .map(|value| {
+                    let value2 = value.clone();
+                    let button: Arc<dyn Widget> = widgets::ToggleButton::new(
+                        hud_inputs.graphics_options.clone(),
+                        move |options| *getter(options) == value,
+                        arcstr::format!("{:?}", value2), // TODO: quick kludge; need real labels
+                        &hud_inputs.hud_blocks.widget_theme,
+                        {
+                            let cc = hud_inputs.app_control_channel.clone();
+                            move || {
+                                let value = value2.clone();
+                                let _ignore_errors =
+                                    cc.send(ControlMessage::ModifyGraphicsOptions(Box::new(
+                                        move |mut g| {
+                                            let mg = Arc::make_mut(&mut g);
+                                            setter(mg, value);
+                                            g
+                                        },
+                                    )));
+                            }
+                        },
+                    );
+                    LayoutTree::leaf(button)
+                })
+                .collect(),
+        }),
+    }
 }
 
 pub(crate) fn pause_toggle_button(hud_inputs: &HudInputs, style: OptionsStyle) -> Arc<dyn Widget> {
