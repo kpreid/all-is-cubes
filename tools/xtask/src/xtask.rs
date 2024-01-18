1a0366998d0754fdd560934a8a7e4250766f1a2c
Kevin Reid <kpreid@switchb.org>
2024-01-17 17:35:05 -0800

    xtask: Temporarily use `cargo doc --no-deps`.
    This works around a rustdoc bug provoked by wgpu-core 0.19.0.
    This commit should be reverted when Rust 1.76 is released.

--- tools/xtask/src/xtask.rs
+++ tools/xtask/src/xtask.rs
@@ -190,7 +190,10 @@ fn main() -> Result<(), ActionError> {
             // libraries with docs elsewhere.
             if config.scope.includes_main_workspace() {
                 let _t = CaptureTime::new(&mut time_log, "doc");
-                cargo().arg("doc").run()?;
+                // TODO: Temporarily using --no-deps due to a bug in Rust 1.75
+                // <https://github.com/rust-lang/rust/issues/114891>.
+                // Revert the commit that introduced this when Rust 1.76 is released.
+                cargo().arg("doc").arg("--no-deps").run()?;
             }
         }
         XtaskCommand::Fmt => {
