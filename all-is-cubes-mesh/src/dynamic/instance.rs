d0ea5b53929d6fb2007635954615464cbcebdf67
Kevin Reid <kpreid@switchb.org>
2024-02-01 09:42:45 -0800

    mesh: Use a vector instead of a set for cubes in `InstanceCollector`.
    We have no need for deduplication.

--- all-is-cubes-mesh/src/dynamic/instance.rs
+++ all-is-cubes-mesh/src/dynamic/instance.rs
@@ -7,11 +7,14 @@ use crate::dynamic::ChunkMesh;
 /// A helper for [`ChunkMesh::block_instances`] which collects instances and re-groups them by
 /// block mesh.
 ///
+/// It guarantees that each [`BlockIndex`] only appears once per group, but does not guarantee that
+/// each (index, cube) pair appears only once; this does not matter for the intended use case.
+///
 /// Use [`Iterator::collect()`] or [`Extend::extend()`] to fill it with instances, then iterate
 /// over them with [`Self::iter()`].
 #[derive(Debug, Default)]
 pub struct InstanceCollector {
-    map: fnv::FnvHashMap<BlockIndex, fnv::FnvHashSet<Cube>>,
+    map: fnv::FnvHashMap<BlockIndex, Vec<Cube>>,
 }
 
 impl InstanceCollector {
@@ -22,8 +25,8 @@ impl InstanceCollector {
 
     /// Clears the collection, removing all instances without deallocating any of the storage.
     pub fn clear(&mut self) {
-        for set in self.map.values_mut() {
-            set.clear()
+        for vector in self.map.values_mut() {
+            vector.clear()
         }
     }
 
@@ -55,5 +58,45 @@ where
         let mut this = Self::new();
         this.extend(iter);
         this
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use all_is_cubes::math::GridCoordinate;
+
+    /// Convert [`InstanceCollector::iter()`] into a deterministic concrete structure.
+    fn collect_collector(c: &InstanceCollector) -> Vec<(BlockIndex, Vec<[GridCoordinate; 3]>)> {
+        let mut by_block: Vec<_> = c
+            .iter()
+            .map(|(block_index, cubes)| {
+                let mut cubes: Vec<_> = cubes.map(<[GridCoordinate; 3]>::from).collect();
+                cubes.sort();
+                (block_index, cubes)
+            })
+            .collect();
+        by_block.sort();
+        by_block
+    }
+
+    fn cube(x: GridCoordinate, y: GridCoordinate, z: GridCoordinate) -> Cube {
+        Cube { x, y, z }
+    }
+
+    #[test]
+    fn basic_collection() {
+        let c = InstanceCollector::from_iter(vec![
+            (0, vec![cube(0, 0, 0), cube(0, 0, 1)]),
+            (1, vec![cube(1, 0, 0), cube(1, 1, 1)]),
+            (0, vec![cube(0, 0, 2), cube(99, 0, 3)]),
+        ]);
+        assert_eq!(
+            collect_collector(&c),
+            vec![
+                (0, vec![[0, 0, 0], [0, 0, 1], [0, 0, 2], [99, 0, 3]]),
+                (1, vec![[1, 0, 0], [1, 1, 1]]),
+            ]
+        )
     }
 }
