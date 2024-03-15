452b86cdc32d55ddb43c71d116a607f58ab05a97
Kevin Reid <kpreid@switchb.org>
2024-03-15 13:33:09 -0700

    content: Add "elevator shaft" to demo city.
    Later I plan to add some stairs or force-fields that can provide
    upwards travel, and decorations and something at the top.
    But this is sufficient to be a "hey there is something below" signal.

--- all-is-cubes-content/src/city/exhibits.rs
+++ all-is-cubes-content/src/city/exhibits.rs
@@ -47,6 +47,7 @@ use crate::{
 ///
 /// Ordered by distance from the center.
 pub(crate) static DEMO_CITY_EXHIBITS: &[Exhibit] = &[
+    ELEVATOR,
     KNOT,
     TRANSPARENCY_LARGE,
     TRANSPARENCY_SMALL,
@@ -72,6 +73,23 @@ pub(crate) static DEMO_CITY_EXHIBITS: &[Exhibit] = &[
     ZOOM,
     BECOME,
 ];
+
+#[macro_rules_attribute::apply(exhibit!)]
+#[exhibit(
+    name: "Elevator",
+    subtitle: "OUT OF SERVICE",
+    placement: Placement::Underground,
+)]
+fn ELEVATOR(_: Context<'_>) {
+    // The exhibit placement algorithm doesn't care about exhibits punching through the ground.
+    // So, by defining a tall, underground exhibit, we can get access from both floors.
+    // TODO: Add some way to get up that isn't just flying, and a connection to the surface road.
+    // But this is sufficient to be a "hey there is something below" signal.
+
+    let space = Space::empty(GridAab::from_lower_size([0, 0, 0], [3, 16, 3]));
+
+    Ok((space, ExhibitTransaction::default()))
+}
 
 #[macro_rules_attribute::apply(exhibit!)]
 #[exhibit(
