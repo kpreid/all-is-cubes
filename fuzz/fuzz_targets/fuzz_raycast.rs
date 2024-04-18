a76d62980a866f603b803b9dd549b2a2030d0423
Kevin Reid <kpreid@switchb.org>
2024-04-18 11:55:40 -0700

    fuzz_raycast: Test more properties of the steps.

--- fuzz/fuzz_targets/fuzz_raycast.rs
+++ fuzz/fuzz_targets/fuzz_raycast.rs
@@ -24,12 +24,29 @@ fuzz_target!(|input: (bool, [f64; 3], [f64; 3])| {
         dbg!(&raycaster);
     }
 
+    let mut prev_cube = None;
+    let mut prev_t = 0.0;
     for step in raycaster.take(10) {
         if PRINT {
             println!("Step: {step:?}");
         }
+
+        // Check that t_distance increases monotonically.
+        assert!(step.t_distance() >= prev_t);
+        prev_t = step.t_distance();
+
+        // Check that steps are always adjacent to each other:
+        // no jumping over diagonals or anything else.
+        if let Some(prev_cube) = prev_cube {
+            assert_eq!(step.cube_behind(), prev_cube);
+        }
+        prev_cube = Some(step.cube_ahead());
+
+        // Check that within() is applied correctly.
         if use_bounds {
             assert!(bounds.contains_cube(step.cube_ahead()));
         }
+
+        // TODO: Check that intersection_point() is good (see existing unit test).
     }
 });
