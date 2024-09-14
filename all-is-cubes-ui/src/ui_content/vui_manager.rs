70209a742e4f997d66e24b1ae2064573619f4257
Kevin Reid <kpreid@switchb.org>
2024-09-14 14:06:04 -0700

    [unstable-rust] Use `feature(large_assignments)`.

--- all-is-cubes-ui/src/ui_content/vui_manager.rs
+++ all-is-cubes-ui/src/ui_content/vui_manager.rs
@@ -177,6 +177,7 @@ impl Vui {
             page_state: state.as_source(),
         };
 
+        #[allow(large_assignments, reason = "doing the best we can")]
         let mut new_self = Box::write(
             empty_box,
             Self {
