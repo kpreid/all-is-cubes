edc7e8d67a858a410020c95dcda494b312b718fc
Kevin Reid <kpreid@switchb.org>
2024-09-14 15:21:18 -0700

    [unstable-rust] Use `feature(assert_matches)`.

--- all-is-cubes-port/src/gltf/tests.rs
+++ all-is-cubes-port/src/gltf/tests.rs
@@ -1,3 +1,4 @@
+use std::assert_matches::assert_matches;
 use std::path::{Path, PathBuf};
 use std::time::Duration;
 
@@ -171,11 +172,11 @@ async fn export_character_not_supported() {
     )
     .await
     .unwrap_err();
-    assert!(matches!(
+    assert_matches!(
         error,
         ExportError::MemberTypeNotRepresentable {
             name,
             ..
         }
-     if name == "x".into()));
+     if name == "x".into());
 }
