11879a651f5901e6a4c655f1b87e0de1e50669a0
Kevin Reid <kpreid@switchb.org>
2024-09-14 15:21:18 -0700

    [unstable-rust] Use `feature(assert_matches)`.

--- all-is-cubes-ui/src/ui_content/vui_manager.rs
+++ all-is-cubes-ui/src/ui_content/vui_manager.rs
@@ -590,6 +590,7 @@ pub(crate) enum CueMessage {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::assert_matches::assert_matches;
 
     async fn new_vui_for_test(paused: bool) -> (Vui, flume::Receiver<ControlMessage>) {
         let (cctx, ccrx) = flume::bounded(1);
@@ -613,7 +614,7 @@ mod tests {
         let (mut vui, control_channel) = new_vui_for_test(false).await;
         vui.back();
         let msg = control_channel.try_recv().unwrap();
-        assert!(matches!(msg, ControlMessage::TogglePause), "{msg:?}");
+        assert_matches!(msg, ControlMessage::TogglePause);
         assert!(control_channel.try_recv().is_err());
     }
 
@@ -623,7 +624,7 @@ mod tests {
         vui.set_state(VuiPageState::Paused);
         vui.back();
         let msg = control_channel.try_recv().unwrap();
-        assert!(matches!(msg, ControlMessage::TogglePause), "{msg:?}");
+        assert_matches!(msg, ControlMessage::TogglePause);
         assert!(control_channel.try_recv().is_err());
     }
 }
