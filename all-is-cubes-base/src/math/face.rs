fea753c4a105c2c4f12041e6cb8e02d77ec95360
Kevin Reid <kpreid@switchb.org>
2024-09-14 14:06:04 -0700

    [unstable-rust] Use `feature(large_assignments)`.

--- all-is-cubes-base/src/math/face.rs
+++ all-is-cubes-base/src/math/face.rs
@@ -1081,6 +1081,7 @@ mod tests {
 
     #[test]
     #[rustfmt::skip]
+    #[allow(large_assignments)]
     fn from_snapped_vector_cases() {
         let mut f = MultiFailure::new();
         for (face, vector, comment) in [
