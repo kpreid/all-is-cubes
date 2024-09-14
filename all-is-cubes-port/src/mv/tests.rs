cc5746e87b278c25b2d392c16d323af3d256c064
Kevin Reid <kpreid@switchb.org>
2024-09-14 15:21:18 -0700

    [unstable-rust] Use `feature(assert_matches)`.

--- all-is-cubes-port/src/mv/tests.rs
+++ all-is-cubes-port/src/mv/tests.rs
@@ -1,3 +1,4 @@
+use std::assert_matches::assert_matches;
 use std::sync::Arc;
 
 use either::Either;
@@ -191,7 +192,7 @@ async fn export_too_large_space() {
     )
     .await
     .unwrap_err();
-    assert!(matches!(error, ExportError::NotRepresentable { .. }));
+    assert_matches!(error, ExportError::NotRepresentable { .. });
 }
 
 /// Test that `export_to_dot_vox_data` accepts BlockDefs.
@@ -216,11 +217,9 @@ async fn export_block_def() {
     .await
     .unwrap();
 
-    assert!(
-        matches!(
-            data,
-            dot_vox::DotVoxData { ref models, .. } if models.len() == 1
-        ),
+    assert_matches!(
+        data,
+        dot_vox::DotVoxData { ref models, .. } if models.len() == 1,
         "{data:#?}",
     );
 }
