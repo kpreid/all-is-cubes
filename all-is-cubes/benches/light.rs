fbea0cc2c05fd5d8708091e3cb85f64419551ad1
Kevin Reid <kpreid@switchb.org>
2024-09-20 13:09:58 -0700

    benches/light: Add benchmark of `fast_evaluate_light()`.

--- all-is-cubes/benches/light.rs
+++ all-is-cubes/benches/light.rs
@@ -13,30 +13,36 @@ pub fn evaluate_light_bench(c: &mut Criterion) {
         .build()
         .unwrap();
 
-    let mut group = c.benchmark_group("evaluate");
-    group.sample_size(10);
+    for fast in [false, true] {
+        let mut group = c.benchmark_group(if fast { "fast" } else { "evaluate" });
+        group.sample_size(if fast { 100 } else { 10 });
 
-    group.bench_function("lighting_bench_space", |b| {
-        b.iter_batched_ref(
-            || {
-                let mut u = Universe::new();
-                let space = rt
-                    .block_on(lighting_bench_space(
-                        &mut u,
-                        yield_progress_for_testing(),
-                        size3(54, 16, 54),
-                    ))
-                    .unwrap();
-                (u, space)
-            },
-            |(_u, space)| {
-                space.evaluate_light::<time::NoTime>(1, |_| {});
-            },
-            BatchSize::LargeInput,
-        )
-    });
+        group.bench_function("lighting_bench_space", |b| {
+            b.iter_batched_ref(
+                || {
+                    let mut u = Universe::new();
+                    let space = rt
+                        .block_on(lighting_bench_space(
+                            &mut u,
+                            yield_progress_for_testing(),
+                            size3(54, 16, 54),
+                        ))
+                        .unwrap();
+                    (u, space)
+                },
+                |(_u, space)| {
+                    if fast {
+                        space.fast_evaluate_light();
+                    } else {
+                        space.evaluate_light::<time::NoTime>(1, |_| {});
+                    }
+                },
+                BatchSize::LargeInput,
+            )
+        });
 
-    group.finish();
+        group.finish();
+    }
 }
 
 criterion_group!(benches, evaluate_light_bench);
