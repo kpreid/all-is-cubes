1905773dba57b29c9ac17289f7dea6d129e2a895
Kevin Reid <kpreid@switchb.org>
2024-10-21 14:52:54 -0700

    [unstable-rust] Use edition = "2024".
    * rustfmt style edition is held back to 2021 to avoid merge conflicts.
    * `wasm-bindgen` doesn't yet support generating the new unsafe extern
      block syntax, so `all-is-cubes-wasm` is still on 2021.

--- all-is-cubes-wasm/tests/browser/main.rs
+++ all-is-cubes-wasm/tests/browser/main.rs
@@ -2,6 +2,8 @@
 //!
 //! These tests are usually run headless via `wasm-pack test --headless`.
 
+#![allow(missing_docs, reason = "TODO: false positive on nightly")]
+
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
 mod js_bindings;
