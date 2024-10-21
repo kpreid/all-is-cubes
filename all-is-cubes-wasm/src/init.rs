149b1190ae776e3382da19609f3920ecc8d71fd8
Kevin Reid <kpreid@switchb.org>
2024-10-21 14:52:54 -0700

    [unstable-rust] Use edition = "2024".
    * rustfmt style edition is held back to 2021 to avoid merge conflicts.
    * `wasm-bindgen` doesn't yet support generating the new unsafe extern
      block syntax, so `all-is-cubes-wasm` is still on 2021.

--- all-is-cubes-wasm/src/init.rs
+++ all-is-cubes-wasm/src/init.rs
@@ -162,7 +162,7 @@ async fn start_game_with_dom(
         .build::<crate::AdaptedInstant>(
             universe_progress,
             all_is_cubes_content::TemplateParameters {
-                seed: thread_rng().gen(),
+                seed: thread_rng().r#gen(),
                 size: None,
             },
         )
