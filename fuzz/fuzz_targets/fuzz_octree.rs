ef6154159a9e58952ed5d4ccfbfcb26d580e8cd2
Kevin Reid <kpreid@switchb.org>
2024-11-03 10:15:52 -0800

    fuzz_octree: Format fuzz input in code-ish fashion.

--- fuzz/fuzz_targets/fuzz_octree.rs
+++ fuzz/fuzz_targets/fuzz_octree.rs
@@ -1,6 +1,8 @@
 #![no_main]
 extern crate all_is_cubes;
 
+use core::fmt;
+
 use all_is_cubes::math::GridAab;
 use all_is_cubes_gpu::octree_alloc::Alloctree;
 
@@ -12,7 +14,7 @@ struct FuzzOctree {
     operations: Vec<Operation>,
 }
 
-#[derive(Arbitrary, Debug)]
+#[derive(Arbitrary)]
 enum Operation {
     Allocate(GridAab),
     AllocateGrow(GridAab, u8),
@@ -50,4 +52,29 @@ fuzz_target!(|input: FuzzOctree| {
 
 fn clean_exponent(input: u8) -> u8 {
     input.rem_euclid(Alloctree::<()>::MAX_SIZE_EXPONENT + 1)
+}
+
+/// Print operations in the form of code that can be roughly copied into an integration test.
+impl fmt::Debug for Operation {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fn fmt_aab(f: &mut fmt::Formatter<'_>, aab: GridAab) -> fmt::Result {
+            let lower = aab.lower_bounds().to_array();
+            let size = aab.size().to_array();
+            write!(f, "GridAab::from_lower_size({lower:?}, {size:?})")
+        }
+
+        match *self {
+            Self::Allocate(request) => {
+                write!(f, "handles.push(t.allocate(")?;
+                fmt_aab(f, request)?;
+                write!(f, ")?)")
+            }
+            Self::AllocateGrow(request, max_growth) => {
+                write!(f, "handles.push(t.allocate_with_growth(")?;
+                fmt_aab(f, request)?;
+                write!(f, ", {max_growth:?})?)")
+            }
+            Self::Free(i) => write!(f, "t.free(handles.remove({i}))"),
+        }
+    }
 }
