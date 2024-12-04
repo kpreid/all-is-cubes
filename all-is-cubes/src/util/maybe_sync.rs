fd73afe91783a441163a37ffbadb0e189bafd158
Kevin Reid <kpreid@switchb.org>
2024-12-03 22:43:18 -0800

    Generalize `maybe_sync` types to `?Sized`.

--- all-is-cubes/src/util/maybe_sync.rs
+++ all-is-cubes/src/util/maybe_sync.rs
@@ -45,10 +45,10 @@ cfg_if::cfg_if! {
 /// * This may or may not implement mutex poisoning.
 /// * This may or may not deadlock if locked again from the same thread.
 #[derive(Default)]
-pub struct Mutex<T>(InnerMutex<T>);
+pub struct Mutex<T: ?Sized>(InnerMutex<T>);
 
 #[allow(missing_debug_implementations)]
-pub struct MutexGuard<'a, T>(InnerMutexGuard<'a, T>);
+pub struct MutexGuard<'a, T: ?Sized>(InnerMutexGuard<'a, T>);
 
 /// Wrapper around [`core::cell::RefCell`] or [`std::sync::RwLock`] depending on whether
 /// the `std` feature is enabled.
@@ -59,10 +59,10 @@ pub struct MutexGuard<'a, T>(InnerMutexGuard<'a, T>);
 /// * This may or may not implement mutex poisoning.
 /// * This may or may not deadlock if locked again from the same thread.
 #[derive(Default)]
-pub(crate) struct RwLock<T>(InnerRwLock<T>);
+pub(crate) struct RwLock<T: ?Sized>(InnerRwLock<T>);
 
-pub(crate) struct RwLockReadGuard<'a, T>(InnerRwLockReadGuard<'a, T>);
-pub(crate) struct RwLockWriteGuard<'a, T>(InnerRwLockWriteGuard<'a, T>);
+pub(crate) struct RwLockReadGuard<'a, T: ?Sized>(InnerRwLockReadGuard<'a, T>);
+pub(crate) struct RwLockWriteGuard<'a, T: ?Sized>(InnerRwLockWriteGuard<'a, T>);
 
 cfg_if::cfg_if! {
     if #[cfg(feature = "std")] {
@@ -80,13 +80,13 @@ cfg_if::cfg_if! {
     }
 }
 
-impl<T: fmt::Debug> fmt::Debug for Mutex<T> {
+impl<T: ?Sized + fmt::Debug> fmt::Debug for Mutex<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         self.0.fmt(f)
     }
 }
 
-impl<T: fmt::Debug> fmt::Debug for RwLock<T> {
+impl<T: ?Sized + fmt::Debug> fmt::Debug for RwLock<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         self.0.fmt(f)
     }
@@ -96,7 +96,9 @@ impl<T> Mutex<T> {
     pub const fn new(value: T) -> Self {
         Self(InnerMutex::new(value))
     }
+}
 
+impl<T: ?Sized> Mutex<T> {
     pub fn lock(&self) -> Result<MutexGuard<'_, T>, LockError<MutexGuard<'_, T>>> {
         cfg_if::cfg_if! {
             if #[cfg(feature = "std")] {
@@ -115,7 +117,9 @@ impl<T> RwLock<T> {
     pub const fn new(value: T) -> Self {
         Self(InnerRwLock::new(value))
     }
+}
 
+impl<T: ?Sized> RwLock<T> {
     pub fn read(&self) -> Result<RwLockReadGuard<'_, T>, LockError<RwLockReadGuard<'_, T>>> {
         cfg_if::cfg_if! {
             if #[cfg(feature = "std")] {
@@ -179,33 +183,33 @@ impl<T> RwLock<T> {
     }
 }
 
-impl<T> ops::Deref for MutexGuard<'_, T> {
+impl<T: ?Sized> ops::Deref for MutexGuard<'_, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
-impl<T> ops::DerefMut for MutexGuard<'_, T> {
+impl<T: ?Sized> ops::DerefMut for MutexGuard<'_, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
     }
 }
-impl<T> ops::Deref for RwLockReadGuard<'_, T> {
+impl<T: ?Sized> ops::Deref for RwLockReadGuard<'_, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
-impl<T> ops::Deref for RwLockWriteGuard<'_, T> {
+impl<T: ?Sized> ops::Deref for RwLockWriteGuard<'_, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
-impl<T> ops::DerefMut for RwLockWriteGuard<'_, T> {
+impl<T: ?Sized> ops::DerefMut for RwLockWriteGuard<'_, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
     }
