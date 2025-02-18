dbd28899c054fd59121bae88570a0525d856894f
Kevin Reid <kpreid@switchb.org>
2025-02-17 18:36:17 -0800

    Rust 1.85: Remove unnecessary `allow(unknown_lints)`.

--- all-is-cubes-desktop/src/universe_source.rs
+++ all-is-cubes-desktop/src/universe_source.rs
@@ -41,7 +41,6 @@ impl UniverseSource {
 
         // TODO: figure out a cleaner way to wrangle this rx hookup
         let notif_rx = Mutex::new(TryRecvKeep::Rx(notif_rx));
-        #[allow(unknown_lints)] // TODO: Remove in Rust 1.85
         #[allow(clippy::literal_string_with_formatting_args)]
         let universe_progress_bar = logging::new_progress_bar(100)
             .with_style(
