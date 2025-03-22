c7245e521a79ab3182cd01cab5b46b1fce3ffb7c
Kevin Reid <kpreid@switchb.org>
2025-03-22 10:30:45 -0700

    content: menger-sponge: Refactor point generation into an iterator.

--- all-is-cubes-content/src/fractal.rs
+++ all-is-cubes-content/src/fractal.rs
@@ -1,12 +1,16 @@
+use alloc::boxed::Box;
+use core::iter;
+
 use all_is_cubes::block::Block;
 use all_is_cubes::character::Spawn;
 use all_is_cubes::content::free_editing_starter_inventory;
-use all_is_cubes::euclid::{Point3D, Size3D, Vector3D};
+use all_is_cubes::euclid::{Point3D, Vector3D};
 use all_is_cubes::inv::Tool;
 use all_is_cubes::linking::{BlockProvider, InGenError};
 use all_is_cubes::math::{Cube, GridAab, GridCoordinate, GridPoint, GridSize, Rgb, rgba_const};
 use all_is_cubes::space::{self, Space};
 use all_is_cubes::universe::Universe;
+use either::Either;
 
 use crate::DemoBlocks;
 
@@ -58,7 +62,7 @@ pub(crate) fn menger_sponge(
             spawn
         })
         .build();
-    visit_menger_sponge_points(world_levels, GridPoint::origin(), &mut |cube| {
+    for cube in menger_sponge_points(world_levels, GridPoint::origin()) {
         let coloring = (cube.lower_bounds() / 3)
             .to_vector()
             .dot(Vector3D::splat(1))
@@ -71,38 +75,35 @@ pub(crate) fn menger_sponge(
                 &leaf_block_2
             },
         )?;
-        Ok::<_, InGenError>(())
-    })?;
+    }
     space.fast_evaluate_light();
     Ok(space)
 }
 
 /// Visit all cubes that are part of the Menger sponge of the given level.
 /// The side length of the bounding box is `3.pow(levels)`.
-fn visit_menger_sponge_points<E, F>(
-    level: u8,
-    lower_corner: GridPoint,
-    function: &mut F,
-) -> Result<(), E>
-where
-    F: FnMut(Cube) -> Result<(), E>,
-{
+fn menger_sponge_points(level: u8, lower_corner: GridPoint) -> impl Iterator<Item = Cube> {
+    // It'd be nice if we could do this without allocation, but not very important since
+    // the rest of the process is quite expensive too.
     if level == 0 {
-        function(Cube::from(lower_corner))?;
+        Either::Left(iter::once(Cube::from(lower_corner)))
     } else {
         let size_of_next_level: GridCoordinate = 3_i32.pow((level - 1).into());
-        for which_section in pow3aab(1).interior_iter() {
-            let Point3D { x, y, z, .. } = which_section
-                .lower_bounds()
-                .map(|c| u8::from(c.rem_euclid(2) == 1));
-            if x + y + z <= 1 {
-                let section_corner =
-                    lower_corner + which_section.lower_bounds().to_vector() * size_of_next_level;
-                visit_menger_sponge_points(level - 1, section_corner, function)?;
-            }
-        }
+        let iterator: Box<dyn Iterator<Item = Cube>> =
+            Box::new(pow3aab(1).interior_iter().flat_map(move |which_section| {
+                let Point3D { x, y, z, .. } = which_section
+                    .lower_bounds()
+                    .map(|c| u8::from(c.rem_euclid(2) == 1));
+                if x + y + z <= 1 {
+                    let section_corner = lower_corner
+                        + which_section.lower_bounds().to_vector() * size_of_next_level;
+                    Either::Left(menger_sponge_points(level - 1, section_corner))
+                } else {
+                    Either::Right(iter::empty())
+                }
+            }));
+        Either::Right(iterator)
     }
-    Ok(())
 }
 
 fn pow3aab(level: u8) -> GridAab {
