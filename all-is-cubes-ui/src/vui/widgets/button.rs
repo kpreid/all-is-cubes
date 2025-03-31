kpreid/all-is-cubes

commit 8333fdacdd4f33bda2c26955a3c01d5818a2ecca

ui,content: Revise colors now that we have emission rendering.

all-is-cubes-ui/src/vui/widgets/button.rs
@@ -496,19 +496,22 @@ impl ButtonBase for ToggleButtonVisualState {
     fn button_block(&self, universe: &mut Universe) -> Result<Block, InGenError> {
         let label_z = self.button_label_z();
         let active = self.value;
-        let back_block = if active {
-            Block::builder()
-                .color(palette::BUTTON_ACTIVATED_BACK)
-                .light_emission(palette::BUTTON_ACTIVATED_GLOW)
-                .build()
-        } else {
-            Block::from(palette::BUTTON_BACK)
+        let illuminate = move |builder: BlockBuilder<block::builder::BlockBuilderAtom>| {
+            if active {
+                builder.light_emission(palette::BUTTON_ACTIVATED_GLOW)
+            } else {
+                builder
+            }
+            .build()
         };
         let frame_brush = VoxelBrush::single(Block::from(palette::BUTTON_FRAME));
-        let back_brush = VoxelBrush::with_thickness(back_block, 0..label_z);
+        let back_brush = VoxelBrush::with_thickness(
+            illuminate(Block::builder().color(palette::BUTTON_ACTIVATED_BACK)),
+            0..label_z,
+        );
         let cap_rim_brush = VoxelBrush::new([(
             [0, 0, label_z - 1],
-            Block::from(theme::rim_lightening(palette::BUTTON_BACK)),
+            illuminate(Block::builder().color(theme::rim_lightening(palette::BUTTON_BACK))),
         )]);
 
         let outer_inset = 2;

all-is-cubes-content/src/animation.rs
@@ -121,15 +121,15 @@ impl Fire {
         fn fire_color(color: Rgba) -> Block {
             Block::builder()
                 .color(color)
-                .light_emission(color.to_rgb() * 20.0)
+                .light_emission(color.to_rgb() * 8.0)
                 .collision(BlockCollision::None)
                 .build()
         }
         let blocks = [
             AIR,
-            fire_color(rgba_const!(1.0, 0.5, 0.0, 1.0)),
-            fire_color(rgba_const!(1.0, 0.0, 0.0, 1.0)),
-            fire_color(rgba_const!(1.0, 1.0, 0.0, 1.0)),
+            fire_color(rgba_const!(1.0, 0.5, 0.1, 1.0)),
+            fire_color(rgba_const!(1.0, 0.1, 0.1, 1.0)),
+            fire_color(rgba_const!(1.0, 1.0, 0.1, 1.0)),
         ];
         Self {
             blocks,

all-is-cubes-content/src/atrium.rs
@@ -681,7 +681,6 @@ async fn install_atrium_blocks(
                 .build(),
             AtriumBlocks::Firepot => Block::builder()
                 .display_name("Firepot")
-                // .light_emission(rgb_const!(1.4, 1.0, 0.8) * 4.0)
                 .voxels_ref(resolution, {
                     let mut space = Space::for_block(resolution).build();
                     // Use a darker color to dampen the effect of interior light

all-is-cubes-content/src/dungeon/demo_dungeon.rs
@@ -631,7 +631,7 @@ pub async fn install_dungeon_blocks(
 
     let light_voxel = Block::builder()
         .color(Rgba::new(0.7, 0.7, 0.0, 1.0))
-        .light_emission(Rgb::new(8.0, 7.0, 0.7))
+        .light_emission(Rgb::new(8.0, 7.0, 0.7) * 0.5)
         .build();
     let spike_metal = Block::from(palette::STEEL);
 

all-is-cubes/src/content/palette.rs
@@ -119,10 +119,10 @@ palette! {
     BUTTON_FRAME = srgb[0x3d 0x3d 0x3d 0xFF];
     BUTTON_BACK = srgb[0xBC 0xBC 0xBC 0xFF];
     BUTTON_LABEL = srgb[0x3d 0x3d 0x3d 0xFF];
-    BUTTON_ACTIVATED_BACK = srgb[0xE1 0xE1 0xE1 0xFF];
+    BUTTON_ACTIVATED_BACK = srgb[0xD1 0xBC 0xBC 0xFF];
     BUTTON_ACTIVATED_LABEL = srgb[0x63 0x63 0x63 0xFF];
 }
-pub const BUTTON_ACTIVATED_GLOW: Rgb = rgb_const!(4.0, 0.8, 0.8); // not representable as integer srgb
+pub const BUTTON_ACTIVATED_GLOW: Rgb = rgb_const!(1.0, 0.2, 0.2);
 
 palette! {
     // In-world debug UI elements (all wireframe)

commit 5b95c5fceb9285ef2dfa3d26533095c010012abb

ui: Fix incorrectly using `BUTTON_ACTIVATED_BACK` color on non-activated buttons.
This was broken in commit 8333fdacdd4f33bda2c26955a3c01d5818a2ecca from
when I added per-voxel light emission rendering.

all-is-cubes-ui/src/vui/widgets/button.rs
@@ -658,14 +658,17 @@ impl ButtonBase for ToggleButtonVisualState {
             }
             .build()
         };
+        let back_color = if active {
+            palette::BUTTON_ACTIVATED_BACK
+        } else {
+            palette::BUTTON_BACK
+        };
         let frame_brush = VoxelBrush::single(block::from_color!(palette::BUTTON_FRAME));
-        let back_brush = VoxelBrush::with_thickness(
-            illuminate(Block::builder().color(palette::BUTTON_ACTIVATED_BACK)),
-            0..label_z,
-        );
+        let back_brush =
+            VoxelBrush::with_thickness(illuminate(Block::builder().color(back_color)), 0..label_z);
         let cap_rim_brush = VoxelBrush::new([(
             [0, 0, label_z - 1],
-            illuminate(Block::builder().color(theme::rim_lightening(palette::BUTTON_BACK))),
+            illuminate(Block::builder().color(theme::rim_lightening(back_color))),
         )]);
 
         let outer_inset = 2;