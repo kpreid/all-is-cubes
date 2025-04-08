9862ec1c36a5ec7ea0b59d2a5b1e7975d45635d2
Kevin Reid <kpreid@switchb.org>
2025-04-08 11:02:58 -0700

    test-renderers: Print log messages at `Warn` and above even if not verbose.
    May require further tuning.

--- test-renderers/src/lib.rs
+++ test-renderers/src/lib.rs
@@ -312,43 +312,45 @@ pub fn compare_rendered_image(
 }
 
 pub fn initialize_logging(args: &HarnessArgs) {
-    if !args.verbose {
-        return;
+    struct RenderHarnessLogger {
+        level_filter: log::LevelFilter,
+    }
+    impl log::Log for RenderHarnessLogger {
+        fn enabled(&self, metadata: &log::Metadata<'_>) -> bool {
+            let t = metadata.target();
+            metadata.level() <= self.level_filter
+                && (!t.starts_with("wgpu") || t == "wgpu_render")
+                && !t.starts_with("naga")
+        }
+        fn log(&self, record: &log::Record<'_>) {
+            use io::Write as _;
+
+            if self.enabled(record.metadata()) {
+                let test_id: Option<TestId> = TEST_ID.try_with(|id| id.clone()).ok();
+                let mut lock = io::stderr().lock();
+                _ = time::OffsetDateTime::now_utc()
+                    .to_offset(time::UtcOffset::UTC)
+                    .format_into(
+                        &mut lock,
+                        time::macros::format_description!("[hour]:[minute]:[second]"),
+                    );
+                _ = write!(lock, " [{level}] (", level = record.level(),);
+                _ = match test_id {
+                    Some(id) => write!(lock, "{id}"),
+                    None => write!(lock, "?"),
+                };
+                _ = writeln!(lock, ") {}", record.args());
+            }
+        }
+        fn flush(&self) {}
     }
 
-    log::set_logger({
-        struct RenderHarnessLogger;
-        impl log::Log for RenderHarnessLogger {
-            fn enabled(&self, metadata: &log::Metadata<'_>) -> bool {
-                let t = metadata.target();
-                // Trace is the finest level, so no need to check it
-                /* metadata.level() <= log::LevelFilter::Trace && */
-                !t.starts_with("wgpu") && !t.starts_with("naga")
-            }
-            fn log(&self, record: &log::Record<'_>) {
-                use io::Write as _;
+    let level_filter = if args.verbose {
+        log::LevelFilter::Trace
+    } else {
+        log::LevelFilter::Warn
+    };
 
-                if self.enabled(record.metadata()) {
-                    let test_id: Option<TestId> = TEST_ID.try_with(|id| id.clone()).ok();
-                    let mut lock = io::stderr().lock();
-                    _ = time::OffsetDateTime::now_utc()
-                        .to_offset(time::UtcOffset::UTC)
-                        .format_into(
-                            &mut lock,
-                            time::macros::format_description!("[hour]:[minute]:[second]"),
-                        );
-                    _ = write!(lock, " [{level}] (", level = record.level(),);
-                    _ = match test_id {
-                        Some(id) => write!(lock, "{id}"),
-                        None => write!(lock, "?"),
-                    };
-                    _ = writeln!(lock, ") {}", record.args());
-                }
-            }
-            fn flush(&self) {}
-        }
-        &RenderHarnessLogger
-    })
-    .unwrap();
-    log::set_max_level(log::LevelFilter::Trace);
+    log::set_logger(Box::leak(Box::new(RenderHarnessLogger { level_filter }))).unwrap();
+    log::set_max_level(level_filter);
 }
