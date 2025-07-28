61663d92db09f648ba32ff85ea2a1f84e5fcb01a
Kevin Reid <kpreid@switchb.org>
2025-07-28 16:24:08 -0700

    [unstable-rust] Use `feature(const_trait_impl)`, etc.
    Note: this does not include declaring any of our traits const,
    because rust-analyzer currently errors on those.

--- all-is-cubes-mesh/src/block_mesh/planar.rs
+++ all-is-cubes-mesh/src/block_mesh/planar.rs
@@ -252,7 +252,7 @@ pub(super) enum QuadColoring<'a, T> {
     },
 }
 impl<T> Copy for QuadColoring<'_, T> {}
-impl<T> Clone for QuadColoring<'_, T> {
+impl<T> const Clone for QuadColoring<'_, T> {
     fn clone(&self) -> Self {
         *self
     }
