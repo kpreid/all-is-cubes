de1262f62e8a64d5edc661dbec8fe55c505bab72
Kevin Reid <kpreid@switchb.org>
2025-07-29 20:52:06 -0700

    raycast: Remove broken link to `raytracer` module; give the public path.

--- all-is-cubes/src/raycast.rs
+++ all-is-cubes/src/raycast.rs
@@ -3,7 +3,7 @@
 //! This deals purely with the question “which cubes does this ray intersect”,
 //! and does not concern itself with what might occupy those cubes. If you’re
 //! looking for *raytracing*, forming an image from many rays, that’s
-//! [`all_is_cubes::raytracer`](crate::raytracer).
+//! `all_is_cubes_render::raytracer`.
 
 use crate::block::Resolution;
 use crate::math::{FreeVector, GridAab};
