ef334f67408c723c16f67ff145116f50ebaf9d31
Kevin Reid <kpreid@switchb.org>
2025-10-12 22:25:39 -0700

    Fix missing rerun-if-changed in all-is-cubes library build script.

--- all-is-cubes/build.rs
+++ all-is-cubes/build.rs
@@ -1,6 +1,10 @@
 #![allow(missing_docs)]
 
 fn main() {
+    // We must emit at least one rerun-if-changed directive to avoid superfluous rebuilds.
+    // <https://doc.rust-lang.org/cargo/reference/build-scripts.html#rerun-if-changed>
+    println!("cargo::rerun-if-changed=build.rs");
+
     // TODO(ecs) TODO(no_std): horrible kludge: pretend the 'std' feature is always enabled,
     // because disabling it is not currently functional.
     println!("cargo::rustc-cfg=feature=\"std\"");
