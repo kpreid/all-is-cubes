cbb5040088bf2b734e8e34e0666a568438de638c
Kevin Reid <kpreid@switchb.org>
2025-11-10 11:30:05 -0800

    [unstable-rust] Use ATPIT in txn check types where possible.

--- all-is-cubes/src/behavior.rs
+++ all-is-cubes/src/behavior.rs
@@ -586,7 +586,7 @@ impl<H: Host> BehaviorSetTransaction<H> {
 impl<H: Host> Transaction for BehaviorSetTransaction<H> {
     type Target = BehaviorSet<H>;
     type Context<'a> = ();
-    type CommitCheck = CommitCheck;
+    type CommitCheck = impl fmt::Debug;
     type Output = transaction::NoOutput;
     type Mismatch = BehaviorTransactionMismatch;
 
@@ -633,9 +633,11 @@ impl<H: Host> Transaction for BehaviorSetTransaction<H> {
     fn commit(
         self,
         target: &mut BehaviorSet<H>,
-        _: Self::CommitCheck,
+        check: Self::CommitCheck,
         _outputs: &mut dyn FnMut(Self::Output),
     ) -> Result<(), transaction::CommitError> {
+        let CommitCheck { _private: () }: CommitCheck = check;
+
         for (key, replacement) in self.replace {
             match replacement.new {
                 Some(new) => {
@@ -689,7 +691,7 @@ impl<H: Host> Transaction for BehaviorSetTransaction<H> {
 }
 
 impl<H: Host> transaction::Merge for BehaviorSetTransaction<H> {
-    type MergeCheck = MergeCheck;
+    type MergeCheck = impl fmt::Debug;
     type Conflict = BehaviorTransactionConflict;
 
     fn check_merge(&self, other: &Self) -> Result<Self::MergeCheck, Self::Conflict> {
@@ -700,7 +702,9 @@ impl<H: Host> transaction::Merge for BehaviorSetTransaction<H> {
         Ok(MergeCheck { _private: () })
     }
 
-    fn commit_merge(&mut self, other: Self, _: Self::MergeCheck) {
+    fn commit_merge(&mut self, other: Self, check: Self::MergeCheck) {
+        let MergeCheck { _private: () }: MergeCheck = check;
+
         self.replace.extend(other.replace);
         self.insert.extend(other.insert);
     }
