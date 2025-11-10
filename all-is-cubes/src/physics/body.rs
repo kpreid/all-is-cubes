54a738f7053ff3dd68b78ed33ca41efcd4ff031b
Kevin Reid <kpreid@switchb.org>
2025-11-10 11:30:05 -0800

    [unstable-rust] Use ATPIT in txn check types where possible.

--- all-is-cubes/src/physics/body.rs
+++ all-is-cubes/src/physics/body.rs
@@ -993,7 +993,7 @@ impl transaction::Transactional for Body {
 impl Transaction for BodyTransaction {
     type Target = Body;
     type Context<'a> = ();
-    type CommitCheck = ();
+    type CommitCheck = impl fmt::Debug;
     type Output = transaction::NoOutput;
     type Mismatch = BodyMismatch;
 
@@ -1010,9 +1010,10 @@ impl Transaction for BodyTransaction {
         self,
         body: &mut Body,
         (): Self::Context<'_>,
-        (): Self::CommitCheck,
+        check: Self::CommitCheck,
         _outputs: &mut dyn FnMut(Self::Output),
     ) -> Result<(), transaction::CommitError> {
+        let (): () = check; // https://github.com/rust-lang/rust/issues/113596
         let Self {
             set_position,
             set_look_direction,
@@ -1028,7 +1029,7 @@ impl Transaction for BodyTransaction {
 }
 
 impl transaction::Merge for BodyTransaction {
-    type MergeCheck = ();
+    type MergeCheck = impl fmt::Debug;
     type Conflict = BodyConflict;
 
     fn check_merge(&self, other: &Self) -> Result<Self::MergeCheck, Self::Conflict> {
@@ -1052,7 +1053,8 @@ impl transaction::Merge for BodyTransaction {
         Ok(())
     }
 
-    fn commit_merge(&mut self, other: Self, (): Self::MergeCheck) {
+    fn commit_merge(&mut self, other: Self, check: Self::MergeCheck) {
+        let (): () = check; // https://github.com/rust-lang/rust/issues/113596
         let Self {
             set_position,
             set_look_direction,
--- all-is-cubes/src/space/space_txn.rs
+++ all-is-cubes/src/space/space_txn.rs
@@ -372,7 +372,7 @@ impl universe::TransactionOnEcs for SpaceTransaction {
     }
 }
 impl Merge for SpaceTransaction {
-    type MergeCheck = <BehaviorSetTransaction<Space> as Merge>::MergeCheck;
+    type MergeCheck = impl fmt::Debug;
     type Conflict = SpaceTransactionConflict;
 
     fn check_merge(&self, other: &Self) -> Result<Self::MergeCheck, Self::Conflict> {
@@ -647,6 +647,7 @@ impl CubeTransaction {
 }
 
 impl Merge for CubeTransaction {
+    /// Not opaque because [`SpaceTransaction`] uses it
     type MergeCheck = CubeMergeCheck;
     type Conflict = CubeConflict;
 
