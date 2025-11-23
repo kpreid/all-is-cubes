044e6e33c0ba23ad1aee0eee08e1185ea1ce3782
Kevin Reid <kpreid@switchb.org>
2025-11-22 19:33:27 -0800

    desktop,wasm: Add ambient sound playback.

--- all-is-cubes-desktop/src/audio.rs
+++ all-is-cubes-desktop/src/audio.rs
@@ -1,16 +1,25 @@
-use std::fmt;
-use std::sync::{atomic, mpsc};
+//! Glue between [`all_is_cubes::sound`] and actual system audio.
+//!
+//! Note: This contains extremely similar code to the web version at
+//! `all-is-cubes-wasm/src/audio.rs`. Most changes should apply to both.
 
-use hashbrown::HashMap;
+use std::collections::HashMap;
+use std::fmt;
+use std::sync::atomic::AtomicU32;
+use std::sync::{Arc, atomic, mpsc};
+
 use kira::AudioManager;
 use kira::PlaySoundError;
-use kira::sound::static_sound::StaticSoundData;
+use kira::sound::static_sound::{StaticSoundData, StaticSoundSettings};
+use rand::{Rng as _, SeedableRng};
 
 use all_is_cubes::fluff::Fluff;
-use all_is_cubes::listen::Listener;
-use all_is_cubes::sound::SoundDef;
+use all_is_cubes::listen::{self, Listen as _};
+use all_is_cubes::sound::{Band, SoundDef, SpatialAmbient};
 
 use crate::Session;
+
+// -------------------------------------------------------------------------------------------------
 
 /// Fills the audio slot in a `DesktopSession` to actually produce audio.
 pub(crate) struct AudioOut {
@@ -30,6 +39,7 @@ impl fmt::Debug for AudioOut {
 #[derive(Debug)]
 enum AudioCommand {
     Fluff(Fluff),
+    UpdateAmbient,
 }
 
 pub(crate) fn init_sound(session: &Session) -> Result<AudioOut, anyhow::Error> {
@@ -41,17 +51,24 @@ pub(crate) fn init_sound(session: &Session) -> Result<AudioOut, anyhow::Error> {
     // We never block on this channel, but drop things if full.
     let (sender, receiver) = mpsc::sync_channel(256);
 
+    // Hook up ambient sound to channel
+    let ambient_source = session.ambient_sound();
+    // TODO: this whole custom listener scheme should be replaced with something more async
+    // and less custom-code-per-use-case.
+    ambient_source.listen(UpdateAmbientListener::new(sender.clone()));
+    sender.send(AudioCommand::UpdateAmbient).unwrap(); // ensure initial sync
+
+    // Hook up fluff to channel
+    session.listen_fluff(FluffListener::new(sender.clone()));
+
     // The audio processing is done on a thread because `AudioManager` wants `&mut self`
     // to perform operations, so I either need a dedicated thread (actor) or a mutex.
     // We also don’t want to find ourselves running synthesis inside the Listener.
     // Note that this is *not* a sample-by-sample realtime audio thread.
     std::thread::Builder::new()
-        .name("all_is_cubes_audio".to_owned())
-        .spawn(move || audio_command_thread(receiver, manager))
+        .name("all_is_cubes audio core".to_owned())
+        .spawn(move || audio_command_thread(receiver, manager, ambient_source))
         .unwrap();
-
-    // Hook up fluff channel
-    session.listen_fluff(FluffListener::new(sender.clone()));
 
     Ok(AudioOut { sender })
 }
@@ -59,9 +76,46 @@ pub(crate) fn init_sound(session: &Session) -> Result<AudioOut, anyhow::Error> {
 /// Thread function for receiving commands and executing them on `&mut AudioManager`.
 /// This is not a real-time audio thread.
 #[expect(clippy::needless_pass_by_value)]
-fn audio_command_thread(receiver: mpsc::Receiver<AudioCommand>, mut manager: AudioManager) {
+fn audio_command_thread(
+    receiver: mpsc::Receiver<AudioCommand>,
+    mut manager: AudioManager,
+    ambient_source: listen::DynSource<SpatialAmbient>,
+) {
     let sample_rate = 44100;
     let mut sound_cache: HashMap<SoundDef, StaticSoundData> = HashMap::new();
+
+    // TODO: maybe synthesize white noise continuously to avoid any chance of audible repetition?
+    let white_noise = StaticSoundData {
+        sample_rate: 44100,
+        frames: (0..44100 * 10)
+            .map({
+                let mut rng = rand::rngs::StdRng::seed_from_u64(0);
+                move |_| kira::Frame::from_mono(rng.random())
+            })
+            .collect(),
+        settings: StaticSoundSettings {
+            loop_region: Some(kira::sound::Region::default()),
+            ..Default::default()
+        },
+        slice: None,
+    };
+
+    let mut ambient_noise_tracks: [(Option<StereoVolume>, Option<kira::track::TrackHandle>);
+        Band::COUNT] = core::array::from_fn(|band_index| {
+        let mut track_builder = kira::track::TrackBuilder::new();
+        // TODO: start with band-limited noise instead of filtering?
+        let frequency: f32 = Band::from_index(band_index).center_frequency().into_inner();
+        track_builder.add_effect(
+            kira::effect::filter::FilterBuilder::new()
+                .cutoff(f64::from(frequency))
+                .mode(kira::effect::filter::FilterMode::BandPass)
+                .resonance(0.8),
+        );
+        let volume = track_builder.add_effect(StereoVolume::new());
+        let mut ambient_track = manager.add_sub_track(track_builder).unwrap(); // TODO: log instead of unwrap
+        ambient_track.play(white_noise.clone()).unwrap();
+        (Some(volume), Some(ambient_track))
+    });
 
     while let Ok(message) = receiver.recv() {
         match message {
@@ -79,18 +133,40 @@ fn audio_command_thread(receiver: mpsc::Receiver<AudioCommand>, mut manager: Aud
                         }
                     };
 
-                    sound.settings.volume = kira::Decibels(amplitude.log10() * 20.0).into();
+                    sound.settings.volume = amplitude_to_value(amplitude);
                     play_fluff(&mut manager, sound)
+                }
+            }
+            AudioCommand::UpdateAmbient => {
+                let new_ambient = ambient_source.get();
+
+                for (left_amplitude, right_amplitude, band_playback_volume) in itertools::izip!(
+                    new_ambient.left.noise_bands.0.iter(),
+                    new_ambient.right.noise_bands.0.iter(),
+                    ambient_noise_tracks.iter_mut().map(|(vol_handle, _)| vol_handle)
+                ) {
+                    if let Some(v) = band_playback_volume {
+                        v.set_amplitude(
+                            [left_amplitude.into_inner(), right_amplitude.into_inner()],
+                            //kira::Tween::default(),
+                        );
+                    }
                 }
             }
         }
     }
 }
 
+// TODO: wait, we are probably mixing up amplitude and power
+fn amplitude_to_value(amplitude: f32) -> kira::Value<kira::Decibels> {
+    kira::Decibels((amplitude.log10() * 20.0).max(-200.0)).into()
+}
+
 fn play_fluff(manager: &mut AudioManager, sound: StaticSoundData) {
     match manager.play(sound) {
         Ok(_handle) => {}
         Err(PlaySoundError::SoundLimitReached) => {
+            log::trace!("sound limit reached");
             // Ignore this, since fluff is inconsequential
         }
         Err(error) => log::error!(
@@ -99,6 +175,10 @@ fn play_fluff(manager: &mut AudioManager, sound: StaticSoundData) {
         ),
     }
 }
+
+// -------------------------------------------------------------------------------------------------
+
+// TODO: De-boilerplate these listener-to-channel adapters
 
 /// Adapter from [`Listener`] to the audio command channel.
 struct FluffListener {
@@ -122,7 +202,7 @@ impl fmt::Debug for FluffListener {
     }
 }
 
-impl Listener<Fluff> for FluffListener {
+impl listen::Listener<Fluff> for FluffListener {
     fn receive(&self, fluffs: &[Fluff]) -> bool {
         if !self.alive.load(atomic::Ordering::Relaxed) {
             return false;
@@ -146,6 +226,50 @@ impl Listener<Fluff> for FluffListener {
 
 // -------------------------------------------------------------------------------------------------
 
+/// Adapter from [`Source`] to the audio command channel.
+struct UpdateAmbientListener {
+    sender: mpsc::SyncSender<AudioCommand>,
+    alive: atomic::AtomicBool,
+}
+impl UpdateAmbientListener {
+    fn new(sender: mpsc::SyncSender<AudioCommand>) -> Self {
+        Self {
+            sender,
+            alive: atomic::AtomicBool::new(true),
+        }
+    }
+}
+
+impl fmt::Debug for UpdateAmbientListener {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("UpdateAmbientListener")
+            .field("alive", &self.alive)
+            .finish_non_exhaustive()
+    }
+}
+
+impl listen::Listener<()> for UpdateAmbientListener {
+    fn receive(&self, messages: &[()]) -> bool {
+        if !self.alive.load(atomic::Ordering::Relaxed) {
+            return false;
+        }
+        if messages.is_empty() {
+            return true;
+        }
+        match self.sender.try_send(AudioCommand::UpdateAmbient) {
+            Ok(()) => {}
+            Err(mpsc::TrySendError::Full(_)) => {}
+            Err(mpsc::TrySendError::Disconnected(_)) => {
+                self.alive.store(false, atomic::Ordering::Relaxed);
+                return false;
+            }
+        }
+        true
+    }
+}
+
+// -------------------------------------------------------------------------------------------------
+
 fn convert_sound_to_kira(sample_rate: u32, sound: &SoundDef) -> StaticSoundData {
     StaticSoundData {
         sample_rate,
@@ -155,5 +279,43 @@ fn convert_sound_to_kira(sample_rate: u32, sound: &SoundDef) -> StaticSoundData 
             .collect(),
         settings: Default::default(),
         slice: Default::default(),
+    }
+}
+
+// -------------------------------------------------------------------------------------------------
+
+/// Effect node for stereo (independent left and right) volume control.
+#[derive(Clone)]
+struct StereoVolume(Arc<[AtomicU32; 2]>);
+impl StereoVolume {
+    fn new() -> Self {
+        Self(Arc::new([
+            AtomicU32::new(0.0f32.to_bits()),
+            AtomicU32::new(0.0f32.to_bits()),
+        ]))
+    }
+
+    fn set_amplitude(&self, [l, r]: [f32; 2]) {
+        self.0[0].store(l.to_bits(), atomic::Ordering::Relaxed);
+        self.0[1].store(r.to_bits(), atomic::Ordering::Relaxed);
+    }
+}
+
+impl kira::effect::Effect for StereoVolume {
+    fn process(&mut self, input: &mut [kira::Frame], _dt: f64, _info: &kira::info::Info<'_>) {
+        let left_amplitude = f32::from_bits(self.0[0].load(atomic::Ordering::Relaxed));
+        let right_amplitude = f32::from_bits(self.0[1].load(atomic::Ordering::Relaxed));
+        for frame in input {
+            frame.left *= left_amplitude;
+            frame.right *= right_amplitude;
+        }
+    }
+}
+
+impl kira::effect::EffectBuilder for StereoVolume {
+    type Handle = Self;
+
+    fn build(self) -> (Box<dyn kira::effect::Effect>, Self::Handle) {
+        (Box::new(self.clone()), self)
     }
 }
--- all-is-cubes-wasm/src/audio.rs
+++ all-is-cubes-wasm/src/audio.rs
@@ -1,13 +1,19 @@
+//! Glue between [`all_is_cubes::sound`] and Web Audio API.
+//!
+//! Note: This contains extremely similar code to the desktop version at
+//! `all-is-cubes-desktop/src/audio.rs`. Most changes should apply to both.
+
 use core::fmt;
 use std::collections::HashMap;
 use std::sync::atomic;
 
+use rand::{Rng as _, SeedableRng as _};
 use wasm_bindgen::JsValue;
-use web_sys::{AudioBuffer, AudioContext};
+use web_sys::{AudioBuffer, AudioContext, GainNode};
 
 use all_is_cubes::fluff::Fluff;
-use all_is_cubes::listen::{self, Listener as _};
-use all_is_cubes::sound::SoundDef;
+use all_is_cubes::listen::{self, Listen as _, Listener as _};
+use all_is_cubes::sound::{self, SoundDef};
 
 use crate::web_session::Session;
 
@@ -24,10 +30,16 @@ pub(crate) fn initialize_audio(session: &Session) -> Result<listen::Gate, JsValu
     // Transfers audio events to the audio processing task.
     let (sender, receiver) = flume::bounded(256);
 
-    wasm_bindgen_futures::spawn_local(audio_command_task(receiver, context));
+    // Hook up ambient sound to channel
+    let ambient_source = session.ambient_sound();
+    ambient_source.listen(UpdateAmbientListener::new(sender.clone()));
+    sender.send(AudioCommand::UpdateAmbient).unwrap(); // ensure initial sync
 
     let (gate, listener) = FluffListener::new(sender).gate();
     session.listen_fluff(listener);
+
+    wasm_bindgen_futures::spawn_local(audio_command_task(receiver, context, ambient_source));
+
     Ok(gate)
 }
 
@@ -36,12 +48,25 @@ pub(crate) fn initialize_audio(session: &Session) -> Result<listen::Gate, JsValu
 #[derive(Debug)]
 enum AudioCommand {
     Fluff(Fluff),
+    UpdateAmbient,
 }
 
 /// Async task for receiving commands, synthesizing audio, and managing the [`AudioContext`].
-async fn audio_command_task(receiver: flume::Receiver<AudioCommand>, context: AudioContext) {
+async fn audio_command_task(
+    receiver: flume::Receiver<AudioCommand>,
+    context: AudioContext,
+    ambient_source: listen::DynSource<sound::SpatialAmbient>,
+) {
     let sample_rate = context.sample_rate().round() as u32;
     let mut sound_cache: HashMap<SoundDef, AudioBuffer> = HashMap::new();
+
+    let mut ambient_noise_gains = match make_ambient_sound_synthesizer(sample_rate, &context) {
+        Ok(g) => Some(g),
+        Err(e) => {
+            log::error!("error initializing ambient sound: {e:?}");
+            None
+        }
+    };
 
     // This loop will exit when the channel is closed, which will happen when the `FluffListener`
     // or the `Gate` is dropped.
@@ -86,10 +111,78 @@ async fn audio_command_task(receiver: flume::Receiver<AudioCommand>, context: Au
                     // log::trace!("played {fluff:?}");
                 }
             }
+            AudioCommand::UpdateAmbient => {
+                let new_ambient = ambient_source.get();
+
+                let Some(ambient_noise_gains) = &mut ambient_noise_gains else {
+                    continue;
+                };
+                for (left_amplitude, right_amplitude, gain_nodes) in itertools::izip!(
+                    new_ambient.left.noise_bands.0.iter(),
+                    new_ambient.right.noise_bands.0.iter(),
+                    ambient_noise_gains.iter_mut()
+                ) {
+                    let [l, r] = gain_nodes;
+                    // TODO: smoothing
+                    l.gain().set_value(left_amplitude.into_inner());
+                    r.gain().set_value(right_amplitude.into_inner());
+                }
+            }
         }
     }
 
     log::trace!("audio_command_task's channel closed");
+}
+
+fn make_ambient_sound_synthesizer(
+    sample_rate: u32,
+    context: &AudioContext,
+) -> Result<[[GainNode; 2]; sound::Band::COUNT], JsValue> {
+    // Errors that are logic bugs are unwrapped and errors that might be resource
+    // exhaustion are not.
+
+    // Node that outputs white noise for use by ambient sound synthesis.
+    let white_noise_node = context.create_buffer_source()?;
+    white_noise_node.set_buffer(Some(&white_noise(sample_rate, 50_000)?));
+    white_noise_node.set_loop(true);
+    white_noise_node.start()?;
+
+    let ambient_noise_gains: [[GainNode; 2]; sound::Band::COUNT] =
+        try_array_from_fn(|band_index| -> Result<[GainNode; 2], JsValue> {
+            // Band-pass filter node to produce filter node.
+            // TODO: start with band-limited noise instead of realtime filtering?
+            let frequency: f32 =
+                sound::Band::from_index(band_index).center_frequency().into_inner();
+            let filter_node = context.create_biquad_filter()?;
+            white_noise_node.connect_with_audio_node(&filter_node).expect("connection");
+            filter_node.set_type(web_sys::BiquadFilterType::Bandpass);
+            filter_node.frequency().set_value(frequency);
+            filter_node.q().set_value(0.8); // TODO: standardize this instead of each backend setting it
+
+            // Merge independent channel gain nodes into one stereo output
+            let stereo_merge = context.create_channel_merger_with_number_of_inputs(2)?;
+            stereo_merge
+                .connect_with_audio_node(&context.destination())
+                .expect("connection");
+
+            let gain_nodes: [GainNode; 2] =
+                try_array_from_fn(|channel| -> Result<GainNode, JsValue> {
+                    let gain_node = context.create_gain()?;
+                    filter_node.connect_with_audio_node(&gain_node).expect("connection");
+                    gain_node
+                        .connect_with_audio_node_and_output_and_input(
+                            &stereo_merge,
+                            0,
+                            channel as u32,
+                        )
+                        .expect("connection");
+                    Ok(gain_node)
+                })?;
+
+            Ok(gain_nodes)
+        })?;
+
+    Ok(ambient_noise_gains)
 }
 
 // -------------------------------------------------------------------------------------------------
@@ -140,14 +233,65 @@ impl listen::Listener<Fluff> for FluffListener {
 
 // -------------------------------------------------------------------------------------------------
 
+/// Adapter from [`Source`] to the audio command channel.
+struct UpdateAmbientListener {
+    sender: flume::Sender<AudioCommand>,
+    alive: atomic::AtomicBool,
+}
+impl UpdateAmbientListener {
+    fn new(sender: flume::Sender<AudioCommand>) -> Self {
+        Self {
+            sender,
+            alive: atomic::AtomicBool::new(true),
+        }
+    }
+}
+
+impl fmt::Debug for UpdateAmbientListener {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("UpdateAmbientListener")
+            .field("alive", &self.alive)
+            .finish_non_exhaustive()
+    }
+}
+
+impl listen::Listener<()> for UpdateAmbientListener {
+    fn receive(&self, messages: &[()]) -> bool {
+        if !self.alive.load(atomic::Ordering::Relaxed) {
+            return false;
+        }
+        if messages.is_empty() {
+            return true;
+        }
+        match self.sender.try_send(AudioCommand::UpdateAmbient) {
+            Ok(()) => {}
+            Err(flume::TrySendError::Full(_)) => {
+                // If the channel is full, indicating the audio task is falling behind,
+                // drop the message.
+            }
+            Err(flume::TrySendError::Disconnected(_)) => {
+                self.alive.store(false, atomic::Ordering::Relaxed);
+                return false;
+            }
+        }
+        true
+    }
+}
+
+// -------------------------------------------------------------------------------------------------
+
 fn convert_sound_to_buffer(sample_rate: u32, sound: &SoundDef) -> Result<AudioBuffer, JsValue> {
     let (left_vec, right_vec): (Vec<f32>, Vec<f32>) =
         sound.synthesize(sample_rate as f32).map(|[l, r]| (l, r)).unzip();
 
-    let options =
-        web_sys::AudioBufferOptions::new(left_vec.len().try_into().unwrap(), sample_rate as f32);
-    options.set_number_of_channels(2);
-    let buffer = AudioBuffer::new(&options)?; // note: error could be OOM error
+    let buffer = AudioBuffer::new(&{
+        let options = web_sys::AudioBufferOptions::new(
+            left_vec.len().try_into().unwrap(),
+            sample_rate as f32,
+        );
+        options.set_number_of_channels(2);
+        options
+    })?; // note: error could be OOM error
 
     buffer
         .copy_to_channel(left_vec.as_slice(), 0)
@@ -157,4 +301,40 @@ fn convert_sound_to_buffer(sample_rate: u32, sound: &SoundDef) -> Result<AudioBu
         .expect("channel buffer length error");
 
     Ok(buffer)
+}
+
+fn white_noise(sample_rate: u32, sample_count: u32) -> Result<AudioBuffer, JsValue> {
+    let mut rng = rand_xoshiro::Xoshiro256Plus::seed_from_u64(0);
+
+    let buffer = AudioBuffer::new(&{
+        let options = web_sys::AudioBufferOptions::new(sample_count, sample_rate as f32);
+        options.set_number_of_channels(1);
+        options
+    })?;
+
+    #[expect(clippy::map_with_unused_argument_over_ranges)]
+    let vec: Vec<f32> = (0..sample_count).map(|_| rng.random()).collect();
+
+    buffer.copy_to_channel(vec.as_slice(), 0).expect("channel buffer length error");
+
+    Ok(buffer)
+}
+
+// -------------------------------------------------------------------------------------------------
+
+// Stable substitute for <https://doc.rust-lang.org/std/array/fn.try_from_fn.html>.
+//
+// Caveat: Will call the function N times even if it fails.
+fn try_array_from_fn<T, E, const N: usize>(
+    function: impl FnMut(usize) -> Result<T, E>,
+) -> Result<[T; N], E> {
+    let array_of_results: [Result<T, E>; N] = core::array::from_fn(function);
+    if array_of_results.iter().any(|result| result.is_err()) {
+        Err(array_of_results.into_iter().find_map(Result::err).unwrap())
+    } else {
+        Ok(array_of_results.map(|result| match result {
+            Ok(value) => value,
+            Err(_) => unreachable!(),
+        }))
+    }
 }
