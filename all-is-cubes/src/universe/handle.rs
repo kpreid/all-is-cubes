a4598aaec246cdc0c04932d548f2c38babb78812
Kevin Reid <kpreid@switchb.org>
2025-11-24 09:22:22 -0800

    [unstable-rust] Use `feature(fmt_debug)` cfgs to allow testing with -Zfmt-debug.

--- all-is-cubes/src/universe/handle.rs
+++ all-is-cubes/src/universe/handle.rs
@@ -1297,6 +1297,7 @@ mod tests {
     use core::hash::BuildHasher as _;
     use pretty_assertions::assert_eq;
 
+    #[cfg(fmt_debug = "full")]
     #[test]
     fn handle_debug_in_universe() {
         let mut u = Universe::new();
@@ -1315,6 +1316,7 @@ mod tests {
         assert_eq!(format!("{erased:?}"), "Handle('foo')");
     }
 
+    #[cfg(fmt_debug = "full")]
     #[test]
     fn handle_debug_pending() {
         let mut txn = UniverseTransaction::default();
