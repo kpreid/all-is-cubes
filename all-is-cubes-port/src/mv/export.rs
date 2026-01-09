e8373b2aed2f2cfbf4e2e48b0b4bc45dc7405ec2
Kevin Reid <kpreid@switchb.org>
2026-01-08 19:38:58 -0800

    port: Add `export_space_as_scene()` for .vox.
    This function isn’t properly working and isn’t used yet.

--- all-is-cubes-port/src/mv/export.rs
+++ all-is-cubes-port/src/mv/export.rs
@@ -1,5 +1,8 @@
-use all_is_cubes::space::Space;
-use all_is_cubes::universe::ReadTicket;
+use std::collections::HashMap;
+
+use all_is_cubes::block::Resolution;
+use all_is_cubes::space::{self, Space};
+use all_is_cubes::universe::{Handle, ReadTicket};
 use all_is_cubes::util::YieldProgress;
 
 use crate::{ExportError, ExportSet, Format, mv};
@@ -66,4 +69,104 @@ pub(crate) async fn export_to_dot_vox_data(
         layers: Vec::new(),
         materials: Vec::new(),
     })
+}
+
+/// Rather than exporting a space as a voxel model, this exports a space as a scene containing
+/// a model per block.
+#[expect(dead_code, reason = "TODO: This is not functional and not used yet.")]
+async fn export_space_as_scene(
+    progress: YieldProgress,
+    read_ticket: ReadTicket<'_>,
+    space_handle: &Handle<Space>,
+    resolution: Resolution,
+    models_out: &mut Vec<dot_vox::Model>,
+    palette_out: &mut Vec<dot_vox::Color>,
+    scene_out: &mut Vec<dot_vox::SceneNode>,
+) -> Result<(), ExportError> {
+    let space = space_handle.read(read_ticket)?;
+    let [convert_blocks_progress, convert_space_progress] = progress.split(0.5);
+
+    let mut block_index_to_model_index: HashMap<space::BlockIndex, u32> = HashMap::new();
+    for ((mut p, block_data), block_index) in convert_blocks_progress
+        .split_evenly(space.block_data().len())
+        .zip(space.block_data())
+        .zip(0..=space::BlockIndex::MAX)
+    {
+        p.set_label(format!("Exporting block model {block_index}"));
+        p.progress(0.0).await;
+
+        block_index_to_model_index.insert(block_index, u32::try_from(models_out.len()).unwrap());
+        models_out.push(mv::model::from_block(
+            space_handle.name(), // TODO: use block's Indirect handle name if it has one.
+            block_data.evaluated(),
+            palette_out,
+        )?);
+
+        p.finish().await;
+    }
+
+    // the root must exist at index 0
+    scene_out.push(dot_vox::SceneNode::Group {
+        attributes: dot_vox::Dict::default(),
+        children: Vec::new(),
+    });
+
+    for cube in space.bounds().interior_iter() {
+        let block_index = space.get_block_index(cube).unwrap();
+        if let Some(&model_id) = block_index_to_model_index.get(&block_index) {
+            let index_of_transform_node = u32::try_from(scene_out.len()).unwrap();
+
+            let translation = cube.lower_bounds() * i32::from(resolution);
+
+            scene_out.push(dot_vox::SceneNode::Transform {
+                attributes: dot_vox::Dict::default(),
+                frames: vec![dot_vox::Frame {
+                    attributes: dot_vox::Dict::from([
+                        (
+                            String::from("_t"),
+                            format!("{} {} {}", translation.x, translation.y, translation.z),
+                        ),
+                        (
+                            String::from("_r"),
+                            String::from("2"), // identity matrix -- TODO: confirm this is the right value
+                        ),
+                    ]),
+                }],
+                child: index_of_transform_node + 1, // shape node
+                layer_id: 0,
+            });
+            scene_out.push(dot_vox::SceneNode::Shape {
+                attributes: dot_vox::Dict::default(),
+                models: vec![dot_vox::ShapeModel {
+                    model_id,
+                    attributes: dot_vox::Dict::new(),
+                }],
+            });
+
+            let dot_vox::SceneNode::Group {
+                children: ref mut root_children,
+                ..
+            } = scene_out[0]
+            else {
+                unreachable!()
+            };
+            root_children.push(index_of_transform_node); // transform node
+        }
+    }
+    convert_space_progress.finish().await;
+
+    Ok(())
+}
+
+#[expect(
+    dead_code,
+    reason = "TODO: to be used to decide how to apply export_space_as_scene()"
+)]
+fn maximum_resolution_of_all_blocks(space: &space::Read<'_>) -> Resolution {
+    space
+        .block_data()
+        .iter()
+        .map(|bd| bd.evaluated().resolution())
+        .max()
+        .unwrap_or(Resolution::R1)
 }
