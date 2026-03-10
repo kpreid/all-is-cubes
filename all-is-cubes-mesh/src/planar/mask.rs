da3f467c37eec70f5f6fe328346f93a0922517e8
Kevin Reid <kpreid@switchb.org>
2026-03-10 13:16:34 -0700

    mesh: Add custom debug formatting to `planar::Mask`.

--- all-is-cubes-mesh/src/planar/mask.rs
+++ all-is-cubes-mesh/src/planar/mask.rs
@@ -1,3 +1,4 @@
+use core::fmt;
 use core::ops;
 
 #[cfg(doc)]
@@ -20,8 +21,7 @@ use crate::planar::Vertex;
 /// ┆    ↙↓           ┆           ↓↘
 /// ┆
 /// └┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄→ sweep direction</pre>
-// TODO: custom Debug
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
 pub struct Mask {
     /// Bit values corresponding to quadrants:
     /// * 1 = forward sweep & forward perpendicular
@@ -53,6 +53,68 @@ impl Mask {
     }
 }
 
+/// When formatted, [`Mask`] is depicted as if the sweep direction is rightward
+/// and the perpendicular direction is upward.
+/// Alignment and width options are respected.
+//---
+// Note: Why so many features?
+// So that anyone stuck debugging why masks are wrong has plenty of textual
+// and visual information to work with (but also for fun).
+impl fmt::Debug for Mask {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let name = match self.flags {
+            0b0000 => "EMPTY",
+            0b0001 => "FSFP",
+            0b0010 => "FSBP",
+            0b0011 => "FSFP | FSBP",
+            0b0100 => "BSFP",
+            0b0101 => "FSFP | BSFP",
+            0b0110 => "FSBP | BSFP",
+            0b0111 => "FSFP | FSBP | BSFP",
+            0b1000 => "BSBP",
+            0b1001 => "FSFP | BSBP",
+            0b1010 => "FSBP | BSBP",
+            0b1011 => "FSFP | FSBP | BSBP",
+            0b1100 => "BSFP | BSBP",
+            0b1101 => "FSFP | BSFP | BSBP",
+            0b1110 => "FSBP | BSFP | BSBP",
+            0b1111 => "FSFP | FSBP | BSFP | BSBP",
+            _ => unreachable!(),
+        };
+        let graphic = match self.flags {
+            0b0000 => " ",
+            0b0001 => "▝",
+            0b0010 => "▗",
+            0b0011 => "▐",
+            0b0100 => "▘",
+            0b0101 => "▀",
+            0b0110 => "▚",
+            0b0111 => "▜",
+            0b1000 => "▖",
+            0b1001 => "▞",
+            0b1010 => "▄",
+            0b1011 => "▟",
+            0b1100 => "▌",
+            0b1101 => "▛",
+            0b1110 => "▙",
+            0b1111 => "█",
+            _ => unreachable!(),
+        };
+        match f.align() {
+            Some(fmt::Alignment::Right) => {
+                f.pad(name)?;
+                write!(f, " ({graphic})")?;
+            }
+            // otherwise use left alignment (center is not supported)
+            _ => {
+                write!(f, "({graphic}) ")?;
+                f.pad(name)?;
+            }
+        }
+        Ok(())
+    }
+}
+
 impl ops::BitOr for Mask {
     type Output = Self;
     #[inline]
@@ -76,5 +138,27 @@ impl ops::Not for Mask {
     #[inline]
     fn not(self) -> Self::Output {
         Self { flags: !self.flags }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::string::String;
+
+    #[test]
+    fn debug() {
+        fn f(mask: Mask) -> String {
+            format!("{mask:>11?} {mask:<11?}")
+        }
+        assert_eq!(f(Mask::EMPTY), "      EMPTY ( ) ( ) EMPTY      ");
+        assert_eq!(f(Mask::FSFP), "       FSFP (▝) (▝) FSFP       ");
+        assert_eq!(f(Mask::FSBP), "       FSBP (▗) (▗) FSBP       ");
+        assert_eq!(f(Mask::BSFP), "       BSFP (▘) (▘) BSFP       ");
+        assert_eq!(f(Mask::BSBP), "       BSBP (▖) (▖) BSBP       ");
+        assert_eq!(
+            f(Mask::FSFP | Mask::BSBP),
+            "FSFP | BSBP (▞) (▞) FSFP | BSBP"
+        );
     }
 }
