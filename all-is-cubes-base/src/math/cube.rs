kpreid/all-is-cubes

commit b0b6aeabb4f766d09a98148d77079592e74376ea

block: Add an explicit resolution to `Move`.
This means that `Move` no longer needs to guess, badly, what resolution
it should produce, and the user-provided parameters no longer have an
arbitrary scale/denominator.

all-is-cubes/src/block/modifier/move.rs
@@ -1,8 +1,7 @@
 use core::assert_matches;
 
-use crate::block::TickAction;
 use crate::block::{
-    self, AIR, Block, BlockAttributes, Evoxel, Evoxels, MinEval, Modifier, Resolution::R16,
+    self, AIR, Block, BlockAttributes, Evoxel, Evoxels, MinEval, Modifier, Resolution, TickAction,
 };
 use crate::math::{Face, GridAab, GridCoordinate, GridRotation, GridVector, Vol};
 use crate::op::Operation;
@@ -27,12 +26,15 @@ pub struct Move {
     /// The direction in which the block is displaced.
     pub direction: Face,
 
-    /// The distance, in 1/256ths, by which it is displaced.
-    pub distance: u16,
+    /// The granularity in which the displacement is expressed.
+    pub resolution: Resolution,
+
+    /// The distance, in units of `self.resolution`, by which it is displaced.
+    pub distance: u8,
 
     /// The amount by which `self.distance` is changing every time
     /// `self.schedule` fires.
-    pub velocity: i16,
+    pub velocity: i8,
 
     /// When to apply the velocity.
     ///
@@ -42,9 +44,10 @@ pub struct Move {
 
 impl Move {
     /// TODO: make a cleaner, less internals-ish constructor
-    pub fn new(direction: Face, distance: u16, velocity: i16) -> Self {
+    pub fn new(direction: Face, resolution: Resolution, distance: u8, velocity: i8) -> Self {
         Self {
             direction,
+            resolution,
             distance,
             velocity,
             schedule: time::Schedule::EVERY_TICK,
@@ -72,7 +75,8 @@ impl Move {
     pub fn complement(&self) -> Self {
         Move {
             direction: self.direction.opposite(),
-            distance: 256 - self.distance,
+            resolution: self.resolution,
+            distance: u8::from(self.resolution) - self.distance,
             velocity: -self.velocity,
             schedule: self.schedule,
         }
@@ -95,6 +99,7 @@ impl Move {
     ) -> Result<MinEval, block::InEvalError> {
         let Move {
             direction,
+            resolution: movement_resolution,
             distance,
             velocity,
             schedule,
@@ -106,25 +111,28 @@ impl Move {
         // but that will involve some sort of predicate and transformation on tick actions.)
         input = block::Quote::default().evaluate(input, filter)?;
 
-        // TODO: short-circuit case when distance is 0
+        // TODO: short-circuit case when distance is 0 (but don't break animation)
 
         let (input_attributes, input_voxels) = input.into_parts();
-        let (original_bounds, effective_resolution) = match input_voxels.single_voxel() {
-            None => (input_voxels.bounds(), input_voxels.resolution()),
-            // Treat atom blocks as having a resolution of 16. TODO: Improve on this hardcoded constant
-            Some(_) => (GridAab::for_block(R16), R16),
-        };
 
-        // For now, our strategy is to work in units of the block's resolution.
-        // TODO: Generalize to being able to increase resolution to a chosen minimum.
-        let distance_in_res =
-            GridCoordinate::from(distance) * GridCoordinate::from(effective_resolution) / 256;
+        // The resolution of the output evaluation must be at least the resolution of the voxels
+        // being moved, and also at least the resolution of the movement.
+        let output_resolution = input_voxels.resolution().max(movement_resolution);
+        let resolution_increase = (output_resolution / input_voxels.resolution()).unwrap();
+
+        // Bounds of the input, expressed in the resolution of the output.
+        let original_bounds_in_res =
+            input_voxels.bounds().multiply(GridCoordinate::from(resolution_increase));
+
+        let distance_in_res = GridCoordinate::from(distance)
+            * GridCoordinate::from(output_resolution)
+            / GridCoordinate::from(movement_resolution);
         let translation_in_res = direction.vector(distance_in_res);
 
         // This will be None if the displacement puts the block entirely out of view.
-        let displaced_bounds: Option<GridAab> = original_bounds
+        let displaced_bounds: Option<GridAab> = original_bounds_in_res
             .translate(translation_in_res)
-            .intersection_cubes(GridAab::for_block(effective_resolution));
+            .intersection_cubes(GridAab::for_block(output_resolution));
 
         let animation_op: Option<Operation> = if displaced_bounds.is_none() && velocity >= 0 {
             // Displaced to invisibility; turn into just plain air.
@@ -194,7 +202,7 @@ impl Move {
                     None => {
                         let voxels = input_voxels.as_vol_ref();
                         Evoxels::from_many(
-                            effective_resolution,
+                            output_resolution,
                             Vol::from_fn(displaced_bounds, |cube| {
                                 voxels[cube - translation_in_res]
                             }),
@@ -205,7 +213,7 @@ impl Move {
                         // TODO: Also synthesize if the resolution is merely low
                         // compared to the velocity.
                         Evoxels::from_many(
-                            effective_resolution,
+                            output_resolution,
                             Vol::from_fn(displaced_bounds, |_| voxel),
                         )
                     }
@@ -227,6 +235,7 @@ impl universe::VisitHandles for Move {
     fn visit_handles(&self, _visitor: &mut dyn universe::HandleVisitor) {
         let Move {
             direction: _,
+            resolution: _,
             distance: _,
             velocity: _,
             schedule: _,
@@ -247,16 +256,18 @@ mod tests {
 
     #[test]
     fn move_atom_block_evaluation() {
+        let resolution = R2;
         let color = rgba_const!(1.0, 0.0, 0.0, 1.0);
         let original = Block::from(color);
         let moved = original.clone().with_modifier(Move {
             direction: Face::PY,
-            distance: 128, // distance 1/2 block × scale factor of 256
+            resolution,
+            distance: 1,
             velocity: 0,
             schedule: time::Schedule::EVERY_TICK,
         });
 
-        let expected_bounds = GridAab::from_lower_size([0, 8, 0], [16, 8, 16]);
+        let expected_bounds = GridAab::from_lower_size([0, 1, 0], [2, 1, 2]);
 
         let ev_original = original.evaluate(ReadTicket::stub()).unwrap();
         assert_eq!(
@@ -265,7 +276,7 @@ mod tests {
                 block: moved,
                 attributes: ev_original.attributes.clone(),
                 voxels: Evoxels::from_many(
-                    R16,
+                    resolution,
                     Vol::repeat(expected_bounds, Evoxel::from_block(&ev_original))
                 ),
                 cost: block::Cost {
@@ -288,7 +299,7 @@ mod tests {
                     visible: true,
                     uniform_collision: None,
                     voxel_opacity_mask: VoxelOpacityMask::new_raw(
-                        R16,
+                        resolution,
                         Vol::repeat(expected_bounds, OpacityCategory::Opaque)
                     ),
                 }
@@ -308,7 +319,8 @@ mod tests {
 
         let moved = original.clone().with_modifier(Move {
             direction: Face::PY,
-            distance: 128, // distance 1/2 block × scale factor of 256
+            resolution,
+            distance: 1,
             velocity: 0,
             schedule: time::Schedule::EVERY_TICK,
         });
@@ -364,7 +376,8 @@ mod tests {
             .build();
         let moved = original.with_modifier(Move {
             direction: Face::PY,
-            distance: 128,
+            resolution: R2,
+            distance: 1,
             velocity: 0,
             schedule: time::Schedule::EVERY_TICK,
         });
@@ -378,11 +391,11 @@ mod tests {
     /// Set up a `Modifier::Move`, let it run, and then allow assertions to be made about the result.
     fn move_block_test(
         direction: Face,
-        velocity: i16,
+        velocity: i8,
         checker: impl FnOnce(space::Read<'_>, &Block),
     ) {
         let [block] = make_some_blocks();
-        let [move_out, move_in] = Move::new(direction, 0, velocity).into_paired();
+        let [move_out, move_in] = Move::new(direction, R16, 0, velocity).into_paired();
         let space = Space::builder(GridAab::from_lower_upper([-1, -1, -1], [2, 2, 2]))
             .build_and_mutate(|m| {
                 m.set([0, 0, 0], block.clone().with_modifier(move_out))?;
@@ -421,7 +434,7 @@ mod tests {
 
     #[test]
     fn velocity_whole_cube_in_one_tick() {
-        move_block_test(Face::PX, 256, |space, block| {
+        move_block_test(Face::PX, 16, |space, block| {
             assert_eq!(&space[[0, 0, 0]], &AIR);
             assert_eq!(&space[[1, 0, 0]], block);
         });
@@ -439,6 +452,7 @@ mod tests {
             .clone()
             .with_modifier(Move {
                 direction: Face::PX,
+                resolution: R32,
                 distance: 10,
                 velocity: 10,
                 schedule: time::Schedule::EVERY_TICK,
@@ -448,6 +462,7 @@ mod tests {
         let expected_after_tick = base
             .with_modifier(Move {
                 direction: Face::PX,
+                resolution: R32,
                 distance: 20,
                 velocity: 10,
                 schedule: time::Schedule::EVERY_TICK,
@@ -471,6 +486,7 @@ mod tests {
             .clone()
             .with_modifier(Move {
                 direction: Face::PX,
+                resolution: R32,
                 distance: 10,
                 velocity: 10,
                 schedule: time::Schedule::EVERY_TICK,
@@ -480,6 +496,7 @@ mod tests {
         let expected_after_tick = base
             .with_modifier(Move {
                 direction: Face::PX,
+                resolution: R32,
                 distance: 20,
                 velocity: 10,
                 schedule: time::Schedule::EVERY_TICK,
@@ -501,6 +518,7 @@ mod tests {
         let block = extra.clone().with_modifier(Composite::new(
             base.clone().with_modifier(Move {
                 direction: Face::PX,
+                resolution: R32,
                 distance: 10,
                 velocity: 10,
                 schedule: time::Schedule::EVERY_TICK,
@@ -511,6 +529,7 @@ mod tests {
         let expected_after_tick = extra.with_modifier(Composite::new(
             base.with_modifier(Move {
                 direction: Face::PX,
+                resolution: R32,
                 distance: 20,
                 velocity: 10,
                 schedule: time::Schedule::EVERY_TICK,

all-is-cubes-content/src/atrium/mod.rs
@@ -307,7 +307,7 @@ fn atrium_non_async(
         }
 
         if MOVING_LIGHT {
-            let mut movement = block::Move::new(Face::PZ, 16, 16);
+            let mut movement = block::Move::new(Face::PZ, block::Resolution::R16, 1, 1);
             movement.schedule = time::Schedule::from_period(NonZero::new(2).unwrap());
             let tick_action = block::TickAction {
                 operation: crate::animation::back_and_forth_movement(movement),

all-is-cubes-content/src/blocks.rs
@@ -723,7 +723,7 @@ fn demo_blocks_generator(
                 .tick_action(TickAction {
                     operation: Operation::Alt(
                         [
-                            Operation::StartMove(block::Move::new(Face::PY, 32, 32)),
+                            Operation::StartMove(block::Move::new(Face::PY, R8, 1, 1)),
                             // if we can't move, vanish
                             Operation::Become(AIR),
                         ]

all-is-cubes-content/src/city/exhibits/move_modifier.rs
@@ -16,12 +16,13 @@ fn MOVED_BLOCKS(ctx: Context<'_>) {
             for x in 0..8 {
                 for z in 0..2 {
                     let i = x + z * 8;
-                    let distance = (i * 16).try_into().unwrap();
+                    let distance: u8 = i.try_into().unwrap();
                     let block = &blocks[i as usize];
 
                     // Vertical
                     {
-                        let [move_out, move_in] = Move::new(Face::PY, distance, 0).into_paired();
+                        let [move_out, move_in] =
+                            Move::new(Face::PY, R16, distance, 0).into_paired();
                         m.set(
                             [x * 2, 0, (1 - z) * 2],
                             block.clone().with_modifier(move_out),
@@ -34,7 +35,8 @@ fn MOVED_BLOCKS(ctx: Context<'_>) {
 
                     // Horizontal
                     {
-                        let [move_out, move_in] = Move::new(Face::PZ, distance, 0).into_paired();
+                        let [move_out, move_in] =
+                            Move::new(Face::PZ, R16, distance, 0).into_paired();
                         m.set([i, 0, -2], block.clone().with_modifier(move_out))?;
                         m.set([i, 0, -1], block.clone().with_modifier(move_in))?;
                     }
@@ -59,7 +61,7 @@ fn PROJECTILE(ctx: Context<'_>) {
 
     let projectile = &demo_blocks[DemoBlocks::Projectile];
 
-    let [move_in, _move_out] = Move::new(Face::NY, 256, -32).into_paired();
+    let [move_in, _move_out] = Move::new(Face::NY, R16, 16, -2).into_paired();
     let projectile_moving_in = projectile.clone().with_modifier(move_in);
     // let projectile_moving_out = projectile.clone().with_modifier(move_out);
 

all-is-cubes-content/src/dungeon/demo_dungeon.rs
@@ -283,7 +283,7 @@ impl DemoTheme {
     // TODO: This should be a definition in the universe, but there's no way to do that with `Tool`
     // yet.
     fn make_key_tool(&self) -> Tool {
-        let move_modifier = block::Modifier::Move(block::Move::new(Face::NX, 0, 16));
+        let move_modifier = block::Modifier::Move(block::Move::new(Face::NX, R16, 0, 1));
         let move_gate = op::Operation::AddModifiers([move_modifier.clone()].into());
         let unlock_unrotated = op::Operation::Neighbors(
             [

all-is-cubes-ui/src/vui/widgets/button/draw.rs
@@ -311,7 +311,8 @@ fn draw_button_multiblock_from_image(
 fn shift_label_block(state: &impl ButtonBase, block: Block) -> Block {
     block.with_modifier(block::Move::new(
         Face::PZ,
-        (state.button_label_z() * 256 / theme::RESOLUTION_G) as u16,
+        theme::RESOLUTION,
+        state.button_label_z() as u8,
         0,
     ))
 }

all-is-cubes-ui/src/vui/widgets/progress_bar.rs
@@ -148,12 +148,14 @@ impl ProgressBarController {
                 let mask_substance = block::from_color!(Rgb01::WHITE);
                 let fill_mask = mask_substance.clone().with_modifier(block::Move::new(
                     Face::NX,
-                    u16::try_from((16 - partial_fill) * 16).unwrap(),
+                    block::Resolution::R16,
+                    u8::try_from(16 - partial_fill).unwrap(),
                     0,
                 ));
                 let empty_mask = mask_substance.with_modifier(block::Move::new(
                     Face::PX,
-                    u16::try_from(partial_fill * 16).unwrap(),
+                    block::Resolution::R16,
+                    u8::try_from(partial_fill).unwrap(),
                     0,
                 ));
 

all-is-cubes/src/block/eval/tests.rs
@@ -625,9 +625,17 @@ fn color_evaluation_regression_1() {
     let block = Block::builder()
         .color(Rgba::new(1e28, 1e28, 1e28, 1.0))
         // Modifier matters because it causes the block to become voxels
-        .modifier(Modifier::Move(modifier::Move::new(Face::NX, 0, 0)))
+        .modifier(Modifier::Move(modifier::Move::new(Face::NX, R16, 0, 0)))
         .build();
-    eval_out(&block);
+
+    // This should not panic.
+    let ev = eval_out(&block);
+
+    assert_ne!(
+        ev.resolution(),
+        R1,
+        "this test is not valid if Move no longer produces higher resolution"
+    );
 }
 
 /// Fuzz-discovered test case for a NaN sneaking in to a color.

commit 2686c6063a667b313254c65137526b9ab75e3a2c

block: Fix handling of `Move` resolution higher than block resolution.
This was broken by b0b6aeabb4f766d09a98148d77079592e74376ea introducing
the possibility of the output resolution being higher. The fix is to
handle it the same way as `Composite` does.

all-is-cubes-base/src/math/cube.rs
@@ -9,6 +9,7 @@ use crate::math::{
     Aab, Face, FreeCoordinate, FreePoint, FreeVector, GridAab, GridCoordinate, GridPoint,
     GridVector,
 };
+use crate::resolution::Resolution;
 use crate::util::ConciseDebug;
 
 /// “A cube”, in this documentation, is a unit cube whose corners' coordinates are integers.
@@ -297,6 +298,28 @@ mod arithmetic {
         #[inline]
         fn sub_assign(&mut self, rhs: Face) {
             *self -= rhs.normal_vector()
+        }
+    }
+
+    impl ops::Div<Resolution> for Cube {
+        type Output = Cube;
+
+        /// Produces the cube containing this cube in a grid scaled up by `scale`.
+        ///
+        /// Equivalently: performs what the Rust standard library calls `div_euclid` on the
+        /// `lower_bounds()` coordinates.
+        ///
+        /// ```
+        /// # use all_is_cubes_base::resolution::Resolution;
+        /// # use all_is_cubes_base::math::Cube;
+        ///
+        /// let cube = Cube::new(-17, 3, 33);
+        /// assert_eq!(cube / Resolution::R16, Cube::new(-2, 0, 2));
+        /// ```
+        #[inline]
+        fn div(self, scale: Resolution) -> Self::Output {
+            let shift = scale.log2();
+            self.map(|c| c >> shift)
         }
     }
 

all-is-cubes/src/block/modifier/composite.rs
@@ -235,11 +235,11 @@ fn evaluate_composition(
 
     let src_resolution = src_voxels.resolution();
     let dst_resolution = dst_voxels.resolution();
+    // TODO: This "max and then divide" pattern occurs both here and in `Modifier::Move`;
+    // add a function to express it without unwraps.
     let effective_resolution = src_resolution.max(dst_resolution);
-    let src_scale =
-        GridCoordinate::from(effective_resolution) / GridCoordinate::from(src_resolution);
-    let dst_scale =
-        GridCoordinate::from(effective_resolution) / GridCoordinate::from(dst_resolution);
+    let src_scale = (effective_resolution / src_resolution).unwrap();
+    let dst_scale = (effective_resolution / dst_resolution).unwrap();
 
     let src_bounds_scaled = bounds_excluding_air(&src_voxels, src_scale);
     let dst_bounds_scaled = bounds_excluding_air(&dst_voxels, dst_scale);
@@ -351,10 +351,9 @@ fn evaluate_composition(
         Evoxels::from_many(
             effective_resolution,
             Vol::from_fn(output_bounds, |cube| {
-                let p = cube.lower_bounds();
                 operator.blend_evoxel(
-                    src_voxels.get(Cube::from(p / src_scale)).unwrap_or(Evoxel::AIR),
-                    dst_voxels.get(Cube::from(p / dst_scale)).unwrap_or(Evoxel::AIR),
+                    src_voxels.get(cube / src_scale).unwrap_or(Evoxel::AIR),
+                    dst_voxels.get(cube / dst_scale).unwrap_or(Evoxel::AIR),
                 )
             }),
         )
@@ -366,11 +365,11 @@ fn evaluate_composition(
 /// Rescale the bounds of the input to the resolution of the output, but also, if the voxels are
 /// [`Evoxel::AIR`] and thus equivalent to out-of-bounds, substitute empty bounds.
 /// This way, we produce suitably tight bounds when one of the blocks is AIR.
-fn bounds_excluding_air(voxels: &Evoxels, src_scale: i32) -> GridAab {
+fn bounds_excluding_air(voxels: &Evoxels, src_scale: block::Resolution) -> GridAab {
     if voxels.single_voxel() == Some(Evoxel::AIR) {
         GridAab::ORIGIN_EMPTY
     } else {
-        voxels.bounds().multiply(src_scale)
+        voxels.bounds().multiply(GridCoordinate::from(src_scale))
     }
 }
 

all-is-cubes/src/block/modifier/move.rs
@@ -204,7 +204,7 @@ impl Move {
                         Evoxels::from_many(
                             output_resolution,
                             Vol::from_fn(displaced_bounds, |cube| {
-                                voxels[cube - translation_in_res]
+                                voxels[(cube - translation_in_res) / resolution_increase]
                             }),
                         )
                     }
@@ -302,7 +302,7 @@ mod tests {
     }
 
     #[test]
-    fn move_voxel_block_evaluation() {
+    fn move_voxel_block_evaluation_same_resolution() {
         let mut universe = Universe::new();
         let resolution = R2;
         let color = rgba_const!(1.0, 0.0, 0.0, 1.0);
@@ -352,6 +352,65 @@ mod tests {
                     uniform_collision: None,
                     voxel_opacity_mask: VoxelOpacityMask::new_raw(
                         resolution,
+                        Vol::repeat(expected_bounds, OpacityCategory::Opaque)
+                    ),
+                }
+            }
+        );
+    }
+
+    #[test]
+    fn move_voxel_block_evaluation_different_resolution() {
+        let mut universe = Universe::new();
+        let block_resolution = R2;
+        let move_resolution = R4;
+        let color = rgba_const!(1.0, 0.0, 0.0, 1.0);
+        let original = Block::builder()
+            .voxels_fn(block_resolution, |_| Block::from(color))
+            .unwrap()
+            .build_into(&mut universe);
+
+        let moved = original.clone().with_modifier(Move {
+            direction: Face::PY,
+            resolution: move_resolution,
+            distance: 1,
+            velocity: 0,
+            schedule: time::Schedule::EVERY_TICK,
+        });
+
+        let expected_bounds = GridAab::from_lower_size([0, 1, 0], [4, 3, 4]);
+
+        let ev_original = original.evaluate(universe.read_ticket()).unwrap();
+        assert_eq!(
+            moved.evaluate(universe.read_ticket()).unwrap(),
+            EvaluatedBlock {
+                block: moved,
+                attributes: ev_original.attributes.clone(),
+                cost: block::Cost {
+                    components: ev_original.cost.components + 1,
+                    voxels: 2 * 2 * 2 + 4 * 4 * 3, // original recur + 3/4 block of Move
+                    recursion: 0
+                },
+                voxels: Evoxels::from_many(
+                    move_resolution,
+                    Vol::repeat(expected_bounds, Evoxel::from_block(&ev_original))
+                ),
+                derived: block::Derived {
+                    color: color.to_rgb().with_alpha(zo32(20. / (6. * 4.))),
+                    face_colors: FaceMap {
+                        nx: color.to_rgb().with_alpha(zo32(0.75)),
+                        ny: color.to_rgb().with_alpha(zo32(1.0)),
+                        nz: color.to_rgb().with_alpha(zo32(0.75)),
+                        px: color.to_rgb().with_alpha(zo32(0.75)),
+                        py: color.to_rgb().with_alpha(zo32(1.0)),
+                        pz: color.to_rgb().with_alpha(zo32(0.75)),
+                    },
+                    light_emission: Rgb::ZERO,
+                    opaque: FaceMap::splat(false).with(Face::PY, true),
+                    visible: true,
+                    uniform_collision: None,
+                    voxel_opacity_mask: VoxelOpacityMask::new_raw(
+                        move_resolution,
                         Vol::repeat(expected_bounds, OpacityCategory::Opaque)
                     ),
                 }