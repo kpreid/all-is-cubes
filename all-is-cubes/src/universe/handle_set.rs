ea093e669b0cdd7597d6568f7dbc24d4746e0369
Kevin Reid <kpreid@switchb.org>
2026-07-18 14:10:39 -0700

    universe: Allow `Builtin` handles in a `HandleSet`.
    This will allow exporting the contents of builtins.

--- all-is-cubes/src/universe/handle_set.rs
+++ all-is-cubes/src/universe/handle_set.rs
@@ -53,18 +53,27 @@ impl<H: Into<universe::AnyHandle>> FromIterator<H> for HandleSet {
     /// Panics if the handles are not all from the same universe.
     #[track_caller]
     fn from_iter<T: IntoIterator<Item = H>>(iter: T) -> Self {
-        let mut universe_id = None;
+        let mut common_universe_id = None;
         let handles = iter
             .into_iter()
             .map(|handle| {
                 let handle = handle.into();
-                match (universe_id, handle.universe_id()) {
+                match (common_universe_id, handle.universe_id()) {
                     (Some(all), Some(this)) if all == this => {}
                     (Some(_), Some(_)) => {
-                        panic!("handles in a HandleSet must be in the same universe")
+                        panic!("a HandleSet may not contain handles in multiple universes")
                     }
-                    (None, Some(this)) => universe_id = Some(this),
-                    (_, None) => panic!("handles in a HandleSet must be in a universe"),
+                    (None, Some(this)) => common_universe_id = Some(this),
+                    (_, None) if matches!(handle.name(), universe::Name::Builtin(_)) => {
+                        // Builtin handles are always permitted.
+                        // TODO: Find a way to express that in general (e.g. `Handle::universe_id()`
+                        // returning more detail than an `Option`), rather than needing a rule here.
+                    }
+                    (_, None) => panic!(
+                        "handles in a HandleSet must be in a universe, but {handle:?} was not",
+                        // deref so as not to print the `AnyHandle` wrapper
+                        handle = &*handle
+                    ),
                 }
 
                 (handle.name(), handle)
@@ -107,17 +116,20 @@ impl Default for PartialUniverse<'_> {
 mod tests {
     use super::*;
     use crate::block;
+    use crate::universe::{ErasedHandle, Name};
     use alloc::vec;
+
+    fn test_block_def() -> block::BlockDef {
+        block::BlockDef::new(universe::ReadTicket::stub(), block::AIR)
+    }
+    fn names(set: &HandleSet) -> Vec<Name> {
+        set.handles.keys().cloned().collect()
+    }
 
     #[test]
     fn handle_set_extract_type() {
         let mut universe = Universe::new();
-        let foo = universe
-            .insert(
-                "foo".into(),
-                block::BlockDef::new(universe::ReadTicket::stub(), block::AIR),
-            )
-            .unwrap();
+        let foo = universe.insert("foo".into(), test_block_def()).unwrap();
         let bar = universe.insert("bar".into(), crate::tag::TagDef).unwrap();
         let mut handle_set = HandleSet::all_of(&universe);
 
@@ -126,5 +138,77 @@ mod tests {
         let tags = handle_set.extract_type::<crate::tag::TagDef>();
         assert_eq!(tags, vec![bar]);
         assert!(handle_set.is_empty());
+    }
+
+    #[test]
+    fn handle_set_may_have_builtins_only() {
+        // This call should not panic.
+        let handle_set =
+            HandleSet::from_iter([universe::Builtin::beep(), universe::Builtin::thump()]);
+
+        // Also check the contents
+        assert_eq!(
+            names(&handle_set),
+            vec![
+                Name::Builtin(universe::Builtin::Beep),
+                Name::Builtin(universe::Builtin::Thump)
+            ]
+        );
+    }
+
+    #[test]
+    fn handle_set_may_have_builtins_first() {
+        let mut universe = Universe::new();
+        let foo = universe.insert("foo".into(), test_block_def()).unwrap();
+
+        // This call should not panic.
+        let handle_set = HandleSet::from_iter([
+            universe::Builtin::beep().to_any_handle(),
+            foo.to_any_handle(),
+        ]);
+
+        assert_eq!(
+            names(&handle_set),
+            vec![foo.name(), Name::Builtin(universe::Builtin::Beep)]
+        );
+    }
+
+    #[test]
+    fn handle_set_may_have_builtins_second() {
+        let mut universe = Universe::new();
+        let foo = universe.insert("foo".into(), test_block_def()).unwrap();
+
+        // This call should not panic.
+        let handle_set = HandleSet::from_iter([
+            foo.to_any_handle(),
+            universe::Builtin::beep().to_any_handle(),
+        ]);
+
+        assert_eq!(
+            names(&handle_set),
+            vec![foo.name(), Name::Builtin(universe::Builtin::Beep)]
+        );
+    }
+
+    #[test]
+    #[should_panic = "a HandleSet may not contain handles in multiple universes"]
+    fn handle_set_may_not_have_two_universes() {
+        let mut universe1 = Universe::new();
+        let mut universe2 = Universe::new();
+        let foo = universe1.insert("foo".into(), test_block_def()).unwrap();
+        let bar = universe2.insert("bar".into(), test_block_def()).unwrap();
+
+        _ = HandleSet::from_iter([foo.to_any_handle(), bar.to_any_handle()]);
+    }
+
+    #[test]
+    #[should_panic = "handles in a HandleSet must be in a universe, but Handle('foo' in no universe) was not"]
+    fn handle_set_may_not_have_pending() {
+        let (handle, _txn) = universe::UniverseTransaction::insert(
+            "foo".into(),
+            block::BlockDef::new(universe::ReadTicket::stub(), block::AIR),
+        );
+
+        _ = HandleSet::from_iter([handle]);
     }
 }
