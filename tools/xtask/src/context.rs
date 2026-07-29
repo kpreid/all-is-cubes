kpreid/all-is-cubes

commit e60b8e7464ada7afe3f1ed0fa083f42e50cb6b24

Rust 1.97: Replace `RUSTFLAGS=-Dwarnings` with `build.warnings='deny'`.
Except when testing in CI on nightly, to avoid
`renamed_and_removed_lints` trouble.

tools/xtask/src/context.rs
@@ -16,6 +16,8 @@ pub struct Config<'a> {
     pub cargo_timings: bool,
     pub cargo_quiet: bool,
     pub time_log_quiet: bool,
+    /// Whether to use `RUSTFLAGS=-Dwarnings` instead of Cargo `build.warnings="deny"`.
+    pub use_d_warnings: bool,
     pub scope: Scope,
     pub main_metadata: cargo_metadata::Metadata,
     pub time_log_tx: std::sync::mpsc::Sender<Timing>,
@@ -85,26 +87,39 @@ impl Config<'_> {
 pub(crate) enum TestOrCheck {
     /// `cargo test`
     Test,
-    /// `cargo test --no-run`
+    /// `cargo test --no-run`.
+    /// Builds everything that [`Self::Test`] would run, as much as possible.
     BuildTests,
-    /// `cargo clippy`
+    /// `cargo clippy --config=build.warnings='deny'`
     Lint,
     /// Really `cargo check`, not `cargo clippy`, for efficiency when all we care about is
-    /// "does it build?"
+    /// "does it build?". Also, does not deny warnings.
     Check,
 }
 
 impl TestOrCheck {
     pub fn cargo_cmd<'a>(self, config: &'a Config<'_>) -> Cmd<'a> {
-        config
+        let mut cmd = config
             .cargo()
             .args(match self {
                 Self::Test => vec!["test"],
                 Self::BuildTests => vec!["test", "--no-run"],
-                Self::Lint => vec!["clippy"],
+                Self::Lint if config.use_d_warnings => vec!["clippy"],
+                Self::Lint => vec!["clippy", "--config=build.warnings='deny'"],
                 Self::Check => vec!["check"],
             })
-            .args(config.cargo_build_args())
+            .args(config.cargo_build_args());
+
+        if config.use_d_warnings {
+            let new_rustflags = match std::env::var("RUSTFLAGS") {
+                Ok(old_rustflags) => format!("{old_rustflags} -Dwarnings"),
+                Err(_) => String::from("-Dwarnings"),
+            };
+            eprintln!("[xtask] note: setting RUSTFLAGS to {new_rustflags}");
+            cmd = cmd.env("RUSTFLAGS", new_rustflags)
+        }
+
+        cmd
     }
 
     /// Return the cargo subcommand to use for the targets that we are *not* planning or able

tools/xtask/src/main.rs
@@ -45,12 +45,26 @@ mod development_files;
 
 // -------------------------------------------------------------------------------------------------
 
+/// Kludge: Certain behavior is tweaked when running in a CI environment.
+/// This constant centralizes where such detection is used.
+const CI: bool = option_env!("CI").is_some();
+
 fn main() -> Result<(), ActionError> {
     let sh = &Shell::new()?;
 
     let main_metadata = cargo_metadata::MetadataCommand::new()
         .manifest_path(PROJECT_DIR.join("Cargo.toml"))
         .exec()?;
+
+    // Can't use build.warnings when linting on unstable/future toolchains, because they might
+    // have new `renamed_and_removed_lints` that should be ignored:
+    //   <https://github.com/rust-lang/cargo/issues/17193>
+    // TODO
+    let use_d_warnings = CI && {
+        let rustc_version_output = sh.cmd("rustc").arg("--version").output().unwrap().stdout;
+        let rustc_version_output = str::from_utf8(&rustc_version_output).unwrap();
+        rustc_version_output.contains("nightly") || rustc_version_output.contains("beta")
+    };
 
     // Using a channel for the time log instead of a `Vec<Timing>` is overkill,
     // but neatly avoids borrow conflicts.
@@ -68,6 +82,7 @@ fn main() -> Result<(), ActionError> {
             cargo_timings: timings,
             cargo_quiet: quiet,
             time_log_quiet: quiet,
+            use_d_warnings,
             scope,
             main_metadata,
             time_log_tx,
@@ -169,9 +184,10 @@ fn run_command(config: &Config<'_>, command: XtaskCommand) -> Result<(), ActionE
             print_revision(config, false);
             do_for_all_packages(
                 config,
-                // no Clippy for better throughput; pragmatically we care about "does it build"
+                // No Clippy for better throughput; pragmatically we care about "does it build"
                 // much more than the more pedantic lints, and this will still give us dead code
-                // and unused imports warnings
+                // and unused imports warnings.
+                // Note that this does not fail on warnings.
                 TestOrCheck::Check,
                 Features::Powerset,
             )?;
@@ -785,8 +801,8 @@ fn build_documentation(config: &Config<'_>) -> Result<(), ActionError> {
     let _t = config.capture_time("doc");
     config
         .cargo()
-        .env("RUSTDOCFLAGS", "-Dwarnings")
         .arg("doc")
+        .arg("--config=build.warnings='deny'")
         .args(config.cargo_build_args())
         .run()?;
     Ok(())

.github/workflows/ci.yml
@@ -277,10 +277,8 @@ jobs:
 
   # Run `xtask lint`.
   #
-  # This job is separate from the main "build" job so that:
-  # * Lint results are delivered quickly by running them in parallel with other jobs.
-  # * We build with `-Dwarnings` so the failures aren't silent, but don't want this to
-  #   cause *re*building in the main build job and its cache.
+  # This job is separate from the main "build" job so that lint results are delivered quickly
+  # by running them in parallel with other jobs and not waiting for any builds.
   lint:
     strategy:
       fail-fast: false
@@ -289,9 +287,6 @@ jobs:
 
     runs-on: ubuntu-latest
     continue-on-error: false
-
-    env:
-      RUSTFLAGS: "-Dwarnings"
 
     steps:
     - uses: actions/checkout@v7

.github/workflows/unstable-ci.yml
@@ -149,10 +149,8 @@ jobs:
 
   # Run `xtask lint`.
   #
-  # This job is separate from the main "build" job so that:
-  # * Lint results are delivered quickly by running them in parallel with other jobs.
-  # * We build with `-Dwarnings` so the failures aren't silent, but don't want this to
-  #   cause *re*building in the main build job and its cache.
+  # This job is separate from the main "build" job so that lint results are delivered quickly
+  # by running them in parallel with other jobs and not waiting for any builds.
   lint:
     strategy:
       fail-fast: false
@@ -161,9 +159,6 @@ jobs:
 
     runs-on: ubuntu-latest
     continue-on-error: false
-
-    env:
-      RUSTFLAGS: "-Dwarnings"
 
     steps:
     - uses: actions/checkout@v7

tools/xtask/src/args.rs
@@ -53,6 +53,8 @@ pub(crate) enum XtaskCommand {
     },
 
     /// Run all tests (and some builds without tests) with default features.
+    ///
+    /// Does not fail on warnings.
     Test {
         /// Build test executables, but don't run them.
         #[arg(long)]
@@ -67,17 +69,20 @@ pub(crate) enum XtaskCommand {
 
     /// Check for lint and generate documentation (to lint the doc markdown), but do not test.
     ///
-    /// Caution: If there are rustc or clippy warnings only, the exit code is still zero.
-    /// <https://github.com/rust-lang/rust-clippy/issues/1209>
+    /// Fails on warnings.
     Lint,
 
     /// Check every feature combination builds (but do not test them).
+    ///
+    /// Does not fail on warnings.
     CheckFeatures,
 
     /// Build documentation.
     ///
     /// This is approximately the same as `cargo doc` but uses the same options as `xtask lint`
     /// does in order to avoid spurious rebuilds.
+    ///
+    /// Fails on warnings.
     Doc,
 
     /// Format code (as `cargo fmt` but covering all packages)

commit 6288b613467851d4cd980028cbd5bd2e3f936db6

xtask: Only `-Dwarnings` for lint and not other commands.
This fixes a logic bug introduced by e60b8e7464ada7afe3f1ed0fa083f42e50cb6b24
where we replaced CI `env: -Dwarnings` with code inside xtask; prior to
that change, and after this change, only lint commands will fail on
warnings.

Also renamed the config field to be clearer

tools/xtask/src/context.rs
@@ -17,7 +17,7 @@ pub struct Config<'a> {
     pub cargo_quiet: bool,
     pub time_log_quiet: bool,
     /// Whether to use `RUSTFLAGS=-Dwarnings` instead of Cargo `build.warnings="deny"`.
-    pub use_d_warnings: bool,
+    pub use_rustflags_to_deny_warnings: bool,
     pub scope: Scope,
     pub main_metadata: cargo_metadata::Metadata,
     pub time_log_tx: std::sync::mpsc::Sender<Timing>,
@@ -99,18 +99,23 @@ pub(crate) enum TestOrCheck {
 
 impl TestOrCheck {
     pub fn cargo_cmd<'a>(self, config: &'a Config<'_>) -> Cmd<'a> {
+        let mut rustflags_deny_warnings = false;
+
         let mut cmd = config
             .cargo()
             .args(match self {
                 Self::Test => vec!["test"],
                 Self::BuildTests => vec!["test", "--no-run"],
-                Self::Lint if config.use_d_warnings => vec!["clippy"],
+                Self::Lint if config.use_rustflags_to_deny_warnings => {
+                    rustflags_deny_warnings = true;
+                    vec!["clippy"]
+                }
                 Self::Lint => vec!["clippy", "--config=build.warnings='deny'"],
                 Self::Check => vec!["check"],
             })
             .args(config.cargo_build_args());
 
-        if config.use_d_warnings {
+        if rustflags_deny_warnings {
             let new_rustflags = match std::env::var("RUSTFLAGS") {
                 Ok(old_rustflags) => format!("{old_rustflags} -Dwarnings"),
                 Err(_) => String::from("-Dwarnings"),

tools/xtask/src/main.rs
@@ -57,10 +57,10 @@ fn main() -> Result<(), ActionError> {
         .exec()?;
 
     // Can't use build.warnings when linting on unstable/future toolchains, because they might
-    // have new `renamed_and_removed_lints` that should be ignored:
+    // have new `renamed_and_removed_lints` that should be ignored, but build.warnings doesn’t
+    // implement ignoring them:
     //   <https://github.com/rust-lang/cargo/issues/17193>
-    // TODO
-    let use_d_warnings = CI && {
+    let use_rustflags_to_deny_warnings = CI && {
         let rustc_version_output = sh.cmd("rustc").arg("--version").output().unwrap().stdout;
         let rustc_version_output = str::from_utf8(&rustc_version_output).unwrap();
         rustc_version_output.contains("nightly") || rustc_version_output.contains("beta")
@@ -82,7 +82,7 @@ fn main() -> Result<(), ActionError> {
             cargo_timings: timings,
             cargo_quiet: quiet,
             time_log_quiet: quiet,
-            use_d_warnings,
+            use_rustflags_to_deny_warnings,
             scope,
             main_metadata,
             time_log_tx,